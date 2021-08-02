1c3326c1f5b0969a975516fc3a700bd8b9fad3a7
Geoffrey Deldique <gde@efficy.com>
2021-08-02 16:34:35 +0200

    BTabs (contd)

--- src/components/BTab.vue
+++ src/components/BTab.vue
@@ -1,22 +1,44 @@
 <template>
-  <div
+  <component
+    :is="tag"
+    :id="id"
     class="tab-pane"
-    :class="{ active, show: active }"
+    :class="classes"
     role="tabpanel"
     aria-labelledby="profile-tab"
   >
     <slot />
-  </div>
+  </component>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import {computed, defineComponent} from 'vue'
 
 export default defineComponent({
     name: 'BTab',
     props: {
         active: { type: Boolean, default: false },
+        buttonId: { type: String, default: null },
+        disabled: { type: Boolean, default: false },
+        id: { type: String },
+        // lazy: { type: Boolean, default: false },
+        noBody: { type: [Boolean, String], default: false },
+        tag: { type: String, default: 'div'},
         title: { type: String },
+        titleItemClass: { type: [Array, Object, String], default: null },
+        titleLinkAttribute: { type: Object, default: null },
+        titleLinkClass: { type: [Array, Object, String], default: null },
     },
+     setup(props) {
+        const classes = computed(() => ({
+            'active': props.active,
+            'show': props.active,
+            'card-body': props.noBody === false
+        }));
+
+        return {
+          classes
+        };
+      }
 })
 </script>
