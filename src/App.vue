1f33efcb8985ada3eb8e2a0a24a33821f3e25a5a
Geoffrey Deldique <gde@efficy.com>
2021-08-03 15:54:37 +0200

    BCard - add missing properties + BTabs minor improvement

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,26 @@
     fluid="sm"
   >
     <div>
-       <h5 class="my-3">
+      <h5 class="my-3">
+        Cards
+      </h5>
+      <b-card
+        title="Card Title"
+        img-src="https://picsum.photos/600/300/?image=25"
+        img-alt="Image"
+        img-top
+        tag="article"
+        style="max-width: 20rem;"
+        class="mb-2"
+      >
+        <b-card-text>
+          Some quick example text to build on the card title and make up the bulk of the card's content.
+        </b-card-text>
+
+        <b-button href="#" variant="primary">Go somewhere</b-button>
+      </b-card>
+
+      <h5 class="my-3">
          Tabs
        </h5>
       <b-tabs small v-model="tabIndex">
