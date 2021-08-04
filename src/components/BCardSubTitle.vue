92fa219e044d1d0557b814a013a05397c0ecc60a
Geoffrey Deldique <gde@efficy.com>
2021-08-04 16:07:24 +0200

    BCardBody & BCardSubTitle

--- src/components/BCardSubTitle.vue
+++ src/components/BCardSubTitle.vue
@@ -1,5 +1,33 @@
 <template>
-  <h6 class="card-subtitle mb-2 text-muted">
-    <slot />
-  </h6>
+  <component
+    :is="subTitleTag"
+    class="card-subtitle mb-2"
+    :class="classes"
+  >
+    <slot>
+      {{ subTitle }}
+    </slot>
+  </component>
 </template>
+
+<script lang="ts">
+import {computed, defineComponent} from 'vue'
+
+export default defineComponent({
+    name: 'BCardSubTitle',
+    props: {
+        subTitle: { type: String },
+        subTitleTag: { type: String, default: 'h6' },
+        subTitleTextVariant: { type: String, default: 'muted' },
+    },
+    setup(props) {
+      const classes = computed(() => ({
+        [`text-${props.subTitleTextVariant}`]: props.subTitleTextVariant,
+      }));
+
+      return {
+        classes,
+      };
+    }
+})
+</script>
