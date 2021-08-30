e636a2f8cb19c3cb8d9c23b42de945dc43ac10bd
Marius <marius.andreiana@gmail.com>
2021-08-30 11:53:45 +0300

    tab size 2 setting

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    presets: [
-        ["@babel/preset-env", {
-            targets: {
-                esmodules: true,
-            }
-        }]
-    ]
+  presets: [
+    ["@babel/preset-env", {
+      targets: {
+        esmodules: true,
+      }
+    }]
+  ]
 }
--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,25 @@
 module.exports = {
-    preset: 'vite-jest',
-    extensionsToTreatAsEsm: ['.ts'],
-    globals: {
-        'ts-jest': {
-            useESM: true,
-        },
+  preset: 'vite-jest',
+  extensionsToTreatAsEsm: ['.ts'],
+  globals: {
+    'ts-jest': {
+      useESM: true,
     },
-    testRegex: "spec.js$",
-    moduleFileExtensions: ["js", "vue", "ts"],
-    transform: {
-        "^.+\\.js$": "babel-jest",
-        "^.+\\.ts$": "ts-jest",
-        ".*\\.(vue)$": "vue3-jest"
-    },
-    coverageDirectory: "./coverage/",
-    testEnvironmentOptions: {
-        pretendToBeVisual: true
-    },
-    testEnvironment: "jsdom",
-    setupFilesAfterEnv: ["./tests/setup.ts"],
-    moduleNameMapper: {
-        "^@/(.*)$": "<rootDir>/src/$1"
-    }
+  },
+  testRegex: "spec.js$",
+  moduleFileExtensions: ["js", "vue", "ts"],
+  transform: {
+    "^.+\\.js$": "babel-jest",
+    "^.+\\.ts$": "ts-jest",
+    ".*\\.(vue)$": "vue3-jest"
+  },
+  coverageDirectory: "./coverage/",
+  testEnvironmentOptions: {
+    pretendToBeVisual: true
+  },
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["./tests/setup.ts"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1"
+  }
 }
