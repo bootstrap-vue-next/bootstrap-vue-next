249cc7bb7ec4b35c5d9420bd1997587ba9172c89
bobaoapae <jvborges@outlook.com>
2021-08-30 10:02:36 -0300

    Remove vue/cli-plugin-babel/preset

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          esmodules: true,
+        },
+      },
+    ],
+  ],
 }
