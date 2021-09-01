a7f16e927dee20569cf95808cb1765c6df38d43f
Geoffrey Deldique <gde@efficy.com>
2021-09-01 14:25:05 +0200

    BDropdownDivider (contd)

--- src/components/BDropdownDivider.vue
+++ src/components/BDropdownDivider.vue
@@ -1,3 +1,21 @@
 <template>
-  <li><hr class="dropdown-divider"></li>
+  <li role="presentation">
+    <component
+        :is="tag"
+        class="dropdown-divider"
+        role="separator"
+        aria-orientation="horizontal"
+    />
+  </li>
 </template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: 'BDropdownDivider',
+  props: {
+    tag: {type: String, default: 'hr'},
+  }
+})
+</script>
