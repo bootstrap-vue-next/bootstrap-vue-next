2d31099e888d9d2e2e5b1532b6b3e79004985215
aceofwings <dan303h@gmail.com>
2021-09-15 16:41:00 -0400

    Add Additional Utils

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -1,5 +1,7 @@
 export const isElement = (el: HTMLElement): boolean => !!(el && el.nodeType === Node.ELEMENT_NODE)
 
+export const getBCR = (el: HTMLElement) => (isElement(el) ? el.getBoundingClientRect() : null)
+
 export const getActiveElement = (excludes = []): Element | null => {
   const {activeElement} = document
   return activeElement && !excludes.some((el: HTMLElement) => el === activeElement)
@@ -27,4 +29,27 @@ export const attemptBlur = (el: HTMLElement): boolean => {
     console.error(e)
   }
   return !isActiveElement(el)
+}
+
+export const getStyle = (el: HTMLElement, prop: string) =>
+  prop && isElement(el) ? el.getAttribute(prop) || null : null
+
+export const contains = (parent: Node, child: Node): boolean => parent.contains(child)
+
+export const isVisible = (el: HTMLElement): boolean => {
+  //if (!isElement(el) || !el.parentNode || !contains(DOCUMENT.body, el)) {
+  // Note this can fail for shadow dom elements since they
+  // are not a direct descendant of document.body
+  //return false
+  //}
+  if (getStyle(el, 'display') === 'none') {
+    // We do this check to help with vue-test-utils when using v-show
+    /* istanbul ignore next */
+    return false
+  }
+  // All browsers support getBoundingClientRect(), except JSDOM as it returns all 0's for values :(
+  // So any tests that need isVisible will fail in JSDOM
+  // Except when we override the getBCR prototype in some tests
+  const bcr = getBCR(el)
+  return !!(bcr && bcr.height > 0 && bcr.width > 0)
 }
--- src/utils/number.ts
+++ src/utils/number.ts
@@ -1,16 +1,13 @@
 // Number utilities
 
-// Converts a value (string, number, etc.) to an integer number
-// Assumes radix base 10
+//Validates a number is an integer. If not return NaN
 export const toInteger = (value: number, defaultValue = NaN): number => {
   return Number.isInteger(value) ? defaultValue : value
 }
-// Converts a value (string, number, etc.) to a number
+//Validates a number is a float. If not return NaN
 export const toFloat = (value: number, defaultValue = NaN): number => {
   return !Number.isInteger(value) ? defaultValue : value
 }
 
-// Converts a value (string, number, etc.) to a string
-// representation with `precision` digits after the decimal
-// Returns the string 'NaN' if the value cannot be converted
-export const toFixed = (val: number, precision: number) => val.toPrecision(precision)
+//Float given to a certain decimal place
+export const toPercison = (val: number, precision: number) => toFloat(val).toPrecision(precision)
