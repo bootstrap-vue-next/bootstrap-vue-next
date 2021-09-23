ffa50aca8e265b9721719efed570cc8fa5e815fc
Geoffrey Deldique <gde@efficy.com>
2021-09-23 16:14:02 +0200

    BTabs - add ability to add custom content to tab title (like https://bootstrap-vue.org/docs/components/tabs#add-custom-content-to-tab-title)

--- src/App.vue
+++ src/App.vue
@@ -234,6 +234,26 @@
       </div>
     </div>
     <div>
+      <h5 class="my-3">Tabs with custom title</h5>
+      <b-tabs>
+        <b-tab active>
+          <template #title>
+            <b-spinner type="grow" small></b-spinner>
+            I'm <i>custom</i> <strong>title</strong>
+          </template>
+          <p class="p-3">Tab contents 1</p>
+        </b-tab>
+
+        <b-tab>
+          <template #title>
+            <b-spinner type="border" small></b-spinner>
+            Tab 2
+          </template>
+          <p class="p-3">Tab contents 2</p>
+        </b-tab>
+      </b-tabs>
+    </div>
+    <div>
       <h5 class="my-3">Basic layout</h5>
       <div class="bd-example-row">
         <b-row>
