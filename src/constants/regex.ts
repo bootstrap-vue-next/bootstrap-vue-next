4ad28b8336e080ea5ec73c309625f992ad367ab3
Jos Clijmans <jos.clijmans@gmail.com>
2021-10-23 00:18:53 +0200

    Fix for BCollapse

--- src/constants/regex.ts
+++ src/constants/regex.ts
@@ -3,3 +3,5 @@ export const RX_LOWER_UPPER = /([a-z])([A-Z])/g
 export const RX_START_SPACE_WORD = /(\s|^)(\w)/g
 export const RX_FIRST_START_SPACE_WORD = /(\s|^)(\w)/
 export const RX_SPACE_SPLIT = /\s+/
+export const RX_HASH = /^#/
+export const RX_HASH_ID = /^#[A-Za-z]+[\w\-:.]*$/
