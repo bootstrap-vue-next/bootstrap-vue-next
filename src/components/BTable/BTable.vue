e669f4235ded79fc7da99bc12e0787d0c0f95b6d
protomors <protomors@gmail.com>
2021-10-26 04:17:13 +0300

    BTable: setting cell classes in field options.

--- src/components/BTable/BTable.vue
+++ src/components/BTable/BTable.vue
@@ -43,23 +43,21 @@ export default defineComponent({
     const itemHelper = useItemHelper()
     const computedFields = computed(() => itemHelper.normaliseFields(props.fields, props.items))
 
-    const headerTable = computed(() => {
-      if (computedFields.value.length > 0) {
-        return computedFields.value.map((f) => f.label)
-      }
-      return []
-    })
-
-    // return {
-    //     classes,
-    //     headerTable
-    // }
     return () => {
       const tHead = h(
         'thead',
         h(
           'tr',
-          headerTable.value.map((th) => h('th', {scope: 'col'}, th))
+          computedFields.value.map((field) =>
+            h(
+              'th',
+              {
+                scope: 'col',
+                class: [field.class, field.thClass],
+              },
+              field.label
+            )
+          )
         )
       )
 
@@ -81,7 +79,14 @@ export default defineComponent({
                     items: props.items,
                   })
                 }
-                return h('td', tdContent)
+
+                return h(
+                  'td',
+                  {
+                    class: [field.class, field.tdClass],
+                  },
+                  tdContent
+                )
               })
             )
           )
@@ -104,7 +109,16 @@ export default defineComponent({
           'tfoot',
           h(
             'tr',
-            headerTable.value.map((th) => h('th', {scope: 'col'}, th))
+            computedFields.value.map((field) =>
+              h(
+                'th',
+                {
+                  scope: 'col',
+                  class: [field.class, field.thClass],
+                },
+                field.label
+              )
+            )
           )
         )
 
