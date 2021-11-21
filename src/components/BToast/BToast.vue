161f3d2f80bf5b11e7c2e27aa5ee698a8df79358
aceofwings <dan303h@gmail.com>
2021-11-20 22:40:48 -0500

    Init Component, Add props

---
+++ src/components/BToast/BToast.vue
@@ -0,0 +1,34 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import {defineComponent, PropType} from 'vue'
+
+export default defineComponent({
+  name: 'BToast',
+  props: {
+    appendToast: {type: Boolean, default: false},
+    autoHideDelay: {type: Number, default: 5000},
+    bodyClass: {type: String},
+    headerClass: {type: String},
+    headerTag: {type: String, default: 'header'},
+    // Switches role to 'status' and aria-live to 'polite'
+    isStatus: {type: Boolean, default: false},
+    noAutoHide: {type: Boolean, default: false},
+    noCloseButton: {type: Boolean, default: false},
+    noFade: {type: Boolean, default: false},
+    noHoverPause: {type: Boolean, default: false},
+    solid: {type: Boolean, default: false},
+    // Render the toast in place, rather than in a portal-target
+    static: {type: Boolean, default: false},
+    title: {type: String},
+    toastClass: {type: Array as PropType<Array<string>>},
+    toaster: {type: String, default: 'b-toaster-top-right'},
+    variant: {type: String},
+  },
+  setup() {
+    return {}
+  },
+})
+</script>
