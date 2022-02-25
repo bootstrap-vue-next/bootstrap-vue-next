4247409fe12f4d315d7eb1b66f04f2ded60c2f14
Samuel Fernandez <sfe@efficy.com>
2022-02-25 15:25:38 +0100

    Add missing gx-x and gy-x classes to control the padding values for the div.

--- src/components/BContainer.vue
+++ src/components/BContainer.vue
@@ -11,6 +11,8 @@ import {computed, defineComponent, PropType} from 'vue'
 export default defineComponent({
   name: 'BContainer',
   props: {
+    gutterX: {type: String, default: null},
+    gutterY: {type: String, default: null},
     fluid: {type: [Boolean, String] as PropType<boolean | Breakpoint>, default: false},
   },
   setup(props) {
@@ -18,6 +20,8 @@ export default defineComponent({
       container: !props.fluid,
       [`container-fluid`]: typeof props.fluid === 'boolean' && props.fluid,
       [`container-${props.fluid}`]: typeof props.fluid === 'string',
+      [`gx-${props.gutterX}`]: props.gutterX !== null,
+      [`gy-${props.gutterY}`]: props.gutterY !== null,
     }))
 
     return {
