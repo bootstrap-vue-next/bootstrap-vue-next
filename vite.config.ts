cd77366a9c297fd0575e545c2ef65d9836bc8707
Jukka Raimovaara <roska@mentalhouse.fi>
2022-03-18 10:53:14 +0200

    add bootstrap js to external

--- vite.config.ts
+++ vite.config.ts
@@ -20,15 +20,34 @@ const config = defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['bootstrap', 'vue'],
+      external: [
+        'bootstrap',
+        'bootstrap/js/dist/alert',
+        'bootstrap/js/dist/collapse',
+        'bootstrap/js/dist/modal',
+        'bootstrap/js/dist/offcanvas',
+        'bootstrap/js/dist/popover',
+        'bootstrap/js/dist/carousel',
+        'bootstrap/js/dist/dropdown',
+        'bootstrap/js/dist/tooltip',
+        'vue',
+      ],
       output: {
         exports: 'named',
         assetFileNames: `bootstrap-vue-3.[ext]`, //without this, it generates build/styles.css
         // Provide global variables to use in the UMD build
         // for externalized deps
         globals: {
-          vue: 'Vue',
-          bootstrap: 'Bootstrap',
+          'vue': 'Vue',
+          'bootstrap': 'Bootstrap',
+          'bootstrap/js/dist/collapse': 'Collapse',
+          'bootstrap/js/dist/alert': 'Alert',
+          'bootstrap/js/dist/carousel': 'Carousel',
+          'bootstrap/js/dist/dropdown': 'Dropdown',
+          'bootstrap/js/dist/modal': 'Modal',
+          'bootstrap/js/dist/offcanvas': 'Offcanvas',
+          'bootstrap/js/dist/popover': 'Popover',
+          'bootstrap/js/dist/tooltip': 'Tooltip',
         },
       },
     },
