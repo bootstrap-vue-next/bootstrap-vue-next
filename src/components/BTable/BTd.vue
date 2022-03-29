c7c23e9e0dc5d76fb2542ee00282bfadcd616e7c
Carlos <carlosbonadeo@gmail.com>
2022-03-29 21:36:55 +0100

    Fix

--- src/components/BTable/BTd.vue
+++ src/components/BTable/BTd.vue
@@ -1,44 +1,43 @@
-<template>
-  <Render />
-</template>
+<script lang="ts">
+import {computed, defineComponent, h, useSlots} from 'vue'
 
-<script setup lang="ts">
-import {computed, h, useSlots, VNode} from 'vue'
-
-const props = defineProps({
-  colspan: {type: [String, Number]},
-  rowspan: {type: [String, Number]},
-  stackedHeading: {type: String},
-  stickyColumn: {type: Boolean, default: false},
-  variant: {type: String},
-})
-
-const slots = useSlots()
-
-const classes = computed(() => ({
-  [`table-${props.variant}`]: props.variant,
-  'b-table-sticky-column': props.stickyColumn,
-  'table-b-table-default': props.stickyColumn && !props.variant,
-}))
-
-const scope = computed(() => (props.colspan ? 'colspan' : props.rowspan ? 'rowspan' : 'col'))
-
-let content: VNode[] | VNode | undefined = slots.default?.()
-
-if (props.stackedHeading) {
-  content = h('div', content)
-}
-
-const Render = h(
-  'td',
-  {
-    'role': 'cell',
-    'scope': scope.value,
-    'class': classes.value,
-    'colspan': props.colspan,
-    'rowspan': props.rowspan,
-    'data-label': props.stackedHeading,
+export default defineComponent({
+  props: {
+    colspan: {type: [String, Number]},
+    rowspan: {type: [String, Number]},
+    stackedHeading: {type: String},
+    stickyColumn: {type: Boolean, default: false},
+    variant: {type: String},
   },
-  content
-)
+
+  setup(props) {
+    const slots = useSlots()
+
+    const classes = computed(() => ({
+      [`table-${props.variant}`]: props.variant,
+      'b-table-sticky-column': props.stickyColumn,
+      'table-b-table-default': props.stickyColumn && !props.variant,
+    }))
+
+    const scope = computed(() => (props.colspan ? 'colspan' : props.rowspan ? 'rowspan' : 'col'))
+
+    const children = computed(() =>
+      props.stackedHeading ? h('div', slots.default?.()) : slots.default?.()
+    )
+
+    return () =>
+      h(
+        'td',
+        {
+          'role': 'cell',
+          'scope': scope.value,
+          'class': classes.value,
+          'colspan': props.colspan,
+          'rowspan': props.rowspan,
+          'data-label': props.stackedHeading,
+        },
+        children.value
+      )
+  },
+})
 </script>
