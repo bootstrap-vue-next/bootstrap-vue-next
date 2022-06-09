7e50fe99509ce6e8918be796d4c3eb6b80705baf
issayah <kwiksilver344@gmail.com>
2022-06-09 16:19:18 -0500

    Stage index file

--- src/types/components/index.ts
+++ src/types/components/index.ts
@@ -7,11 +7,13 @@ export type {Emits as BAvatarEmits} from './BAvatar/BAvatar'
 export type {Props as BAvatarGroupProps} from './BAvatar/BAvatarGroup'
 export type {ParentData as BAvatarGroupParentData} from './BAvatar/BAvatarGroup'
 // BBadge
+export type {Props as BBadgeProps} from './BBadge/BBadge'
 // BBreadcrumb
 export type {Props as BBreadcrumbProps} from './BBreadcrumb/BBreadcrumb'
-// TODO BBreadcrumbItem
+export type {Props as BBreadcrumbItem} from './BBreadcrumb/BBreadcrumbItem'
 // BButton
-// TODO BButton
+export type {Props as BButtonProps} from './BButton/BButton'
+export type {Emits as BButtonEmits} from './BButton/BButton'
 export type {Props as BButtonGroupProps} from './BButton/BButtonGroup'
 export type {Props as BButtonToolbarProps} from './BButton/BButtonToolbar'
 export type {Props as BCloseButtonProps} from './BButton/BCloseButton'
@@ -23,7 +25,6 @@ export type {Props as BCardGroupProps} from './BCard/BCardGroup'
 export type {Props as BCardHeaderProps} from './BCard/BCardHeader'
 export type {Props as BCardImgProps} from './BCard/BCardImg'
 export type {Props as BCardSubtitleProps} from './BCard/BCardSubtitle'
-// TODO BCardText
 export type {Props as BCardTitleProps} from './BCard/BCardTitle'
 // BCarousel
 export type {Props as BCarouselProps} from './BCarousel/BCarousel'
@@ -34,14 +35,11 @@ export type {Props as BCarouselSlideProps} from './BCarousel/BCarouselSlide'
 export type {Props as BDropdownProps} from './BDropdown/BDropdown'
 export type {Emits as BDropdownEmits} from './BDropdown/BDropdown'
 export type {Props as BDropdownDividerProps} from './BDropdown/BDropdownDivider'
-// TODO BDropdownForm
 export type {Props as BDropdownGroupProps} from './BDropdown/BDropdownGroup'
-// TODO BDropdownHeader
 export type {Props as BDropdownItemProps} from './BDropdown/BDropdownItem'
 export type {Emits as BDropdownItemEmits} from './BDropdown/BDropdownItem'
 export type {Props as BDropdownItemButtonProps} from './BDropdown/BDropdownItemButton'
 export type {Emits as BDropdownItemButtonEmits} from './BDropdown/BDropdownItemButton'
-// TODO BDropdownText
 // BForm
 export type {Props as BFormProps} from './BForm/BForm'
 export type {Emits as BFormEmits} from './BForm/BForm'
@@ -58,7 +56,8 @@ export type {Emits as BFormCheckboxGroupEmits} from './BFormCheckbox/BFormCheckb
 // BFormGroup
 export type {Props as BFormGroupProps} from './BFormGroup/BFormGroup'
 // BFormInput
-// TODO BFormInput
+export type {Props as BFormInputProps} from './BFormInput/BFormInput'
+export type {Emits as BFormInputEmits} from './BFormInput/BFormInput'
 // BFormRadio
 export type {Props as BFormRadioProps} from './BFormRadio/BFormRadio'
 export type {Emits as BFormRadioEmits} from './BFormRadio/BFormRadio'
@@ -75,7 +74,8 @@ export type {Emits as BFormTagEmits} from './BFormTags/BFormTag'
 export type {Props as BFormTagsProps} from './BFormTags/BFormTags'
 export type {Emits as BFormTagsEmits} from './BFormTags/BFormTags'
 // BFormTextarea
-// TODO BFormTextarea
+export type {Props as BFormTextareaProps} from './BFormTextarea/BFormTextarea'
+export type {Emits as BFormTextareaEmits} from './BFormTextarea/BFormTextarea'
 // BInputGroup
 export type {Props as BInputGroupProps} from './BInputGroup/BInputGroup'
 export type {Props as BInputGroupAddonProps} from './BInputGroup/BInputGroupAddon'
@@ -91,7 +91,7 @@ export type {Props as BListGroupItemProps} from './BListGroup/BListGroupItem'
 // BOverlay
 export type {Props as BOverlayProps} from './BOverlay/BOverlay'
 export type {Emits as BOverlayEmits} from './BOverlay/BOverlay'
-export type {ISlotScope as BOverlayISlotScope} from './BOverlay/BOverlay'
+export type {SlotScope as BOverlaySlotScope} from './BOverlay/BOverlay'
 // BPagination
 export type {Props as BPaginationProps} from './BPagination/BPagination'
 export type {Emits as BPaginationEmits} from './BPagination/BPagination'
@@ -114,27 +114,45 @@ export type {Props as BTabProps} from './BTabs/BTab'
 export type {Props as BTabsProps} from './BTabs/BTabs'
 // BToast
 export type {Props as BToastProps} from './BToast/BToast'
+export type {Emits as BToastEmits} from './BToast/BToast'
 export type {Props as BToasterProps} from './BToast/BToaster'
 // BTransition
 export type {Props as BTransitionProps} from './BTransition/BTransition'
 //
 export type {Props as BAlertProps} from './BAlert'
 export type {Emits as BAlertEmits} from './BAlert'
-export type {Props as BColProps} from './BCol'
+
+// export type {Props as BColProps} from './BCol'
+// TODO BCol
+
 export type {Props as BCollapseProps} from './BCollapse'
 export type {Emits as BCollapseEmits} from './BCollapse'
+
 export type {Props as BContainerProps} from './BContainer'
+
 export type {Props as BImgProps} from './BImg'
+
 export type {Props as BModalProps} from './BModal'
 export type {Emits as BModalEmits} from './BModal'
+
 export type {Props as BNavProps} from './BNav'
+
 export type {Props as BNavItemProps} from './BNavItem'
+
 export type {Props as BNavItemDropdownProps} from './BNavItemDropdown'
+
 export type {Props as BOffcanvasProps} from './BOffcanvas'
 export type {Emits as BOffcanvasEmits} from './BOffcanvas'
+
 export type {Props as BPopoverProps} from './BPopover'
 export type {Emits as BPopoverEmits} from './BPopover'
+
 export type {Props as BProgressProps} from './BProgress'
+
 export type {Props as BProgressBarProps} from './BProgressBar'
+
 export type {Props as BRowProps} from './BRow'
+
+// TODO BRow
+
 export type {Props as BSpinnerProps} from './BSpinner'
