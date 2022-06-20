51e4fcf21d61c0379c60fc36122afdd37b4c7755
issayah <kwiksilver344@gmail.com>
2022-06-20 10:56:13 -0500

    BButton's script setup conversion and improvements

---
+++ src/components/BButton/BButtonToolbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div :class="classes" class="btn-toolbar" role="toolbar" :aria-label="ariaRole">
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+// import type {BButtonToolbarProps} from '@/types/components'
+import {computed} from 'vue'
+
+interface BButtonToolbarProps {
+  ariaRole?: string
+  justify?: boolean
+  // keyNav?: boolean
+}
+
+const props = withDefaults(defineProps<BButtonToolbarProps>(), {
+  ariaRole: 'group',
+  justify: false,
+})
+
+const classes = computed(() => ({
+  'justify-content-between': props.justify,
+}))
+</script>
