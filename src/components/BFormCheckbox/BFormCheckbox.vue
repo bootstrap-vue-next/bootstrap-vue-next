37d489fde04a1d2aae20605fb0559cd93b1a4d08
issayah <kwiksilver344@gmail.com>
2022-06-23 09:13:49 -0500

    BFormCheckbox script setup and bug fixes*breaking*

--- src/components/BFormCheckbox/BFormCheckbox.vue
+++ src/components/BFormCheckbox/BFormCheckbox.vue
@@ -8,12 +8,12 @@
       :class="inputClasses"
       type="checkbox"
       :disabled="disabled"
-      :required="name && required"
+      :required="!!name && !!required"
       :name="name"
       :form="form"
       :aria-label="ariaLabel"
       :aria-labelledby="ariaLabelledBy"
-      :aria-required="name && required ? 'true' : null"
+      :aria-required="name && required ? 'true' : undefined"
       :value="value"
       :indeterminate="indeterminate"
       @focus="isFocused = true"
@@ -29,104 +29,119 @@
   </div>
 </template>
 
-<script lang="ts">
-import {getClasses, getInputClasses, getLabelClasses} from '../../composables/useFormCheck'
-import {computed, defineComponent, onMounted, PropType, Ref, ref} from 'vue'
-import {InputSize} from '../../types'
-import useId from '../../composables/useId'
+<script setup lang="ts">
+// import type {BFormCheckboxEmits, BFormCheckboxProps} from '@/types/components'
+import {getClasses, getInputClasses, getLabelClasses} from '@/composables/useFormCheck'
+import type {ButtonVariant, InputSize} from '@/types'
+import useId from '@/composables/useId'
 
-export default defineComponent({
-  name: 'BFormCheckbox',
-  inheritAttrs: false,
-  props: {
-    id: {type: String, default: undefined},
-    ariaLabel: {type: String},
-    ariaLabelledBy: {type: String},
-    autofocus: {type: Boolean, default: false},
-    plain: {type: Boolean, default: false},
-    button: {type: Boolean, default: false},
-    switch: {type: Boolean, default: false},
-    disabled: {type: Boolean, default: false},
-    buttonVariant: {type: String, default: 'secondary'},
-    form: {type: String},
-    indeterminate: {type: Boolean},
-    inline: {type: Boolean, default: false},
-    name: {type: String},
-    required: {type: Boolean, default: undefined},
-    size: {type: String as PropType<InputSize>, default: 'md'},
-    state: {type: Boolean, default: null},
-    uncheckedValue: {type: [Boolean, String, Array, Object, Number], default: false},
-    value: {type: [Boolean, String, Array, Object, Number], default: true},
-    modelValue: {type: [Boolean, String, Array, Object, Number], default: null},
-  },
-  emits: ['update:modelValue', 'input', 'change'],
-  setup(props, {emit}) {
-    const computedId = useId(props.id, 'form-check')
-    const input: Ref<HTMLElement> = ref(null as unknown as HTMLElement)
-    const isFocused = ref(false)
+interface BFormCheckboxProps {
+  ariaLabel?: string
+  ariaLabelledBy?: string
+  form?: string
+  indeterminate?: boolean
+  name?: string
+  id?: string
+  autofocus?: boolean
+  plain?: boolean
+  button?: boolean
+  switch?: boolean
+  disabled?: boolean
+  buttonVariant?: ButtonVariant
+  inline?: boolean
+  required?: boolean
+  size?: InputSize
+  state?: boolean
+  uncheckedValue?: Array<unknown> | Set<unknown> | boolean
+  value?: Array<unknown> | Set<unknown> | boolean
+  modelValue?: Array<unknown> | Set<unknown> | boolean
+}
 
-    const localValue = computed({
-      get: () => {
-        if (props.uncheckedValue) {
-          if (!Array.isArray(props.modelValue)) {
-            return props.modelValue === props.value
-          }
-          return props.modelValue.indexOf(props.value) > -1
-        }
-        return props.modelValue
-      },
-      set: (newValue: any) => {
-        let emitValue = newValue
-        if (!Array.isArray(props.modelValue)) {
-          emitValue = newValue ? props.value : props.uncheckedValue
-        } else {
-          if (props.uncheckedValue) {
-            emitValue = props.modelValue
-            if (newValue) {
-              if (emitValue.indexOf(props.uncheckedValue) > -1)
-                emitValue.splice(emitValue.indexOf(props.uncheckedValue), 1)
-              emitValue.push(props.value)
-            } else {
-              if (emitValue.indexOf(props.value) > -1)
-                emitValue.splice(emitValue.indexOf(props.value), 1)
-              emitValue.push(props.uncheckedValue)
-            }
-          }
-        }
-        emit('input', emitValue)
-        emit('update:modelValue', emitValue)
-        emit('change', emitValue)
-      },
-    })
+const props = withDefaults(defineProps<BFormCheckboxProps>(), {
+  id: undefined,
+  autofocus: false,
+  plain: false,
+  button: false,
+  switch: false,
+  disabled: false,
+  buttonVariant: 'secondary',
+  inline: false,
+  required: undefined,
+  size: 'md',
+  state: undefined,
+  uncheckedValue: false,
+  value: true,
+  modelValue: undefined,
+})
 
-    const isChecked = computed(() => {
-      if (Array.isArray(props.modelValue)) {
-        return props.modelValue.indexOf(props.value) > -1
+interface BFormCheckboxEmits {
+  (e: 'update:modelValue', value: unknown): void
+  (e: 'input', value: unknown): void
+  (e: 'change', value: unknown): void
+}
+
+const emit = defineEmits<BFormCheckboxEmits>()
+
+const computedId = useId(props.id, 'form-check')
+const input = ref<HTMLElement>(null as unknown as HTMLElement)
+const isFocused = ref<boolean>(false)
+
+const localValue = computed({
+  get: () => {
+    if (props.uncheckedValue) {
+      if (!Array.isArray(props.modelValue)) {
+        return props.modelValue === props.value
       }
-      return JSON.stringify(props.modelValue) === JSON.stringify(props.value)
-    })
-
-    const classes = getClasses(props)
-    const inputClasses = getInputClasses(props)
-    const labelClasses = getLabelClasses(props)
-
-    // TODO: make tests compatible with the v-focus directive
-    if (props.autofocus) {
-      onMounted(() => {
-        input.value.focus()
-      })
+      return props.modelValue.indexOf(props.value) > -1
     }
-
-    return {
-      input,
-      computedId,
-      classes,
-      inputClasses,
-      labelClasses,
-      localValue,
-      isChecked,
-      isFocused,
-    }
+    return props.modelValue
   },
+  set: (newValue: any) => {
+    let emitValue = newValue
+    if (!Array.isArray(props.modelValue)) {
+      emitValue = newValue ? props.value : props.uncheckedValue
+    } else {
+      if (props.uncheckedValue) {
+        emitValue = props.modelValue
+        if (newValue) {
+          if (emitValue.indexOf(props.uncheckedValue) > -1)
+            emitValue.splice(emitValue.indexOf(props.uncheckedValue), 1)
+          emitValue.push(props.value)
+        } else {
+          if (emitValue.indexOf(props.value) > -1)
+            emitValue.splice(emitValue.indexOf(props.value), 1)
+          emitValue.push(props.uncheckedValue)
+        }
+      }
+    }
+    emit('input', emitValue)
+    emit('update:modelValue', emitValue)
+    emit('change', emitValue)
+  },
+})
+
+const isChecked = computed<boolean>(() => {
+  if (Array.isArray(props.modelValue)) {
+    return props.modelValue.indexOf(props.value) > -1
+  }
+  return JSON.stringify(props.modelValue) === JSON.stringify(props.value)
+})
+
+const classes = getClasses(props)
+const inputClasses = getInputClasses(props)
+const labelClasses = getLabelClasses(props)
+
+// TODO: make tests compatible with the v-focus directive
+onMounted((): void => {
+  if (props.autofocus) {
+    input.value.focus()
+  }
+})
+</script>
+
+<script lang="ts">
+import {computed, defineComponent, onMounted, ref} from 'vue'
+export default defineComponent({
+  inheritAttrs: false,
 })
 </script>
--- src/components/BFormCheckbox/BFormCheckboxGroup.vue
+++ src/components/BFormCheckbox/BFormCheckboxGroup.vue
@@ -18,87 +18,104 @@
   </div>
 </template>
 
-<script lang="ts">
-import {computed, defineComponent, PropType} from 'vue'
+<script setup lang="ts">
+// import type {BFormCheckboxGroupEmits, BFormCheckboxGroupProps} from '@/types/components'
+import {computed, useSlots} from 'vue'
 import BFormCheckbox from './BFormCheckbox.vue'
-import {ColorVariant, Size} from '../../types'
-import useId from '../../composables/useId'
+import type {ButtonVariant, Size} from '@/types'
+import useId from '@/composables/useId'
 import {
   bindGroupProps,
   getGroupAttr,
   getGroupClasses,
   optionToElement,
   slotsToElements,
-} from '../../composables/useFormCheck'
+} from '@/composables/useFormCheck'
 
-export default defineComponent({
-  name: 'BFormCheckboxGroup',
-  components: {BFormCheckbox},
-  props: {
-    modelValue: {type: Array, default: () => []},
-    ariaInvalid: {type: [Boolean, String], default: null},
-    autofocus: {type: Boolean, default: false},
-    buttonVariant: {type: String as PropType<ColorVariant>, default: 'secondary'},
-    buttons: {type: Boolean, default: false},
-    disabled: {type: Boolean, default: false},
-    disabledField: {type: String, default: 'disabled'},
-    form: {type: String},
-    htmlField: {type: String, default: 'html'},
-    id: {type: String},
-    name: {type: String},
-    options: {type: Array, default: () => []}, // Objects are not supported yet
-    plain: {type: Boolean, default: false},
-    required: {type: Boolean, default: false},
-    size: {type: String as PropType<Size>},
-    stacked: {type: Boolean, default: false},
-    state: {type: Boolean, default: null},
-    switches: {type: Boolean, default: false},
-    textField: {type: String, default: 'text'},
-    validated: {type: Boolean, default: false},
-    valueField: {type: String, default: 'value'},
-  },
-  emits: ['update:modelValue', 'change', 'input'],
-  setup(props, {emit, slots}) {
-    const slotsName = 'BFormCheckbox'
-    const computedId = useId(props.id, 'checkbox')
-    const computedName = useId(props.name, 'checkbox')
+interface BFormCheckboxGroupProps {
+  id?: string
+  form?: string
+  modelValue?: Array<unknown>
+  ariaInvalid?: boolean | string
+  autofocus?: boolean
+  buttonVariant?: ButtonVariant
+  buttons?: boolean
+  disabled?: boolean
+  disabledField?: string
+  htmlField?: string
+  name?: string
+  options?: Array<unknown> // Objects are not supported yet
+  plain?: boolean
+  required?: boolean
+  size?: Size
+  stacked?: boolean
+  state?: boolean
+  switches?: boolean
+  textField?: string
+  validated?: boolean
+  valueField?: string
+}
 
-    const localValue = computed({
-      get: () => props.modelValue,
-      set: (newValue: any) => {
-        if (JSON.stringify(newValue) === JSON.stringify(props.modelValue)) return
-        emit('input', newValue)
-        emit('update:modelValue', newValue)
-        emit('change', newValue)
-      },
-    })
+const props = withDefaults(defineProps<BFormCheckboxGroupProps>(), {
+  modelValue: () => [],
+  ariaInvalid: undefined,
+  autofocus: false,
+  buttonVariant: 'secondary',
+  buttons: false,
+  disabled: false,
+  disabledField: 'disabled',
+  htmlField: 'html',
+  options: () => [],
+  plain: false,
+  required: false,
+  stacked: false,
+  state: undefined,
+  switches: false,
+  textField: 'text',
+  validated: false,
+  valueField: 'value',
+})
 
-    const checkboxList = computed(() =>
-      (slots.first ? slotsToElements(slots.first(), slotsName, props.disabled) : [])
-        .concat(props.options.map((e) => optionToElement(e, props)))
-        .concat(slots.default ? slotsToElements(slots.default(), slotsName, props.disabled) : [])
-        .map((e, idx) => bindGroupProps(e, idx, props, computedName, computedId))
-        .map((e) => ({
-          ...e,
-          props: {
-            switch: props.switches,
-            ...e.props,
-          },
-        }))
-    )
+interface BFormCheckboxGroupEmits {
+  (e: 'input', value: unknown): void
+  (e: 'update:modelValue', value: unknown): void
+  (e: 'change', value: unknown): void
+}
 
-    const attrs = getGroupAttr(props)
-    const classes = getGroupClasses(props)
+const emit = defineEmits<BFormCheckboxGroupEmits>()
 
-    // TODO: make tests compatible with the v-focus directive
+const slots = useSlots()
 
-    return {
-      attrs,
-      classes,
-      checkboxList,
-      computedId,
-      localValue,
-    }
+const slotsName = 'BFormCheckbox'
+const computedId = useId(props.id, 'checkbox')
+const computedName = useId(props.name, 'checkbox')
+
+const localValue = computed({
+  get: () => props.modelValue,
+  set: (newValue: any) => {
+    if (JSON.stringify(newValue) === JSON.stringify(props.modelValue)) return
+    emit('input', newValue)
+    emit('update:modelValue', newValue)
+    emit('change', newValue)
   },
 })
+
+const checkboxList = computed(() =>
+  (slots.first ? slotsToElements(slots.first(), slotsName, props.disabled) : [])
+    .concat(props.options.map((e) => optionToElement(e, props)))
+    .concat(slots.default ? slotsToElements(slots.default(), slotsName, props.disabled) : [])
+    .map((e, idx) => bindGroupProps(e, idx, props, computedName, computedId))
+    .map((e) => ({
+      ...e,
+      props: {
+        switch: props.switches,
+        ...e.props,
+      },
+    }))
+)
+
+const attrs = getGroupAttr(props)
+const classes = getGroupClasses(props)
+
+// TODO: make tests compatible with the v-focus directive
 </script>
