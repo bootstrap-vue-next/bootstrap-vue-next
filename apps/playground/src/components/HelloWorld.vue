8a3241230032675f420d9aeb41bf337c7b1c0d9d
Issayah <31498096+VividLemon@users.noreply.github.com>
2022-07-16 18:54:38 -0500

    fix: merge global-comps into bootstrapvue main
    Without merging this into the main BootstrapVue.ts, "global-components.d.ts" is never actually fetched. Since it's not fetched, it's injection of global comps is never actually reached. By merging it into the main, it is always injected.

---
+++ apps/playground/src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Hello</h1>
+    <b-accordion />
+    <b-container />
+    {{ a }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import {ref} from 'vue'
+const a = ref(1)
+</script>
