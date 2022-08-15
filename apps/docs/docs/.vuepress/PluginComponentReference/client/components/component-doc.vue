5c60799b7fa81f244aa818d668586d5f250b3229
Issayah <31498096+VividLemon@users.noreply.github.com>
2022-08-15 11:59:22 -0500

    docs: fix github url to main package

--- apps/docs/docs/.vuepress/PluginComponentReference/client/components/component-doc.vue
+++ apps/docs/docs/.vuepress/PluginComponentReference/client/components/component-doc.vue
@@ -145,7 +145,7 @@ export default defineComponent({
         // Example component (most likely an auto generated component)
         return ''
       }
-      const base = 'https://github.com/cdmoro/bootstrap-vue-3/tree/main/src/components'
+      const base = 'https://github.com/cdmoro/bootstrap-vue-3/tree/main/packages/bootstrap-vue-3/src/components'
       // const slug = this.$route.params.slug
       // Always point to the `.js` file (which may import a `.vue` file)
       return `${base}/${name}/${name}.vue`
