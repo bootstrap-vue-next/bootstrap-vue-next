329cc3bab4eb4331c73a8125a1f11d69cfc92911
Issayah <31498096+VividLemon@users.noreply.github.com>
2022-08-16 11:12:00 -0500

    fix: prop type replaced with prop dark Booleanish

--- packages/bootstrap-vue-3/src/components/BNav/BNavbar.vue
+++ packages/bootstrap-vue-3/src/components/BNav/BNavbar.vue
@@ -15,17 +15,19 @@ interface Props {
   sticky?: 'top' | 'bottom'
   tag?: string
   toggleable?: false | 'sm' | 'md' | 'lg' | 'xl' // Type Omit<Breakpoint, 'xxl'>
-  type?: 'dark'
+  dark?: Booleanish
   variant?: ColorVariant
 }
 
 const props = withDefaults(defineProps<Props>(), {
   print: false,
   tag: 'nav',
+  dark: false,
   toggleable: false, // navbar never collapses
 })
 
 const printBoolean = useBooleanish(toRef(props, 'print'))
+const darkBoolean = useBooleanish(toRef(props, 'dark'))
 
 const computedRole = computed<undefined | 'navigation'>(() =>
   props.tag === 'nav' ? undefined : 'navigation'
@@ -42,7 +44,7 @@ const computedNavbarExpand = computed<undefined | string>(() =>
 const classes = computed(() => ({
   'd-print': printBoolean.value,
   [`sticky-${props.sticky}`]: props.sticky !== undefined,
-  'navbar-dark': props.type !== undefined,
+  'navbar-dark': darkBoolean.value,
   [`bg-${props.variant}`]: props.variant !== undefined,
   [`fixed-${props.fixed}`]: props.fixed !== undefined,
   [`${computedNavbarExpand.value}`]: computedNavbarExpand.value !== undefined,
