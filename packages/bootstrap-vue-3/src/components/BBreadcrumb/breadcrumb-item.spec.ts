0815e3580123e2016d91bbfe1d864502cd4939aa
Issayah <31498096+VividLemon@users.noreply.github.com>
2022-08-26 15:24:50 -0500

    test: use wrapper.unmount

--- packages/bootstrap-vue-3/src/components/BBreadcrumb/breadcrumb-item.spec.ts
+++ packages/bootstrap-vue-3/src/components/BBreadcrumb/breadcrumb-item.spec.ts
@@ -7,6 +7,8 @@ describe('breadcrumb-item', () => {
   it('has static class breadcrumb-item', () => {
     const wrapper = mount(BBreadcrumbItem)
     expect(wrapper.classes()).toContain('breadcrumb-item')
+
+    wrapper.unmount()
   })
 
   it('has class active when prop active', async () => {
@@ -16,6 +18,8 @@ describe('breadcrumb-item', () => {
     expect(wrapper.classes()).toContain('active')
     await wrapper.setProps({active: false})
     expect(wrapper.classes()).not.toContain('active')
+
+    wrapper.unmount()
   })
 
   it('contains a span child when prop active', () => {
@@ -24,12 +28,16 @@ describe('breadcrumb-item', () => {
     })
     const $span = wrapper.find('span')
     expect($span.exists()).toBe(true)
+
+    wrapper.unmount()
   })
 
   it('contains a blink child by default', () => {
     const wrapper = mount(BBreadcrumbItem)
     const $blink = wrapper.findComponent(BLink)
     expect($blink.exists()).toBe(true)
+
+    wrapper.unmount()
   })
 
   it('child contains attr aria-current when prop active', async () => {
@@ -41,6 +49,8 @@ describe('breadcrumb-item', () => {
     await wrapper.setProps({active: false})
     const $blink = wrapper.getComponent(BLink)
     expect($blink.attributes('aria-current')).toBeUndefined()
+
+    wrapper.unmount()
   })
 
   it('emits click event', async () => {
@@ -48,6 +58,8 @@ describe('breadcrumb-item', () => {
     const $blink = wrapper.getComponent(BLink)
     await $blink.trigger('click')
     expect($blink.emitted()).toHaveProperty('click')
+
+    wrapper.unmount()
   })
 
   it('does not emit click event when prop disabled', async () => {
@@ -57,6 +69,8 @@ describe('breadcrumb-item', () => {
     const $blink = wrapper.getComponent(BLink)
     await $blink.trigger('click')
     expect(wrapper.emitted('click')).toBeUndefined()
+
+    wrapper.unmount()
   })
 
   it('emits a MouseEvent when clicked', async () => {
@@ -65,6 +79,8 @@ describe('breadcrumb-item', () => {
     await $blink.trigger('click')
     const $emitted = $blink.emitted('click') ?? []
     expect($emitted[0][0] instanceof MouseEvent).toBe(true)
+
+    wrapper.unmount()
   })
 
   it('renders default slot in child when child is span', () => {
@@ -74,5 +90,7 @@ describe('breadcrumb-item', () => {
     })
     const $span = wrapper.get('span')
     expect($span.text()).toBe('foobar')
+
+    wrapper.unmount()
   })
 })
