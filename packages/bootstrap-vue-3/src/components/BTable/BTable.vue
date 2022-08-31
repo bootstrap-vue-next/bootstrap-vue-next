40b30a01922a437921764a4e5f004b6182a1c184
devhus <devhus01@gmail.com>
2022-08-31 21:35:13 +0200

    fix(BTable): Hotfix to selectAllRows method, Calling the method now emits the selection events correctly.

--- packages/bootstrap-vue-3/src/components/BTable/BTable.vue
+++ packages/bootstrap-vue-3/src/components/BTable/BTable.vue
@@ -330,7 +330,13 @@ const getRowClasses = (item: TableItem) => [
 
 const selectAllRows = () => {
   if (!selectableBoolean.value) return
+  const unselectableItems = selectedItems.value.size > 0 ? Array.from(selectedItems.value) : []
   selectedItems.value = new Set([...computedItems.value])
+  selectedItems.value.forEach((item) => {
+    if (unselectableItems.includes(item)) return
+    emits('rowSelected', item)
+  })
+  emits('selection', Array.from(selectedItems.value))
 }
 
 defineExpose({
