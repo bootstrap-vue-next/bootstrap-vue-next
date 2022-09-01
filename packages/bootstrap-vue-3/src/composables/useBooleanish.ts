206bb557afc9373d5e33e0d4114073f4a74707bd
Issayah <31498096+VividLemon@users.noreply.github.com>
2022-09-01 11:10:29 -0500

    refactor(useBooleanish): remove dead codeand clean

--- packages/bootstrap-vue-3/src/composables/useBooleanish.ts
+++ packages/bootstrap-vue-3/src/composables/useBooleanish.ts
@@ -1,26 +1,13 @@
 import type {Booleanish} from '../types'
-import {computed, ComputedRef, reactive, Ref, toRef} from 'vue'
+import {computed, ComputedRef, Ref} from 'vue'
 import {resolveBooleanish} from '../utils'
-
-// export const useBooleanish = (el: Ref<Booleanish>): ComputedRef<boolean> =>
-//   computed(() => resolveBooleanish(el.value))
-
-// export const useBooleanishUndefined = (
-//   el: Ref<Booleanish | undefined>
-// ): ComputedRef<boolean | undefined> =>
-//   computed(() => (el.value !== undefined ? resolveBooleanish(el.value) : undefined))
 
 function useBooleanish(el: Ref<Booleanish>): ComputedRef<boolean>
 function useBooleanish(el: Ref<Booleanish | undefined>): ComputedRef<boolean | undefined>
 function useBooleanish(
   el: Ref<Booleanish> | Ref<Booleanish | undefined>
 ): ComputedRef<boolean> | ComputedRef<boolean | undefined> {
-  return computed(() => {
-    if (el.value === undefined) {
-      return undefined
-    }
-    return resolveBooleanish(el.value)
-  })
+  return computed(() => (el.value === undefined ? undefined : resolveBooleanish(el.value)))
 }
 
 export default useBooleanish
