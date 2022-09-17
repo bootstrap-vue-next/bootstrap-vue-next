6aed806eb1eb154eb81e5ceee242fb6623fa4971
devhus <devhus01@gmail.com>
2022-09-18 01:44:53 +0200

    patched last commit

--- packages/bootstrap-vue-3/src/components/BTable/BTableContainer.vue
+++ packages/bootstrap-vue-3/src/components/BTable/BTableContainer.vue
@@ -1,21 +1,24 @@
 <template>
-  <div v-if="responsive" :class="responsiveClass">
+  <table v-if="!responsive" :class="classes">
     <slot />
+  </table>
+  <div v-else :class="responsiveClass">
+    <table :class="classes">
+      <slot />
+    </table>
   </div>
-  <template v-else>
-    <slot />
-  </template>
 </template>
 
 <script lang="ts" setup>
 import type {ClassValue} from '../../types'
 
 interface BTableContainerProps {
+  classes?: any
   responsive?: boolean | 'sm' | 'md' | 'lg' | 'xl' | 'xxl'
   responsiveClass?: ClassValue
 }
 
-withDefaults(defineProps<BTableContainerProps>(), {
+const props = withDefaults(defineProps<BTableContainerProps>(), {
   responsive: false,
 })
 </script>
