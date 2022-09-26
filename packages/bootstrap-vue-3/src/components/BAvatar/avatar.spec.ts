0448cfa34b5bbeac3376d00b9b02b4ac2e672a71
Issayah <31498096+VividLemon@users.noreply.github.com>
2022-09-26 15:49:32 -0500

    fix(BAvatar): fix tag to be button and has attr type when prop button and prop buttonType

--- packages/bootstrap-vue-3/src/components/BAvatar/avatar.spec.ts
+++ packages/bootstrap-vue-3/src/components/BAvatar/avatar.spec.ts
@@ -10,18 +10,32 @@ describe('avatar', () => {
     expect(wrapper.classes()).toContain('b-avatar')
   })
 
-  it('tag is default button when prop button is true', () => {
+  it('tag is button when prop button is true', () => {
     const wrapper = mount(BAvatar, {
       props: {button: true},
     })
     expect(wrapper.element.tagName).toBe('BUTTON')
   })
 
-  it('tag is prop buttonType when prop button is true', () => {
+  it('has attr type when prop button to be default button', () => {
     const wrapper = mount(BAvatar, {
-      props: {button: true, buttonType: 'div'},
+      props: {button: true},
     })
-    expect(wrapper.element.tagName).toBe('DIV')
+    expect(wrapper.attributes('type')).toBe('button')
+  })
+
+  it('has attr type when prop button to be prop buttonType', () => {
+    const wrapper = mount(BAvatar, {
+      props: {button: true, buttonType: 'submit'},
+    })
+    expect(wrapper.attributes('type')).toBe('submit')
+  })
+
+  it('does not have attr type when prop buttonType but not prop button', () => {
+    const wrapper = mount(BAvatar, {
+      props: {button: false, buttonType: 'submit'},
+    })
+    expect(wrapper.attributes('type')).toBeUndefined()
   })
 
   it('tag is default span', () => {
