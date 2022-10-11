9a172e381b00d6a53b816a620d8d6f24b64dda00
Issayah <31498096+VividLemon@users.noreply.github.com>
2022-10-11 17:58:12 -0500

    refactor: begin modal bootstrapjs removal

--- packages/bootstrap-vue-3/src/components/BModal.vue
+++ packages/bootstrap-vue-3/src/components/BModal.vue
@@ -1,93 +1,98 @@
 <template>
   <teleport to="body">
-    <div
-      :id="computedId"
-      ref="element"
-      class="modal"
-      :class="modalClasses"
-      tabindex="-1"
-      v-bind="$attrs"
+    <b-transition
+      :no-fade="true"
+      enter-to-class="show"
+      @before-enter="onBeforeEnter"
+      @after-enter="onAfterEnter"
+      @before-leave="onBeforeLeave"
+      @after-leave="onAfterLeave"
     >
-      <div class="modal-dialog" :class="modalDialogClasses">
-        <div
-          v-if="
-            !lazyBoolean ||
-            (lazyBoolean && lazyLoadCompleted) ||
-            (lazyBoolean && modelValueBoolean === true)
-          "
-          class="modal-content"
-          :class="contentClass"
-        >
-          <div v-if="!hideHeaderBoolean" class="modal-header" :class="computedHeaderClasses">
-            <slot name="header">
-              <component :is="titleTag" class="modal-title" :class="computedTitleClasses">
-                <slot name="title">
-                  {{ title }}
+      <div
+        v-show="modelValueBoolean"
+        :id="computedId"
+        ref="element"
+        class="modal"
+        :class="modalClasses"
+        style="display: block"
+        tabindex="-1"
+        v-bind="$attrs"
+        @keyup.esc="onEsc"
+      >
+        <div class="modal-dialog" :class="modalDialogClasses">
+          <div v-if="renderLazy" class="modal-content" :class="contentClass">
+            <div v-if="!hideHeaderBoolean" class="modal-header" :class="headerClasses">
+              <slot name="header">
+                <component :is="titleTag" class="modal-title" :class="titleClasses">
+                  <slot name="title">
+                    {{ title }}
+                  </slot>
+                </component>
+                <template v-if="!hideHeaderCloseBoolean">
+                  <button
+                    v-if="$slots['header-close']"
+                    type="button"
+                    data-bs-dismiss="modal"
+                    @click="hide()"
+                  >
+                    <slot name="header-close" />
+                  </button>
+                  <b-close-button
+                    v-else
+                    :aria-label="headerCloseLabel"
+                    data-bs-dismiss="modal"
+                    :white="headerCloseWhiteBoolean"
+                    @click="hide()"
+                  />
+                </template>
+              </slot>
+            </div>
+            <div class="modal-body" :class="bodyClasses">
+              <slot />
+            </div>
+            <div v-if="!hideFooterBoolean" class="modal-footer" :class="footerClasses">
+              <slot name="footer">
+                <slot name="cancel">
+                  <b-button
+                    v-if="!okOnlyBoolean"
+                    type="button"
+                    class="btn"
+                    :disabled="disableCancel"
+                    :size="buttonSize"
+                    :variant="cancelVariant"
+                    @click="hide(), emit('cancel')"
+                  >
+                    {{ cancelTitle }}
+                  </b-button>
                 </slot>
-              </component>
-              <template v-if="!hideHeaderCloseBoolean">
-                <button
-                  v-if="$slots['header-close']"
-                  type="button"
-                  data-bs-dismiss="modal"
-                  @click="hide()"
-                >
-                  <slot name="header-close" />
-                </button>
-                <b-close-button
-                  v-else
-                  :aria-label="headerCloseLabel"
-                  data-bs-dismiss="modal"
-                  :white="headerCloseWhiteBoolean"
-                  @click="hide()"
-                />
-              </template>
-            </slot>
-          </div>
-          <div class="modal-body" :class="computedBodyClasses">
-            <slot />
-          </div>
-          <div v-if="!hideFooterBoolean" class="modal-footer" :class="computedFooterClasses">
-            <slot name="footer">
-              <b-button
-                v-if="!okOnlyBoolean"
-                type="button"
-                class="btn"
-                :disabled="disableCancel"
-                :size="buttonSize"
-                :variant="cancelVariant"
-                @click="hide(), emit('cancel')"
-              >
-                {{ cancelTitle }}
-              </b-button>
-              <b-button
-                type="button"
-                class="btn"
-                :disabled="disableOk"
-                :size="buttonSize"
-                :variant="okVariant"
-                @click="hide(), emit('ok')"
-              >
-                {{ okTitle }}
-              </b-button>
-            </slot>
+                <slot name="ok">
+                  <b-button
+                    type="button"
+                    class="btn"
+                    :disabled="disableOk"
+                    :size="buttonSize"
+                    :variant="okVariant"
+                    @click="hide(), emit('ok')"
+                  >
+                    {{ okTitle }}
+                  </b-button>
+                </slot>
+              </slot>
+            </div>
           </div>
         </div>
+        <slot v-if="!hideBackdropBoolean" name="backdrop">
+          <div class="modal-backdrop fade show" @click="!noCloseOnBackdropBoolean && hide()" />
+        </slot>
       </div>
-      <div
-        v-if="hideBackdropBoolean === false"
-        class="modal-backdrop fade show"
-        @click.prevent="noCloseOnBackdropBoolean === false && hide()"
-      />
-    </div>
+    </b-transition>
   </teleport>
 </template>
 
 <script setup lang="ts">
 // import type {BModalEmits, BModalProps} from '../types/components'
-import {Modal} from 'bootstrap'
-import {computed, nextTick, onBeforeUnmount, onMounted, ref, toRef, watch} from 'vue'
-import {useBooleanish, useEventListener, useId} from '../composables'
+import {computed, ref, toRef, watch} from 'vue'
+import {useBooleanish, useId} from '../composables'
 import type {Booleanish, ClassValue, ColorVariant, InputSize} from '../types'
 import BButton from './BButton/BButton.vue'
 import BCloseButton from './BButton/BCloseButton.vue'
@@ -170,6 +175,21 @@ const props = withDefaults(defineProps<BModalProps>(), {
   titleTag: 'h5',
 })
 
+interface BModalEmits {
+  (e: 'update:modelValue', value: boolean): void
+  (e: 'show'): void
+  (e: 'shown'): void
+  (e: 'hide'): void
+  (e: 'hidden'): void
+  (e: 'hide-prevented', value: Event): void
+  (e: 'ok'): void
+  (e: 'cancel'): void
+}
+
+const emit = defineEmits<BModalEmits>()
+
+const computedId = useId(toRef(props, 'id'), 'modal')
+
 const busyBoolean = useBooleanish(toRef(props, 'busy'))
 const lazyBoolean = useBooleanish(toRef(props, 'lazy'))
 const cancelDisabledBoolean = useBooleanish(toRef(props, 'cancelDisabled'))
@@ -187,36 +207,20 @@ const noFocusBoolean = useBooleanish(toRef(props, 'noFocus'))
 const okDisabledBoolean = useBooleanish(toRef(props, 'okDisabled'))
 const okOnlyBoolean = useBooleanish(toRef(props, 'okOnly'))
 const scrollableBoolean = useBooleanish(toRef(props, 'scrollable'))
-const showBoolean = useBooleanish(toRef(props, 'show'))
 const titleSrOnlyBoolean = useBooleanish(toRef(props, 'titleSrOnly'))
 
+const element = ref<HTMLElement | null>(null)
 const lazyLoadCompleted = ref(false)
 
-const computedId = useId(toRef(props, 'id'), 'modal')
-
-interface BModalEmits {
-  (e: 'update:modelValue', value: boolean): void
-  (e: 'show', value: Event): void
-  (e: 'shown', value: Event): void
-  (e: 'hide', value: Event): void
-  (e: 'hidden', value: Event): void
-  (e: 'hide-prevented', value: Event): void
-  (e: 'ok'): void
-  (e: 'cancel'): void
-}
-
-const emit = defineEmits<BModalEmits>()
-
-const element = ref<HTMLElement>()
-const instance = ref<Modal>()
 const modalClasses = computed(() => [
+  props.modalClass,
   {
     fade: !noFadeBoolean.value,
-    show: showBoolean.value,
   },
-  props.modalClass,
 ])
+
 const modalDialogClasses = computed(() => [
+  props.dialogClass,
   {
     'modal-fullscreen': props.fullscreen === true,
     [`modal-fullscreen-${props.fullscreen}-down`]: typeof props.fullscreen === 'string',
@@ -224,142 +228,68 @@ const modalDialogClasses = computed(() => [
     'modal-dialog-centered': centeredBoolean.value,
     'modal-dialog-scrollable': scrollableBoolean.value,
   },
-  props.dialogClass,
 ])
 
-const computedBodyClasses = computed(() => [
-  {
-    [`bg-${props.bodyBgVariant}`]: props.bodyBgVariant,
-    [`text-${props.bodyTextVariant}`]: props.bodyTextVariant,
-  },
+const bodyClasses = computed(() => [
   props.bodyClass,
+  {
+    [`bg-${props.bodyBgVariant}`]: props.bodyBgVariant !== undefined,
+    [`text-${props.bodyTextVariant}`]: props.bodyTextVariant !== undefined,
+  },
 ])
 
-const computedHeaderClasses = computed(() => [
-  {
-    [`bg-${props.headerBgVariant}`]: props.headerBgVariant,
-    [`border-${props.headerBorderVariant}`]: props.headerBorderVariant,
-    [`text-${props.headerTextVariant}`]: props.headerTextVariant,
-  },
+const headerClasses = computed(() => [
   props.headerClass,
-])
-
-const computedFooterClasses = computed(() => [
   {
-    [`bg-${props.footerBgVariant}`]: props.footerBgVariant,
-    [`border-${props.footerBorderVariant}`]: props.footerBorderVariant,
-    [`text-${props.footerTextVariant}`]: props.footerTextVariant,
+    [`bg-${props.headerBgVariant}`]: props.headerBgVariant !== undefined,
+    [`border-${props.headerBorderVariant}`]: props.headerBorderVariant !== undefined,
+    [`text-${props.headerTextVariant}`]: props.headerTextVariant !== undefined,
   },
-  props.footerClass,
 ])
 
-const computedTitleClasses = computed(() => [
+const footerClasses = computed(() => [
+  props.footerClass,
+  {
+    [`bg-${props.footerBgVariant}`]: props.footerBgVariant !== undefined,
+    [`border-${props.footerBorderVariant}`]: props.footerBorderVariant !== undefined,
+    [`text-${props.footerTextVariant}`]: props.footerTextVariant !== undefined,
+  },
+])
+
+const titleClasses = computed(() => [
+  props.titleClass,
   {
     ['visually-hidden']: titleSrOnlyBoolean.value,
   },
-  props.titleClass,
 ])
-
 const disableCancel = computed<boolean>(() => cancelDisabledBoolean.value || busyBoolean.value)
 const disableOk = computed<boolean>(() => okDisabledBoolean.value || busyBoolean.value)
-
-useEventListener(element, 'shown.bs.modal', (e) => modalShowed(e))
-useEventListener(element, 'hidden.bs.modal', (e) => modalHided(e))
-useEventListener(element, 'show.bs.modal', (e) => modalShow(e))
-useEventListener(element, 'hide.bs.modal', (e) => modalHide(e))
-
-const modalShowed = (e: Event) => {
-  emit('shown', e)
-
-  if (lazyBoolean.value === true) lazyLoadCompleted.value = true
-  if (modelValueBoolean.value === false) emit('update:modelValue', true)
-  ;(e.target as HTMLElement).focus()
-}
-
-const modalHided = (e: Event) => {
-  emit('hidden', e)
-
-  if (lazyBoolean.value === true) lazyLoadCompleted.value = false
-  if (modelValueBoolean.value === true) emit('update:modelValue', false)
-
-  const parentModal = document.querySelector('.modal')
-  if (parentModal) {
-    ;(parentModal as HTMLElement).focus()
-  }
-}
-
-const modalShow = (e: Event) => {
-  emit('show', e)
-}
-
-const modalHide = (e: Event) => {
-  emit('hide', e)
-}
-
-const show = () => {
-  if (modelValueBoolean.value) emit('update:modelValue', true)
-  getInstance().show()
-}
-
-const hide = () => {
-  if (modelValueBoolean.value) emit('update:modelValue', false)
-  getInstance().hide()
-}
-
-const getInstance = (): Modal => {
-  if (instance.value !== undefined) return instance.value
-  instance.value = new Modal(element.value as HTMLElement, {
-    backdrop: false,
-    // backdrop: hideBackdropBoolean.value
-    //   ? false
-    //   : noCloseOnBackdropBoolean.value
-    //     ? 'static'
-    //     : !hideBackdropBoolean.value,
-    keyboard: !noCloseOnEscBoolean.value,
-    focus: !noFocusBoolean.value,
-  })
-  return instance.value
-}
-
-onMounted(() => {
-  if (modelValueBoolean.value) {
-    getInstance().show()
-  }
-})
-
-onBeforeUnmount((): void => {
-  instance.value?.dispose()
-  instance.value = undefined
-})
-
-watch(
-  () => props.noCloseOnBackdrop,
-  (newValue) => {
-    ;(getInstance() as unknown as {_config: Modal.Options})._config.backdrop = props.hideBackdrop
-      ? false
-      : newValue
-      ? 'static'
-      : !props.hideBackdrop
-  }
+const renderLazy = computed(
+  () =>
+    !lazyBoolean.value ||
+    (lazyBoolean.value && lazyLoadCompleted.value) ||
+    (lazyBoolean.value && modelValueBoolean.value)
 )
 
-watch(
-  () => props.noCloseOnEsc,
-  (newValue) => {
-    ;(getInstance() as unknown as {_config: Modal.Options})._config.keyboard = !newValue
+const hide = () => emit('update:modelValue', false)
+
+const onEsc = (e: KeyboardEvent) => {
+  if (!noCloseOnEscBoolean.value && modelValueBoolean.value && e.key === 'Escape') {
+    hide()
   }
-)
+}
+
+const onBeforeEnter = () => emit('show')
+const onAfterEnter = () => emit('shown')
+const onBeforeLeave = () => emit('hide')
+const onAfterLeave = () => emit('hidden')
 
 watch(
   () => modelValueBoolean.value,
-  (value) => {
-    nextTick(() => {
-      if (value) {
-        show()
-      } else {
-        hide()
-      }
-    })
+  (newValue) => {
+    if (newValue === true && !noFocusBoolean.value && element.value !== null) {
+      element.value.focus()
+    }
   }
 )
 </script>
