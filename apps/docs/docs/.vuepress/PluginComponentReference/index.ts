3caaeca3583a94a171ef73f5eaea5e0e891ccb01
Daniel Harrington <danielh@rotair.com>
2022-10-14 15:50:41 -0400

    docs(Type Introspection): Generate temp file to pull type info from

--- apps/docs/docs/.vuepress/PluginComponentReference/index.ts
+++ apps/docs/docs/.vuepress/PluginComponentReference/index.ts
@@ -1,18 +1,24 @@
-import {fs} from '@vuepress/utils'
+import {fs,info, error} from '@vuepress/utils'
 // import type {Page, Plugin, App, PageData} from '@vuepress/core'
 import type { Plugin, App, PageData, Page } from 'vuepress'
 import {path} from '@vuepress/utils'
+import {  getComponentsFromDir,  RegisterComponentsPluginOptions } from '@vuepress/plugin-register-components'
+import {createComponentMetaChecker, MetaCheckerOptions, createComponentMetaCheckerByJsonConfig} from 'vue-component-meta'
+
+
+export type ComponentPropsOptions  = Omit<RegisterComponentsPluginOptions, 'components'> & 
+{metaCheckerOptions? : MetaCheckerOptions, rootPath?: string } 
 
 export interface ComponentReferenceOptions {
-  baseDir: string
-}
+  baseDir?: string,
+  extendsProps? : (app : App, page : Page, componentReference : any | null) => any,
+} 
 
-function resolvePageComponentReferencePath(baseDir: string, page, app: App): {filePath: string} {
+
+function resolvePageComponentReferencePath(baseDir: string, page, app: App): {filePath: string } {
   const filePath = path.resolve(app.dir.source(), baseDir, path.addExt(page.slug, '.json'))
   if (!filePath) {
-    return {
-      filePath: null,
-    }
+    throw new Error('Cannot resolve Component Reference Path')
   }
 
   return {
@@ -20,17 +26,91 @@ function resolvePageComponentReferencePath(baseDir: string, page, app: App): {fi
   }
 }
 
-export const componentReference: Plugin<ComponentReferenceOptions> = ({
-  baseDir = 'component-references',
-}) => ({
+export const componentReference  = ({
+  baseDir = "component-references",
+  extendsProps,
+   
+} : ComponentReferenceOptions = {baseDir : "component-references"
+
+}, {
+  metaCheckerOptions = undefined,
+  componentsDir = null,
+  rootPath,
+  componentsPatterns = ['**/*.vue', '!App.vue'],
+  getComponentName = (filename) =>
+    path.basename(filename, ".vue"),
+} : ComponentPropsOptions = {}) : Plugin => ({
   name: 'plugin-object',
   clientConfigFile: path.resolve(__dirname, './client.ts'),
+  onPrepared : async (app) => {
+    info("ComponentProps Gathering Prop Defintions")
+    
+    if(componentsDir){
+
+    let componentMetaMap = {}
+    if(fs.pathExistsSync(app.dir.temp('componentMeta.json'))){
+        info("Skip Checker")  
+        return
+    }
+    let componentsMap =  await getComponentsFromDir({componentsDir, componentsPatterns, getComponentName})
+    // Our component directory and tsconfig must share a simliar path
+    let checker = createComponentMetaCheckerByJsonConfig(path.resolve(componentsDir),
+      {
+        "include": [
+          "**/*",
+        ],
+      },
+      metaCheckerOptions)
+      
+    info("Established Checker")
+   
+      async function meta(component, filepath){
+        try{
+               let {props, events, exposed ,slots} =  checker.getComponentMeta(path.resolve(filepath))
+               componentMetaMap[component] = {props: props, events: events ,  exposed: exposed,  slots: slots}
+             }catch(erroy :any){
+              error(`Parsing Failed`, error)
+             }
+      }
+
+     await  Promise.all(Object.entries(componentsMap).map((attributes)=> meta(attributes[0], attributes[1])))
+    
+    app.writeTemp("componentMeta.json", JSON.stringify(componentMetaMap))
+    }
+  }, 
   extendsPage(page, app: App) {
     const {filePath} = resolvePageComponentReferencePath(baseDir, page, app)
     let componentReference = null
     if (fs.existsSync(filePath)) {
-      componentReference = require(filePath)
+      componentReference = require(filePath) 
+      // see if componentMeta exists to augment existing user defined types
+      if (fs.existsSync( path.resolve(path.join(app.dir.temp(), 'componentMeta.json')))) {
+         let componentMeta = require( path.resolve(path.join(app.dir.temp(), 'componentMeta')))
+
+         if(componentReference){
+         componentReference.meta.components.forEach(({component, props}, index) => {
+
+          if (props && componentMeta[component]){
+            props.forEach((prop, propindex)=>{
+              let propDefiniton = componentMeta[component].props.find((propMeta) => {
+                if (propMeta.name === prop.prop){
+                  return propMeta
+                }
+              })
+
+              if (propDefiniton){
+                prop.type = propDefiniton.type
+              }
+            })
+          }
+        });
+      }}
     }
+
+    if (extendsProps){
+      extendsProps(app, page, componentReference)
+    }
+
     page.data.componentReference = componentReference
   },
 })
