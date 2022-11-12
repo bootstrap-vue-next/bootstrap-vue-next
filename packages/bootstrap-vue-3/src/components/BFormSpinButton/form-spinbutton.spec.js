531cd528c1f7be176f92bf0e92a159a9e5024890
Daniel Harrington <danielh@rotair.com>
2022-11-12 01:57:05 -0500

    feat(SpinButton): Finish Porting Over Tests

--- packages/bootstrap-vue-3/src/components/BFormSpinButton/form-spinbutton.spec.js
+++ packages/bootstrap-vue-3/src/components/BFormSpinButton/form-spinbutton.spec.js
@@ -486,10 +486,11 @@ describe('form-spin-button', () => {
     const wrapper = mount(BFormSpinbutton, {
       attachTo: document.body,
       props: {
-        min: 1,
-        max: 100,
-        step: 1,
-        modelValue: 1,
+        'min': 1,
+        'max': 100,
+        'step': 1,
+        'modelValue': 1,
+        'onUpdate:modelValue': (e) => wrapper.setProps({modelValue: e}),
       },
     })
 
@@ -497,170 +498,128 @@ describe('form-spin-button', () => {
 
     const $output = wrapper.find('output')
 
-    // We need to specify keyCode as using event string will default with
-    // hex code
-    await wrapper.trigger('keydown', {keyCode: 'ArrowDown'})
-    await wrapper.trigger('keyup', {keyCode: 'ArrowDown'})
-    await wrapper.trigger('keydown', {keyCode: 'ArrowUp'})
-    // await wrapper.trigger('keyup', {keyCode: 'ArrowUp'})
+    expect($output.exists()).toBe(true)
+    expect($output.attributes('role')).toEqual('spinbutton')
+    expect($output.attributes('tabindex')).toEqual('0')
+    expect($output.attributes('aria-live')).toEqual('off')
+    expect($output.attributes('aria-valuemin')).toEqual('1')
+    expect($output.attributes('aria-valuemax')).toEqual('100')
+    expect($output.attributes('aria-valuenow')).toEqual('1')
+    expect($output.attributes('aria-valuetext')).toEqual('1')
 
-    //TODO this will need to catch the modelValue info....
-    // expect($output.attributes('aria-valuemin')).toEqual('1')
-    // expect($output.attributes('aria-valuemax')).toEqual('100')
-    // expect($output.attributes('aria-valuenow')).toEqual('2')
-    // expect($output.attributes('aria-valuetext')).toEqual('2')
-
-    // const $output = wrapper.find('output')
-    // expect($output.exists()).toBe(true)
-    // expect($output.attributes('role')).toEqual('spinbutton')
-    // expect($output.attributes('tabindex')).toEqual('0')
-    // expect($output.attributes('aria-live')).toEqual('off')
-    // expect($output.attributes('aria-valuemin')).toEqual('1')
-    // expect($output.attributes('aria-valuemax')).toEqual('100')
-    // expect($output.attributes('aria-valuenow')).toEqual('1')
-    // expect($output.attributes('aria-valuetext')).toEqual('1')
     // expect(wrapper.emitted('input')).toBeUndefined()
-    // expect(wrapper.emitted('change')).toBeUndefined()
+    expect(wrapper.emitted('change')).toBeUndefined()
 
-    // await wrapper.trigger('keydown', {keyCode: 'ArrowDown'})
-    // await wrapper.trigger('keyup', {keyCode: 'ArrowDown'})
-    // await wrapper.trigger('keydown', {keyCode: 'ArrowUp'})
-    // await wrapper.trigger('keyup', {keyCode: 'ArrowUp'})
+    await wrapper.trigger('keydown', {keyCode: 'ArrowUp'})
 
-    // expect($output.attributes('aria-valuenow')).toEqual('2')
-    // expect($output.attributes('aria-valuetext')).toEqual('2')
+    expect($output.attributes('aria-valuenow')).toEqual('2')
+    expect($output.attributes('aria-valuetext')).toEqual('2')
     // expect(wrapper.emitted('input')).toBeDefined()
     // expect(wrapper.emitted('input').length).toBe(1)
-    // expect(wrapper.emitted('change')).toBeUndefined()
+    expect(wrapper.emitted('change')).toBeUndefined()
 
-    // // Advance past delay time
-    // jest.runOnlyPendingTimers()
-    // await waitNT(wrapper.vm)
-    // await waitRAF()
-    // // Now we have to wait for interval to happen
-    // expect($output.attributes('aria-valuenow')).toEqual('2')
-    // expect($output.attributes('aria-valuetext')).toEqual('2')
-    // expect(wrapper.emitted('input').length).toBe(1)
-    // expect(wrapper.emitted('change')).toBeUndefined()
+    await vi.runOnlyPendingTimers()
 
-    // // Advance past interval time
-    // // Repeat #1
-    // jest.runOnlyPendingTimers()
-    // await waitNT(wrapper.vm)
-    // await waitRAF()
-    // expect($output.attributes('aria-valuenow')).toEqual('3')
-    // expect($output.attributes('aria-valuetext')).toEqual('3')
-    // expect(wrapper.emitted('input').length).toBe(2)
-    // expect(wrapper.emitted('change')).toBeUndefined()
+    expect($output.attributes('aria-valuenow')).toEqual('2')
+    expect($output.attributes('aria-valuetext')).toEqual('2')
+    //expect(wrapper.emitted('input').length).toBe(1)
+    expect(wrapper.emitted('change')).toBeUndefined()
 
-    // // Repeat #2
-    // jest.runOnlyPendingTimers()
-    // await waitNT(wrapper.vm)
-    // await waitRAF()
-    // expect($output.attributes('aria-valuenow')).toEqual('4')
-    // expect($output.attributes('aria-valuetext')).toEqual('4')
-    // expect(wrapper.emitted('input').length).toBe(3)
-    // expect(wrapper.emitted('change')).toBeUndefined()
+    await vi.runOnlyPendingTimers()
 
-    // // Repeat #3
-    // jest.runOnlyPendingTimers()
-    // await waitNT(wrapper.vm)
-    // await waitRAF()
-    // expect($output.attributes('aria-valuenow')).toEqual('5')
-    // expect($output.attributes('aria-valuetext')).toEqual('5')
-    // expect(wrapper.emitted('input').length).toBe(4)
-    // expect(wrapper.emitted('change')).toBeUndefined()
+    // Advance past interval time
+    // Repeat #1
+    expect($output.attributes('aria-valuenow')).toEqual('3')
+    expect($output.attributes('aria-valuetext')).toEqual('3')
+    //expect(wrapper.emitted('input').length).toBe(2)
+    expect(wrapper.emitted('change')).toBeUndefined()
+    // Repeat #2
 
-    // // Repeat #4
-    // jest.runOnlyPendingTimers()
-    // await waitNT(wrapper.vm)
-    // await waitRAF()
-    // expect($output.attributes('aria-valuenow')).toEqual('6')
-    // expect($output.attributes('aria-valuetext')).toEqual('6')
-    // expect(wrapper.emitted('input').length).toBe(5)
-    // expect(wrapper.emitted('change')).toBeUndefined()
+    await vi.runOnlyPendingTimers()
+    expect($output.attributes('aria-valuenow')).toEqual('4')
+    expect($output.attributes('aria-valuetext')).toEqual('4')
+    //expect(wrapper.emitted('input').length).toBe(3)
+    expect(wrapper.emitted('change')).toBeUndefined()
+
+    // Repeat #3
+    await vi.runOnlyPendingTimers()
+    expect($output.attributes('aria-valuenow')).toEqual('5')
+    expect($output.attributes('aria-valuetext')).toEqual('5')
+    //expect(wrapper.emitted('input').length).toBe(4)
+    expect(wrapper.emitted('change')).toBeUndefined()
+
+    // Repeat #4
+    await vi.runOnlyPendingTimers()
+    expect($output.attributes('aria-valuenow')).toEqual('6')
+    expect($output.attributes('aria-valuetext')).toEqual('6')
+    //expect(wrapper.emitted('input').length).toBe(5)
+    expect(wrapper.emitted('change')).toBeUndefined()
 
     // // Repeat #5
-    // jest.runOnlyPendingTimers()
-    // await waitNT(wrapper.vm)
-    // await waitRAF()
-    // expect($output.attributes('aria-valuenow')).toEqual('7')
-    // expect($output.attributes('aria-valuetext')).toEqual('7')
+    await vi.runOnlyPendingTimers()
+    expect($output.attributes('aria-valuenow')).toEqual('7')
+    expect($output.attributes('aria-valuetext')).toEqual('7')
     // expect(wrapper.emitted('input').length).toBe(6)
-    // expect(wrapper.emitted('change')).toBeUndefined()
+    expect(wrapper.emitted('change')).toBeUndefined()
 
-    // // Repeat #6
-    // jest.runOnlyPendingTimers()
-    // await waitNT(wrapper.vm)
-    // await waitRAF()
-    // expect($output.attributes('aria-valuenow')).toEqual('8')
-    // expect($output.attributes('aria-valuetext')).toEqual('8')
-    // expect(wrapper.emitted('input').length).toBe(7)
-    // expect(wrapper.emitted('change')).toBeUndefined()
+    // Repeat #6
+    await vi.runOnlyPendingTimers()
+    expect($output.attributes('aria-valuenow')).toEqual('8')
+    expect($output.attributes('aria-valuetext')).toEqual('8')
+    //expect(wrapper.emitted('input').length).toBe(7)
+    expect(wrapper.emitted('change')).toBeUndefined()
 
-    // // Repeat #7
-    // jest.runOnlyPendingTimers()
-    // await waitNT(wrapper.vm)
-    // await waitRAF()
-    // expect($output.attributes('aria-valuenow')).toEqual('9')
-    // expect($output.attributes('aria-valuetext')).toEqual('9')
-    // expect(wrapper.emitted('input').length).toBe(8)
-    // expect(wrapper.emitted('change')).toBeUndefined()
+    // Repeat #7
+    await vi.runOnlyPendingTimers()
+    expect($output.attributes('aria-valuenow')).toEqual('9')
+    expect($output.attributes('aria-valuetext')).toEqual('9')
+    //expect(wrapper.emitted('input').length).toBe(8)
+    expect(wrapper.emitted('change')).toBeUndefined()
 
-    // // Repeat #8
-    // jest.runOnlyPendingTimers()
-    // await waitNT(wrapper.vm)
-    // await waitRAF()
-    // expect($output.attributes('aria-valuenow')).toEqual('10')
-    // expect($output.attributes('aria-valuetext')).toEqual('10')
-    // expect(wrapper.emitted('input').length).toBe(9)
-    // expect(wrapper.emitted('change')).toBeUndefined()
+    // Repeat #8
+    await vi.runOnlyPendingTimers()
+    expect($output.attributes('aria-valuenow')).toEqual('10')
+    expect($output.attributes('aria-valuetext')).toEqual('10')
+    //expect(wrapper.emitted('input').length).toBe(9)
+    expect(wrapper.emitted('change')).toBeUndefined()
 
-    // // Repeat #9
-    // jest.runOnlyPendingTimers()
-    // await waitNT(wrapper.vm)
-    // await waitRAF()
-    // expect($output.attributes('aria-valuenow')).toEqual('11')
-    // expect($output.attributes('aria-valuetext')).toEqual('11')
-    // expect(wrapper.emitted('input').length).toBe(10)
-    // expect(wrapper.emitted('change')).toBeUndefined()
+    // Repeat #9
+    await vi.runOnlyPendingTimers()
+    expect($output.attributes('aria-valuenow')).toEqual('11')
+    expect($output.attributes('aria-valuetext')).toEqual('11')
+    //expect(wrapper.emitted('input').length).toBe(10)
+    expect(wrapper.emitted('change')).toBeUndefined()
 
-    // // Repeat #10
-    // jest.runOnlyPendingTimers()
-    // await waitNT(wrapper.vm)
-    // await waitRAF()
-    // expect($output.attributes('aria-valuenow')).toEqual('12')
-    // expect($output.attributes('aria-valuetext')).toEqual('12')
+    // Repeat #10
+    await vi.runOnlyPendingTimers()
+    expect($output.attributes('aria-valuenow')).toEqual('12')
+    expect($output.attributes('aria-valuetext')).toEqual('12')
     // expect(wrapper.emitted('input').length).toBe(11)
-    // expect(wrapper.emitted('change')).toBeUndefined()
+    expect(wrapper.emitted('change')).toBeUndefined()
 
-    // // Repeat #11 - Multiplier kicks in
-    // jest.runOnlyPendingTimers()
-    // await waitNT(wrapper.vm)
-    // await waitRAF()
-    // // Note even though step is `4`, it jumps to `17` (not `16`) as it rounds to the
-    // // nearest multiple of step (relative to the min value, which is `1` in this test)
-    // // If min was set to `0`, then this would have been `16`
-    // expect($output.attributes('aria-valuenow')).toEqual('17')
-    // expect($output.attributes('aria-valuetext')).toEqual('17')
-    // expect(wrapper.emitted('input').length).toBe(12)
-    // expect(wrapper.emitted('change')).toBeUndefined()
+    // Repeat #11 - Multiplier kicks in
+    await vi.runOnlyPendingTimers()
+    // Note even though step is `4`, it jumps to `17` (not `16`) as it rounds to the
+    // nearest multiple of step (relative to the min value, which is `1` in this test)
+    // If min was set to `0`, then this would have been `16`
+    expect($output.attributes('aria-valuenow')).toEqual('17')
+    expect($output.attributes('aria-valuetext')).toEqual('17')
+    //expect(wrapper.emitted('input').length).toBe(12)
+    expect(wrapper.emitted('change')).toBeUndefined()
 
-    // // Repeat #12
-    // jest.runOnlyPendingTimers()
-    // await waitNT(wrapper.vm)
-    // await waitRAF()
-    // expect($output.attributes('aria-valuenow')).toEqual('21')
-    // expect($output.attributes('aria-valuetext')).toEqual('21')
+    // Repeat #12
+    await vi.runOnlyPendingTimers()
+    expect($output.attributes('aria-valuenow')).toEqual('21')
+    expect($output.attributes('aria-valuetext')).toEqual('21')
     // expect(wrapper.emitted('input').length).toBe(13)
-    // expect(wrapper.emitted('change')).toBeUndefined()
+    expect(wrapper.emitted('change')).toBeUndefined()
 
-    // // Un-press key
-    // await wrapper.trigger('keyup.up')
-    // expect($output.attributes('aria-valuenow')).toEqual('21')
-    // expect($output.attributes('aria-valuetext')).toEqual('21')
+    // Un-press key
+    await wrapper.trigger('keyup', {keyCode: 'ArrowUp'})
+    expect($output.attributes('aria-valuenow')).toEqual('21')
+    expect($output.attributes('aria-valuetext')).toEqual('21')
     // expect(wrapper.emitted('input').length).toBe(13)
-    // expect(wrapper.emitted('change')).toBeDefined()
-    // expect(wrapper.emitted('change').length).toBe(1)
+    expect(wrapper.emitted('change')).toBeDefined()
+    expect(wrapper.emitted('change').length).toBe(1)
   })
 })
