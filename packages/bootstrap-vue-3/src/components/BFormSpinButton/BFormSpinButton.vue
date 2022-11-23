52764425cc79237aab7b62c11e9d5b33b796a740
Daniel Harrington <danielh@rotair.com>
2022-11-23 09:40:25 -0500

    chore: replace characters with SVG

--- packages/bootstrap-vue-3/src/components/BFormSpinButton/BFormSpinButton.vue
+++ packages/bootstrap-vue-3/src/components/BFormSpinButton/BFormSpinButton.vue
@@ -436,7 +436,20 @@ export default defineComponent({
       const $increment = makeButton(
         stepUp,
         props.labelIncrement,
-        '+',
+        h(
+          'svg',
+          {
+            xmlns: 'http://www.w3.org/2000/svg',
+            width: '16',
+            height: '16',
+            fill: 'currentColor',
+            class: 'bi bi-plus',
+            viewBox: '0 0 16 16',
+          },
+          h('path', {
+            d: 'M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z',
+          })
+        ),
         'inc',
         'ArrowUp',
         false,
@@ -445,7 +458,18 @@ export default defineComponent({
       const $decrement = makeButton(
         stepDown,
         props.labelDecrement,
-        '-',
+        h(
+          'svg',
+          {
+            xmlns: 'http://www.w3.org/2000/svg',
+            width: '16',
+            height: '16',
+            fill: 'currentColor',
+            class: 'bi bi-dash',
+            viewBox: '0 0 16 16',
+          },
+          h('path', {d: 'M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z'})
+        ),
         'dec',
         'ArrowDown',
         false,
