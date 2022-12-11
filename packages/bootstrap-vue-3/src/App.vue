3beb958a87d068856f60357560779a10d24d647b
issayah <kwiksilver344@gmail.com>
2022-12-11 13:55:57 -0600

    feat: begin creation of a countdown timer

--- packages/bootstrap-vue-3/src/App.vue
+++ packages/bootstrap-vue-3/src/App.vue
@@ -1,12 +1,26 @@
 <template>
   <b-container>
     <b-row>
-      <b-col> Hello world </b-col>
+      <b-col>{{ isPending }}</b-col>
+      <b-col>{{ value }}</b-col>
+      <b-col>{{ a }}</b-col>
+      <b-col><b-button @click="a = a + 1000">add a second</b-button></b-col>
+      <b-col><b-button @click="pause()">click me pause</b-button></b-col>
+      <b-col><b-button @click="resume()">click me resume</b-button></b-col>
+      <b-col><b-button @click="restart()">click me restart</b-button></b-col>
+      <b-col><b-button @click="stop()">click me stop</b-button></b-col>
     </b-row>
   </b-container>
 </template>
 
 <script setup lang="ts">
+import {ref} from 'vue'
+import useCountdown from './composables/useCountdown'
+
+const a = ref(5400)
+
+const {isPending, stop, value, pause, restart, resume} = useCountdown(a)
+
 // You can use this file as a development spot to test your changes
 // Please do not commit this file
 </script>
---
+++ packages/bootstrap-vue-3/src/composables/useCountdown.ts
@@ -0,0 +1,112 @@
+import {type MaybeComputedRef, resolveUnref, useTimeoutFn, UseTimeoutFnOptions} from '@vueuse/core'
+import {computed, type ComputedRef, ref, type Ref, toRef, watch} from 'vue'
+
+type VoidFn = () => void
+
+interface CountdownReturn {
+  isPending: Ref<boolean>
+  restart: VoidFn
+  stop: VoidFn
+  resume: VoidFn
+  pause: VoidFn
+  value: ComputedRef<number>
+}
+
+/**
+ * In order to make the timeout reactive if input length changes, the timer restarts.
+ * Meaning if you change length from 5 seconds to 2 seconds, it will lower to 2 seconds.
+ * If you change from 5 seconds to 10 seconds, it will restart the timer to 10 seconds.
+ */
+export default (
+  length: MaybeComputedRef<number>,
+  opts: UseTimeoutFnOptions = {immediate: false}
+): CountdownReturn => {
+  const timePassed = ref(0)
+
+  const pausedAtTime = ref<null | number>(null)
+
+  const resolvedLength = computed(() => resolveUnref(length))
+
+  /**
+   * When starting the timer, check if there is a paused set-point.
+   * If there is, that is how long the timer should last. If not, use arg time
+   */
+  const computedLength = computed(() => pausedAtTime.value ?? resolvedLength.value)
+
+  let interval: ReturnType<typeof setInterval> = setInterval(() => {
+    timePassed.value = timePassed.value + 1000
+  }, 1000)
+
+  const resetTimePassed = () => {
+    timePassed.value = 0
+  }
+
+  const resetPauseTimer = () => {
+    pausedAtTime.value = null
+  }
+
+  const resetInterval = () => {
+    clearInterval(interval)
+    interval = setInterval(() => {
+      timePassed.value = timePassed.value + 1000
+    }, 1000)
+  }
+
+  const {isPending, start, stop} = useTimeoutFn(
+    () => clearInterval(interval),
+    toRef(computedLength, 'value'),
+    {immediate: opts.immediate}
+  )
+
+  const myResume = () => {
+    // If there is no pause set-point OR if there is not a FULL SECOND to pass, do not resume
+    // ie if length is 5400ms, and timePassed is at 5s, remaining is 400, check 400/1000 for if there is a full second to pass
+    if (pausedAtTime.value === null || computedLength.value / 1000 < 1) return
+    resetInterval()
+    start()
+    resetPauseTimer()
+  }
+
+  const myPause = () => {
+    // Check if the timePassed is passed the initial length of the timer. Do not create a pause set-point if the timer is done
+    // OR if the timer has not started, do not create a pause set-point
+    if (timePassed.value >= resolvedLength.value || timePassed.value === 0) return
+    pausedAtTime.value = resolvedLength.value - timePassed.value
+    clearInterval(interval)
+    stop()
+  }
+
+  const myRestart = () => {
+    resetTimePassed()
+    resetPauseTimer()
+    resetInterval()
+    start()
+  }
+
+  const myStop = () => {
+    resetTimePassed()
+    resetPauseTimer()
+    clearInterval(interval)
+    stop()
+  }
+
+  const computedVal = computed(() =>
+    isPending.value ? Math.round((computedLength.value - timePassed.value) / 1000) : 0
+  )
+
+  /**
+   * If arg length is changed, reset timer
+   */
+  watch(resolvedLength, () => {
+    myRestart()
+  })
+
+  return {
+    isPending,
+    restart: myRestart,
+    stop: myStop,
+    pause: myPause,
+    resume: myResume,
+    value: computedVal,
+  }
+}
