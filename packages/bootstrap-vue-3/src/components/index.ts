f9defa2d7eaf39086dd818b7360e6c5e2f1880f1
issayah <kwiksilver344@gmail.com>
2022-12-23 16:51:16 -0600

    chore: cleanup imports and exports
    chore: add some items to gitignore

--- packages/bootstrap-vue-3/src/components/index.ts
+++ packages/bootstrap-vue-3/src/components/index.ts
@@ -1,209 +1,103 @@
-import BAccordion from './BAccordion/BAccordion.vue'
-import BAccordionItem from './BAccordion/BAccordionItem.vue'
-import BAlert from './BAlert/BAlert.vue'
-import BAvatar from './BAvatar/BAvatar.vue'
-import BAvatarGroup from './BAvatar/BAvatarGroup.vue'
-import BBadge from './BBadge/BBadge.vue'
-import BBreadcrumb from './BBreadcrumb/BBreadcrumb.vue'
-import BBreadcrumbItem from './BBreadcrumb/BBreadcrumbItem.vue'
-import BButton from './BButton/BButton.vue'
-import BButtonGroup from './BButton/BButtonGroup.vue'
-import BButtonToolbar from './BButton/BButtonToolbar.vue'
-import BCloseButton from './BButton/BCloseButton.vue'
-import BCard from './BCard/BCard.vue'
-import BCardBody from './BCard/BCardBody.vue'
-import BCardFooter from './BCard/BCardFooter.vue'
-import BCardGroup from './BCard/BCardGroup.vue'
-import BCardHeader from './BCard/BCardHeader.vue'
-import BCardImg from './BCard/BCardImg.vue'
-import BCardSubtitle from './BCard/BCardSubtitle.vue'
-import BCardText from './BCard/BCardText.vue'
-import BCardTitle from './BCard/BCardTitle.vue'
-import BCarousel from './BCarousel/BCarousel.vue'
-import BCarouselSlide from './BCarousel/BCarouselSlide.vue'
-import BCol from './BCol.vue'
-import BCollapse from './BCollapse.vue'
-import BContainer from './BContainer.vue'
-import BDropdown from './BDropdown/BDropdown.vue'
-import BDropdownDivider from './BDropdown/BDropdownDivider.vue'
-import BDropdownForm from './BDropdown/BDropdownForm.vue'
-import BDropdownGroup from './BDropdown/BDropdownGroup.vue'
-import BDropdownHeader from './BDropdown/BDropdownHeader.vue'
-import BDropdownItem from './BDropdown/BDropdownItem.vue'
-import BDropdownItemButton from './BDropdown/BDropdownItemButton.vue'
-import BDropdownText from './BDropdown/BDropdownText.vue'
-import BForm from './BForm/BForm.vue'
-import BFormFloatingLabel from './BForm/BFormFloatingLabel.vue'
-import BFormInvalidFeedback from './BForm/BFormInvalidFeedback.vue'
-import BFormRow from './BForm/BFormRow.vue'
-import BFormText from './BForm/BFormText.vue'
-import BFormValidFeedback from './BForm/BFormValidFeedback.vue'
-import BFormCheckbox from './BFormCheckbox/BFormCheckbox.vue'
-import BFormCheckboxGroup from './BFormCheckbox/BFormCheckboxGroup.vue'
-import BFormGroup from './BFormGroup/BFormGroup.vue'
-import BFormInput from './BFormInput/BFormInput.vue'
-import BFormRadio from './BFormRadio/BFormRadio.vue'
-import BFormRadioGroup from './BFormRadio/BFormRadioGroup.vue'
-import BFormSelect from './BFormSelect/BFormSelect.vue'
-import BFormSelectOption from './BFormSelect/BFormSelectOption.vue'
-import BFormSelectOptionGroup from './BFormSelect/BFormSelectOptionGroup.vue'
-import BFormTag from './BFormTags/BFormTag.vue'
-import BFormTags from './BFormTags/BFormTags.vue'
-import BFormTextarea from './BFormTextarea/BFormTextarea.vue'
-import BImg from './BImg.vue'
-import BInputGroup from './BInputGroup/BInputGroup.vue'
-import BInputGroupAddon from './BInputGroup/BInputGroupAddon.vue'
-import BInputGroupAppend from './BInputGroup/BInputGroupAppend.vue'
-import BInputGroupPrepend from './BInputGroup/BInputGroupPrepend.vue'
-import BInputGroupText from './BInputGroup/BInputGroupText.vue'
-import BLink from './BLink/BLink.vue'
-import BListGroup from './BListGroup/BListGroup.vue'
-import BListGroupItem from './BListGroup/BListGroupItem.vue'
-import BModal from './BModal.vue'
-import BNav from './BNav/BNav.vue'
-import BNavForm from './BNav/BNavForm.vue'
-import BNavItem from './BNav/BNavItem.vue'
-import BNavItemDropdown from './BNav/BNavItemDropdown.vue'
-import BNavText from './BNav/BNavText.vue'
-import BNavbar from './BNavbar/BNavbar.vue'
-import BNavbarBrand from './BNavbar/BNavbarBrand.vue'
-import BNavbarNav from './BNavbar/BNavbarNav.vue'
-import BNavbarToggle from './BNavbar/BNavbarToggle.vue'
-import BOffcanvas from './BOffcanvas.vue'
-import BOverlay from './BOverlay/BOverlay.vue'
-import BPagination from './BPagination/BPagination.vue'
-import BPlaceholder from './BPlaceholder/BPlaceholder.vue'
-import BPlaceholderButton from './BPlaceholder/BPlaceholderButton.vue'
-import BPlaceholderCard from './BPlaceholder/BPlaceholderCard.vue'
-import BPlaceholderTable from './BPlaceholder/BPlaceholderTable.vue'
-import BPlaceholderWrapper from './BPlaceholder/BPlaceholderWrapper.vue'
-import BPopover from './BPopover.vue'
-import BProgress from './BProgress/BProgress.vue'
-import BProgressBar from './BProgress/BProgressBar.vue'
-import BRow from './BRow.vue'
-import BSkeleton from './BSkeleton/BSkeleton.vue'
-import BSkeletonIcon from './BSkeleton/BSkeletonIcon.vue'
-import BSkeletonTable from './BSkeleton/BSkeletonTable.vue'
-import BSkeletonWrapper from './BSkeleton/BSkeletonWrapper.vue'
-import BSpinner from './BSpinner.vue'
-import BFormSpinButton from './BFormSpinButton/BFormSpinButton.vue'
-import BTable from './BTable/BTable.vue'
-import BTableSimple from './BTable/BTableSimple.vue'
-import BTbody from './BTable/BTbody.vue'
-import BTd from './BTable/BTd.vue'
-import BTfoot from './BTable/BTfoot.vue'
-import BTh from './BTable/BTh.vue'
-import BThead from './BTable/BThead.vue'
-import BTr from './BTable/BTr.vue'
-import BTab from './BTabs/BTab.vue'
-import BTabs from './BTabs/BTabs.vue'
-import {BToast, BToaster, BToastPlugin} from './BToast'
-import BToastContainer from './BToast/BToaster.vue'
-import BTransition from './BTransition/BTransition.vue'
+export {default as BAccordion} from './BAccordion/BAccordion.vue'
+export {default as BAccordionItem} from './BAccordion/BAccordionItem.vue'
+export {default as BAlert} from './BAlert/BAlert.vue'
+export {default as BAvatar} from './BAvatar/BAvatar.vue'
+export {default as BAvatarGroup} from './BAvatar/BAvatarGroup.vue'
+export {default as BBadge} from './BBadge/BBadge.vue'
+export {default as BBreadcrumb} from './BBreadcrumb/BBreadcrumb.vue'
+export {default as BBreadcrumbItem} from './BBreadcrumb/BBreadcrumbItem.vue'
+export {default as BButton} from './BButton/BButton.vue'
+export {default as BButtonGroup} from './BButton/BButtonGroup.vue'
+export {default as BButtonToolbar} from './BButton/BButtonToolbar.vue'
+export {default as BCloseButton} from './BButton/BCloseButton.vue'
+export {default as BCard} from './BCard/BCard.vue'
+export {default as BCardBody} from './BCard/BCardBody.vue'
+export {default as BCardFooter} from './BCard/BCardFooter.vue'
+export {default as BCardGroup} from './BCard/BCardGroup.vue'
+export {default as BCardHeader} from './BCard/BCardHeader.vue'
+export {default as BCardImg} from './BCard/BCardImg.vue'
+export {default as BCardSubtitle} from './BCard/BCardSubtitle.vue'
+export {default as BCardText} from './BCard/BCardText.vue'
+export {default as BCardTitle} from './BCard/BCardTitle.vue'
+export {default as BCarousel} from './BCarousel/BCarousel.vue'
+export {default as BCarouselSlide} from './BCarousel/BCarouselSlide.vue'
+export {default as BCol} from './BCol.vue'
+export {default as BCollapse} from './BCollapse.vue'
+export {default as BContainer} from './BContainer.vue'
+export {default as BDropdown} from './BDropdown/BDropdown.vue'
+export {default as BDropdownDivider} from './BDropdown/BDropdownDivider.vue'
+export {default as BDropdownForm} from './BDropdown/BDropdownForm.vue'
+export {default as BDropdownGroup} from './BDropdown/BDropdownGroup.vue'
+export {default as BDropdownHeader} from './BDropdown/BDropdownHeader.vue'
+export {default as BDropdownItem} from './BDropdown/BDropdownItem.vue'
+export {default as BDropdownItemButton} from './BDropdown/BDropdownItemButton.vue'
+export {default as BDropdownText} from './BDropdown/BDropdownText.vue'
+export {default as BForm} from './BForm/BForm.vue'
+export {default as BFormFloatingLabel} from './BForm/BFormFloatingLabel.vue'
+export {default as BFormInvalidFeedback} from './BForm/BFormInvalidFeedback.vue'
+export {default as BFormRow} from './BForm/BFormRow.vue'
+export {default as BFormText} from './BForm/BFormText.vue'
+export {default as BFormValidFeedback} from './BForm/BFormValidFeedback.vue'
+export {default as BFormCheckbox} from './BFormCheckbox/BFormCheckbox.vue'
+export {default as BFormCheckboxGroup} from './BFormCheckbox/BFormCheckboxGroup.vue'
+export {default as BFormGroup} from './BFormGroup/BFormGroup.vue'
+export {default as BFormInput} from './BFormInput/BFormInput.vue'
+export {default as BFormRadio} from './BFormRadio/BFormRadio.vue'
+export {default as BFormRadioGroup} from './BFormRadio/BFormRadioGroup.vue'
+export {default as BFormSelect} from './BFormSelect/BFormSelect.vue'
+export {default as BFormSelectOption} from './BFormSelect/BFormSelectOption.vue'
+export {default as BFormSelectOptionGroup} from './BFormSelect/BFormSelectOptionGroup.vue'
+export {default as BFormTag} from './BFormTags/BFormTag.vue'
+export {default as BFormTags} from './BFormTags/BFormTags.vue'
+export {default as BFormTextarea} from './BFormTextarea/BFormTextarea.vue'
+export {default as BImg} from './BImg.vue'
+export {default as BInputGroup} from './BInputGroup/BInputGroup.vue'
+export {default as BInputGroupAddon} from './BInputGroup/BInputGroupAddon.vue'
+export {default as BInputGroupAppend} from './BInputGroup/BInputGroupAppend.vue'
+export {default as BInputGroupPrepend} from './BInputGroup/BInputGroupPrepend.vue'
+export {default as BInputGroupText} from './BInputGroup/BInputGroupText.vue'
+export {default as BLink} from './BLink/BLink.vue'
+export {default as BListGroup} from './BListGroup/BListGroup.vue'
+export {default as BListGroupItem} from './BListGroup/BListGroupItem.vue'
+export {default as BModal} from './BModal.vue'
+export {default as BNav} from './BNav/BNav.vue'
+export {default as BNavForm} from './BNav/BNavForm.vue'
+export {default as BNavItem} from './BNav/BNavItem.vue'
+export {default as BNavItemDropdown} from './BNav/BNavItemDropdown.vue'
+export {default as BNavText} from './BNav/BNavText.vue'
+export {default as BNavbar} from './BNavbar/BNavbar.vue'
+export {default as BNavbarBrand} from './BNavbar/BNavbarBrand.vue'
+export {default as BNavbarNav} from './BNavbar/BNavbarNav.vue'
+export {default as BNavbarToggle} from './BNavbar/BNavbarToggle.vue'
+export {default as BOffcanvas} from './BOffcanvas.vue'
+export {default as BOverlay} from './BOverlay/BOverlay.vue'
+export {default as BPagination} from './BPagination/BPagination.vue'
+export {default as BPlaceholder} from './BPlaceholder/BPlaceholder.vue'
+export {default as BPlaceholderButton} from './BPlaceholder/BPlaceholderButton.vue'
+export {default as BPlaceholderCard} from './BPlaceholder/BPlaceholderCard.vue'
+export {default as BPlaceholderTable} from './BPlaceholder/BPlaceholderTable.vue'
+export {default as BPlaceholderWrapper} from './BPlaceholder/BPlaceholderWrapper.vue'
+export {default as BPopover} from './BPopover.vue'
+export {default as BProgress} from './BProgress/BProgress.vue'
+export {default as BProgressBar} from './BProgress/BProgressBar.vue'
+export {default as BRow} from './BRow.vue'
+export {default as BSkeleton} from './BSkeleton/BSkeleton.vue'
+export {default as BSkeletonIcon} from './BSkeleton/BSkeletonIcon.vue'
+export {default as BSkeletonTable} from './BSkeleton/BSkeletonTable.vue'
+export {default as BSkeletonWrapper} from './BSkeleton/BSkeletonWrapper.vue'
+export {default as BSpinner} from './BSpinner.vue'
+export {default as BFormSpinButton} from './BFormSpinButton/BFormSpinButton.vue'
+export {default as BTable} from './BTable/BTable.vue'
+export {default as BTableSimple} from './BTable/BTableSimple.vue'
+export {default as BTbody} from './BTable/BTbody.vue'
+export {default as BTd} from './BTable/BTd.vue'
+export {default as BTfoot} from './BTable/BTfoot.vue'
+export {default as BTh} from './BTable/BTh.vue'
+export {default as BThead} from './BTable/BThead.vue'
+export {default as BTr} from './BTable/BTr.vue'
+export {default as BTab} from './BTabs/BTab.vue'
+export {default as BTabs} from './BTabs/BTabs.vue'
+export {default as BToastContainer} from './BToast/BToaster.vue'
+export {default as BTransition} from './BTransition/BTransition.vue'
 
-export default {
-  BAccordion,
-  BAccordionItem,
-  BNavText,
-  BAlert,
-  BAvatar,
-  BAvatarGroup,
-  BNavForm,
-  BBadge,
-  BBreadcrumb,
-  BBreadcrumbItem,
-  BButton,
-  BButtonGroup,
-  BButtonToolbar,
-  BCard,
-  BCardBody,
-  BCardFooter,
-  BCardGroup,
-  BCardHeader,
-  BCardImg,
-  BCardSubtitle,
-  BCardText,
-  BCardTitle,
-  BCarousel,
-  BCarouselSlide,
-  BCloseButton,
-  BCol,
-  BCollapse,
-  BContainer,
-  BDropdown,
-  BDropdownDivider,
-  BDropdownForm,
-  BDropdownGroup,
-  BDropdownHeader,
-  BDropdownItem,
-  BDropdownItemButton,
-  BDropdownText,
-  BForm,
-  BFormCheckbox,
-  BFormCheckboxGroup,
-  BFormFloatingLabel,
-  BFormGroup,
-  BFormInput,
-  BFormInvalidFeedback,
-  BFormRadio,
-  BFormRadioGroup,
-  BFormRow,
-  BFormSelect,
-  BFormSelectOption,
-  BFormSelectOptionGroup,
-  BFormText,
-  BFormTextarea,
-  BFormTag,
-  BFormTags,
-  BFormValidFeedback,
-  BImg,
-  BInputGroup,
-  BInputGroupAddon,
-  BInputGroupAppend,
-  BInputGroupPrepend,
-  BInputGroupText,
-  BLink,
-  BListGroup,
-  BListGroupItem,
-  BModal,
-  BNav,
-  BNavbar,
-  BNavbarBrand,
-  BNavbarNav,
-  BNavbarToggle,
-  BNavItem,
-  BNavItemDropdown,
-  BOffcanvas,
-  BOverlay,
-  BPagination,
-  BPlaceholder,
-  BPlaceholderButton,
-  BPlaceholderCard,
-  BPlaceholderTable,
-  BPlaceholderWrapper,
-  BPopover,
-  BProgress,
-  BProgressBar,
-  BRow,
-  BSkeleton,
-  BSkeletonIcon,
-  BSkeletonTable,
-  BSkeletonWrapper,
-  BSpinner,
-  BFormSpinButton,
-  BTab,
-  BTable,
-  BTableSimple,
-  BTbody,
-  BTd,
-  BTfoot,
-  BTh,
-  BThead,
-  BTr,
-  BToast,
-  BToaster,
-  BToastContainer,
-  BTabs,
-  BTransition,
-  BToastPlugin,
-}
+export {BToast, BToaster, BToastPlugin} from './BToast'
---
+++ packages/bootstrap-vue-3/src/utils/classes/index.ts
@@ -0,0 +1,2 @@
+export {default as BvEvent} from './BvEvent'
+export {default as BvModalEvent} from './BvModalEvent'
