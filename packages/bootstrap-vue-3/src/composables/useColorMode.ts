30e9729806f9d3579af6e1c2987cfe6e5942ff89
issayah <github.me.mzu5a@simplelogin.com>
2023-01-04 15:38:09 -0600

    fix(useColorMode): remove broken modes option

--- packages/bootstrap-vue-3/src/composables/useColorMode.ts
+++ packages/bootstrap-vue-3/src/composables/useColorMode.ts
@@ -1,19 +1,12 @@
-import {useColorMode, UseColorModeOptions} from '@vueuse/core'
+import {useColorMode, type UseColorModeOptions} from '@vueuse/core'
 
-export default (
-  modes: UseColorModeOptions['modes'] = {},
-  persist = false,
-  opts: UseColorModeOptions = {}
-) => {
+export default (persist = false, opts: UseColorModeOptions = {}) => {
   const attribute = 'data-bs-theme'
   const selector = 'body'
   return useColorMode({
     attribute,
     selector,
     storageKey: persist ? `${opts.attribute ?? attribute}-${opts.selector ?? selector}` : null,
-    modes: {
-      ...modes,
-    },
     ...opts,
   })
 }
