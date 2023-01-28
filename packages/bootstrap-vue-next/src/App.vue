e51972a5dfcf8f07a288729ef74de207fc52492f
Issayah <github.me.mzu5a@simplelogin.com>
2023-01-28 11:01:23 -0600

    revert: App.vue to default

--- packages/bootstrap-vue-next/src/App.vue
+++ packages/bootstrap-vue-next/src/App.vue
@@ -1,13 +1,7 @@
 <template>
   <b-container>
     <b-row>
-      <b-col>
-        <b-carousel v-model="val" controls indicators>
-          <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=10" />
-          <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=11" />
-          <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=12" />
-        </b-carousel>
-      </b-col>
+      <b-col> Hello world </b-col>
     </b-row>
   </b-container>
 </template>
@@ -15,6 +9,4 @@
 <script setup lang="ts">
 // You can use this file as a development spot to test your changes
 // Please do not commit this file
-import {ref} from 'vue'
-const val = ref(0)
 </script>
