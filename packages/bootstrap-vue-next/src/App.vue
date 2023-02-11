6f8f5e6cee65f5c716ec9fadec7a291da0ba4e07
Issayah <github.me.mzu5a@simplelogin.com>
2023-02-11 12:26:56 -0600

    chore: revert app.vue

--- packages/bootstrap-vue-next/src/App.vue
+++ packages/bootstrap-vue-next/src/App.vue
@@ -1,21 +1,7 @@
 <template>
   <b-container>
     <b-row>
-      <b-col> foo </b-col>
-    </b-row>
-    <b-row>
-      <b-col>
-        <div>
-          <b-dropdown id="dropdown-1" v-model="a" text="Dropdown Button" class="m-md-2">
-            <b-dropdown-item>First Action</b-dropdown-item>
-            <b-dropdown-item>Second Action</b-dropdown-item>
-            <b-dropdown-item>Third Action</b-dropdown-item>
-            <b-dropdown-divider />
-            <b-dropdown-item active>Active action</b-dropdown-item>
-            <b-dropdown-item disabled>Disabled action</b-dropdown-item>
-          </b-dropdown>
-        </div>
-      </b-col>
+      <b-col> Hello world </b-col>
     </b-row>
   </b-container>
 </template>
@@ -23,6 +9,4 @@
 <script setup lang="ts">
 // You can use this file as a development spot to test your changes
 // Please do not commit this file
-import {ref} from 'vue'
-const a = ref(false)
 </script>
