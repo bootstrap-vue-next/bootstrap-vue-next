dc8be0d7f4e77424f819f20d568e18e8ef0e5a93
Issayah <github.me.mzu5a@simplelogin.com>
2023-02-12 18:27:24 -0600

    refactor: useColorMode move persist option to opts {} rather than as a parameter

--- packages/bootstrap-vue-next/src/composables/useColorMode.ts
+++ packages/bootstrap-vue-next/src/composables/useColorMode.ts
@@ -1,12 +1,17 @@
 import {useColorMode, type UseColorModeOptions} from '@vueuse/core'
 
-export default (persist = false, opts: UseColorModeOptions = {}) => {
+interface ColorModeOptions extends UseColorModeOptions {
+  persist?: boolean
+}
+
+export default (opts: ColorModeOptions = {}) => {
   const attribute = 'data-bs-theme'
   const selector = 'body'
   return useColorMode({
     attribute,
     selector,
-    storageKey: persist ? `${opts.attribute ?? attribute}-${opts.selector ?? selector}` : null,
+    storageKey:
+      opts.persist === true ? `${opts.attribute ?? attribute}-${opts.selector ?? selector}` : null,
     ...opts,
   })
 }
