2df3bbe261258dcad86f0745f651afe02f07c712
Issayah <github.me.mzu5a@simplelogin.com>
2023-05-09 22:02:32 -0500

    revert: foobar?

--- packages/nuxt/src/module.ts
+++ packages/nuxt/src/module.ts
@@ -21,7 +21,6 @@ export default defineNuxtModule<ModuleOptions>({
     // @ts-ignore
     const resolver = createResolver(import.meta.url)
 
-    // FOO!
     // const normalizedDirectives =
     //   typeof options.directives === 'boolean' ? {all: options.directives} : options.directives
     const normalizedComposables =
