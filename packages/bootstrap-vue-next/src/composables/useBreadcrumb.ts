da5898459a87c535951721c3617a907d6d59e6c4
Issayah <github.me.mzu5a@simplelogin.com>
2023-05-26 17:41:18 -0500

    refactor: use export default

--- packages/bootstrap-vue-next/src/composables/useBreadcrumb.ts
+++ packages/bootstrap-vue-next/src/composables/useBreadcrumb.ts
@@ -2,7 +2,7 @@ import {reactive} from 'vue'
 import type {BreadcrumbItem} from '../types'
 import {createGlobalState} from '@vueuse/core'
 
-export const useBreadcrumb = createGlobalState(() => {
+export default createGlobalState(() => {
   const items = reactive<BreadcrumbItem[]>([])
   const reset = (): void => {
     items.splice(0, items.length)
