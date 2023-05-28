4b9766ec5837e466722aacfa3f50dc496601b5ed
Glyn Gray <glyn@stirstudiosdesign.com>
2023-05-27 23:36:38 -0700

    Copy Icon

--- apps/docs/.vitepress/theme/Layout.vue
+++ apps/docs/.vitepress/theme/Layout.vue
@@ -235,6 +235,17 @@ const globalData = inject(appInfoKey, {
   [class^='language-'] {
     .copy {
       float: right;
+      border: none;
+      padding: 0;
+      outline: inherit;
+      height: 1.5rem;
+      width: 1.2rem;
+      background: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='rgba(108, 122, 137, 0.5' viewBox='0 0 384 512'%3E%3C!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --%3E%3Cpath d='M280 64h40c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128C0 92.7 28.7 64 64 64h40 9.6C121 27.5 153.3 0 192 0s71 27.5 78.4 64H280zM64 112c-8.8 0-16 7.2-16 16V448c0 8.8 7.2 16 16 16H320c8.8 0 16-7.2 16-16V128c0-8.8-7.2-16-16-16H304v24c0 13.3-10.7 24-24 24H192 104c-13.3 0-24-10.7-24-24V112H64zm128-8a24 24 0 1 0 0-48 24 24 0 1 0 0 48z'/%3E%3C/svg%3E")
+        0 0 no-repeat;
+      transition: transform 250ms;
+      &:hover {
+        transform: translateY(-2px);
+      }
     }
     .lang {
       display: none;
