f12cf1a86f961c16fd18c1e49096013c88326936
Glyn Gray <glyn@stirstudiosdesign.com>
2023-05-28 23:38:30 -0700

    TOC offcanvas menu

--- apps/docs/.vitepress/theme/Layout.vue
+++ apps/docs/.vitepress/theme/Layout.vue
@@ -1,95 +1,104 @@
 <template>
-  <b-container fluid class="p-0">
-    <b-row>
-      <b-col>
-        <b-navbar variant="primary">
-          <b-navbar-nav>
-            <b-navbar-brand :to="withBase('/')">Home</b-navbar-brand>
-            <b-nav>
-              <b-nav-item :to="withBase('/getting-started')">Getting Started</b-nav-item>
-              <b-nav-item :to="withBase('/reference/components')">Components</b-nav-item>
-              <b-nav-item :to="withBase('/reference/composables')">Composables</b-nav-item>
-              <b-nav-item :to="withBase('/reference/directives')">Directives</b-nav-item>
-              <b-nav-item :to="withBase('/reference/icons')">Icons</b-nav-item>
-              <b-nav-item :to="withBase('/reference/types')">Types</b-nav-item>
-              <b-nav-item :to="withBase('/migration-guide')">Migrate</b-nav-item>
-            </b-nav>
-          </b-navbar-nav>
-          <b-nav>
-            <b-button
-              :variant="null"
-              :href="globalData.githubUrl"
-              aria-label="Open Github"
-              target="_blank"
-              rel="noopener"
-            >
-              <github-icon aria-hidden />
-            </b-button>
-            <b-button
-              :variant="null"
-              :href="globalData.opencollectiveUrl"
-              aria-label="Open Github"
-              target="_blank"
-              rel="noopener"
-            >
-              <opencollective-icon />
-            </b-button>
-            <b-button
-              :variant="null"
-              :href="globalData.discordUrl"
-              aria-label="Open Discord Server"
-              target="_blank"
-              rel="noopener"
-            >
-              <discord-icon aria-hidden />
-            </b-button>
-            <client-only>
-              <b-dropdown :variant="null">
-                <!-- TODO there's no way to adjust these options, say if you wanted to remove the padding -->
-                <template #button-content>
-                  <component :is="currentIcon" :aria-label="`Toggle theme (${dark})`" />
-                </template>
-                <b-dropdown-item
-                  v-for="el in options"
-                  :key="el"
-                  :active="dark === el"
-                  @click="set(el)"
-                >
-                  <component :is="map[el]" /> {{ el }}
-                </b-dropdown-item>
-              </b-dropdown>
-            </client-only>
-          </b-nav>
-        </b-navbar>
-      </b-col>
-    </b-row>
-    <b-row v-if="page.isNotFound">
-      <b-col>
-        <b-container class="text-center my-auto p-5">
-          <b-row>
-            <b-col>
-              <h1>Oh No!</h1>
-            </b-col>
-          </b-row>
-          <b-row>
-            <b-col>
-              <h2>File Not Found</h2>
-            </b-col>
-          </b-row>
-        </b-container>
-      </b-col>
-    </b-row>
-    <b-row v-else>
-      <b-col>
-        <b-container>
-          <b-row>
-            <b-col>
-              <Content class="bd-content mt-5" />
-            </b-col>
-          </b-row>
-        </b-container>
-      </b-col>
-    </b-row>
+  <b-navbar variant="primary" sticky="top" toggleable="lg">
+    <b-navbar-toggle @click="toggler" />
+    <b-collapse is-nav>
+      <b-navbar-nav>
+        <b-navbar-brand :to="withBase('/')">Home</b-navbar-brand>
+        <b-nav>
+          <b-nav-item :to="withBase('/getting-started')">Getting Started</b-nav-item>
+          <!-- <b-nav-item :to="withBase('/reference/components')">Components</b-nav-item>
+          <b-nav-item :to="withBase('/reference/composables')">Composables</b-nav-item>
+          <b-nav-item :to="withBase('/reference/directives')">Directives</b-nav-item> -->
+          <b-nav-item :to="withBase('/reference/icons')">Icons</b-nav-item>
+          <b-nav-item :to="withBase('/reference/types')">Types</b-nav-item>
+          <b-nav-item :to="withBase('/migration-guide')">Migrate</b-nav-item>
+        </b-nav>
+      </b-navbar-nav>
+    </b-collapse>
+    <b-nav>
+      <b-button
+        :variant="null"
+        :href="globalData.githubUrl"
+        aria-label="Open Github"
+        target="_blank"
+        rel="noopener"
+      >
+        <github-icon aria-hidden />
+      </b-button>
+      <b-button
+        :variant="null"
+        :href="globalData.opencollectiveUrl"
+        aria-label="Open Github"
+        target="_blank"
+        rel="noopener"
+      >
+        <opencollective-icon />
+      </b-button>
+      <b-button
+        :variant="null"
+        :href="globalData.discordUrl"
+        aria-label="Open Discord Server"
+        target="_blank"
+        rel="noopener"
+      >
+        <discord-icon aria-hidden />
+      </b-button>
+      <client-only>
+        <b-dropdown :variant="null">
+          <!-- TODO there's no way to adjust these options, say if you wanted to remove the padding -->
+          <template #button-content>
+            <component :is="currentIcon" :aria-label="`Toggle theme (${dark})`" />
+          </template>
+          <b-dropdown-item v-for="el in options" :key="el" :active="dark === el" @click="set(el)">
+            <component :is="map[el]" /> {{ el }}
+          </b-dropdown-item>
+        </b-dropdown>
+      </client-only>
+    </b-nav>
+  </b-navbar>
+  <b-container fluid class="container-lg mt-3 my-md-4 bd-layout">
+    <aside class="bd-sidebar">
+      <b-offcanvas
+        v-model="sidebar"
+        static="true"
+        :backdrop="isLargeScreen ? 'static' : ''"
+        title="Browse docs"
+        class="h-100"
+      >
+        <TableOfContentsNav />
+      </b-offcanvas>
+    </aside>
+    <main class="bd-main">
+      <div class="bd-content">
+        <b-row v-if="page.isNotFound">
+          <b-col>
+            <b-container class="text-center my-auto p-5">
+              <b-row>
+                <b-col>
+                  <h1>Oh No!</h1>
+                </b-col>
+              </b-row>
+              <b-row>
+                <b-col>
+                  <h2>File Not Found</h2>
+                </b-col>
+              </b-row>
+            </b-container>
+          </b-col>
+        </b-row>
+        <b-row v-else>
+          <b-col>
+            <b-container>
+              <b-row>
+                <b-col>
+                  <Content class="bd-content" />
+                </b-col>
+              </b-row>
+            </b-container>
+          </b-col>
+        </b-row>
+      </div>
+    </main>
   </b-container>
 </template>
 
@@ -102,13 +111,17 @@ import {
   BDropdownItem,
   BNav,
   BNavbar,
+  BNavbarToggle,
+  BCollapse,
   BNavbarBrand,
   BNavbarNav,
   BNavItem,
   BRow,
   useColorMode,
+  BOffcanvas,
 } from 'bootstrap-vue-next'
-import {computed, inject} from 'vue'
+import {computed, inject, watch} from 'vue'
+import {ref, type Ref} from 'vue'
 import GithubIcon from '~icons/bi/github'
 import OpencollectiveIcon from '~icons/simple-icons/opencollective'
 import DiscordIcon from '~icons/bi/discord'
@@ -117,9 +130,24 @@ import SunFill from '~icons/bi/sun-fill'
 import CircleHalf from '~icons/bi/circle-half'
 import {useData, withBase} from 'vitepress'
 import {appInfoKey} from './keys'
+import {useMediaQuery} from '@vueuse/core'
+import TableOfContentsNav from '../../src/components/TableOfContentsNav.vue'
 
 // https://vitepress.dev/reference/runtime-api#usedata
 const {page} = useData()
+
+const sidebar = ref(true)
+const isLargeScreen = useMediaQuery('(min-width: 992px)')
+const toggler = () => {
+  sidebar.value = !sidebar.value
+}
+watch(isLargeScreen, (newValue) => {
+  if (newValue === true) {
+    sidebar.value = true
+  } else {
+    sidebar.value = false
+  }
+})
 
 const dark = useColorMode({
   persist: true,
@@ -292,6 +320,72 @@ const globalData = inject(appInfoKey, {
         opacity: 1;
       }
     }
+  }
+  .offcanvas {
+    .list-group {
+      padding: 0.5rem;
+      .bd-links-heading {
+        display: block;
+        margin: 0 0 1rem;
+      }
+      .list-group-item {
+        border: none;
+        padding: 0;
+        a {
+          display: block;
+          padding: 0.1875rem 0.7rem;
+          margin: 0.125rem 0;
+          text-decoration: none;
+        }
+      }
+    }
+  }
+}
+// TOC
+@media (min-width: 992px) {
+  .bd-layout {
+    display: grid !important;
+    grid-template-areas: 'sidebar main';
+    grid-template-columns: 1fr 5fr;
+    gap: 1.5rem;
+    .bd-sidebar {
+      grid-area: sidebar;
+      position: -webkit-sticky;
+      position: sticky;
+      top: 5rem;
+      display: block !important;
+      height: calc(100vh - 6rem);
+      padding-left: 0.25rem;
+      margin-left: -0.25rem;
+      overflow-y: auto;
+      min-width: 300px;
+      .offcanvas {
+        transition: none !important;
+        transform: 0;
+        position: relative !important;
+        .offcanvas-header {
+          .btn-close {
+            display: none;
+          }
+        }
+      }
+    }
+    .bd-main {
+      display: grid;
+      grid-area: main;
+      grid-template-areas: 'intro toc' 'content toc';
+      grid-template-rows: auto 1fr;
+      grid-template-columns: 4fr 1fr;
+      .bd-content {
+        grid-area: content;
+        min-width: 1px;
+      }
+    }
+  }
+}
+@media (max-width: 992px) {
+  .offcanvas.offcanvas-start {
+    width: 300px !important;
   }
 }
 </style>
