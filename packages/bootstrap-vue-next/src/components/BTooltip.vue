a7d9718184d25413407952e9b3a588943124be1d
Duncan Sanderson <duncan@duncansanderson.co.uk>
2023-07-04 12:07:25 +0100

    feat(BTooltip): expose hide, show and toggle methods from BPopover

--- packages/bootstrap-vue-next/src/components/BTooltip.vue
+++ packages/bootstrap-vue-next/src/components/BTooltip.vue
@@ -1,5 +1,5 @@
 <template>
-  <BPopover tooltip>
+  <BPopover ref="popover" tooltip>
     <template v-for="(_, name) in $slots" #[name]="slotData">
       <slot :name="name" v-bind="slotData" />
     </template>
@@ -7,5 +7,20 @@
 </template>
 
 <script setup lang="ts">
+import {ref} from 'vue'
 import BPopover from './BPopover.vue'
+
+const popover = ref()
+
+/* Extend the hide, show and toggle functions of the BPopover component
+ to allow the user to programmatically control the tooltip. */
+const hideFn = (event: Event) => popover.value.hideFn(event)
+const show = () => popover.value.show()
+const toggle = (event: Event) => popover.value.toggle(event)
+
+defineExpose({
+  hideFn,
+  show,
+  toggle,
+})
 </script>
