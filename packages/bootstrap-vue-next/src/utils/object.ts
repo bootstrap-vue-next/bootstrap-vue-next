417979688f383f310124ab2fe75b07c9e42a3350
Issayah <github.me.mzu5a@simplelogin.com>
2023-07-14 18:25:13 -0500

    chore: remove dead code

--- packages/bootstrap-vue-next/src/utils/object.ts
+++ packages/bootstrap-vue-next/src/utils/object.ts
@@ -13,25 +13,3 @@ export const omit = <A extends Record<PropertyKey, unknown>, B extends ReadonlyA
   Object.keys(objToPluck)
     .filter((key) => !keysToPluck.includes(key))
     .reduce((result, key) => ({...result, [key]: objToPluck[key]}), {} as Omit<A, B[number]>)
-
-/**
- * Deeply clones an item
- *
- * @param {T} source
- * @returns
- */
-export const cloneDeep = <T>(source: T): T =>
-  Array.isArray(source)
-    ? source.map((item) => cloneDeep(item))
-    : source instanceof Date
-    ? new Date(source.getTime())
-    : source && typeof source === 'object'
-    ? Object.getOwnPropertyNames(source).reduce((o, prop) => {
-        Object.defineProperty(o, prop, Object.getOwnPropertyDescriptor(source, prop) ?? {})
-        o[prop] = cloneDeep((source as unknown as {[key: string]: unknown})[prop])
-        return o
-      }, Object.create(Object.getPrototypeOf(source)))
-    : source
-
-export const cloneDeepAsync = <T>(source: T): Promise<T> =>
-  new Promise((resolve) => resolve(cloneDeep(source)))
