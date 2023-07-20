a10a31bfbd51542ca32b1c4454986611328617e1
Daniel Harrington <danielh@rotair.com>
2023-07-19 23:12:10 -0400

    feat(Toast): Revert App

--- packages/bootstrap-vue-next/src/App.vue
+++ packages/bootstrap-vue-next/src/App.vue
@@ -1,24 +1,12 @@
 <template>
-  <BContainer>
-    <BRow>
-      <BCol> Hello World! </BCol>
-    </BRow>
-  </BContainer>
+  <b-container>
+    <b-row>
+      <b-col> Hello World! </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script setup lang="ts">
-import {ToastInstance, useToast} from './components/BToast/plugin'
-
-const toast = useToast()
-
-console.log(toast)
-toast?.show({title: 'wwsw', body: 'wow'}, {autoHide: false})
-
-toast?.info({title: 'wow', body: 'wow'}, {autoHide: false})
-toast?.warning({body: 'wow'}, {autoHide: false})
-toast?.danger({body: 'wow'}, {autoHide: false})
-toast?.success({body: 'wow'}, {autoHide: false, variant: 'success'})
-
 // You can use this file as a development spot to test your changes
 // Please do not commit this file
 </script>
