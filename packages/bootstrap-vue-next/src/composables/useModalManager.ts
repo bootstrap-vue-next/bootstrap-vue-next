09f8c6cb594956f34697eea7f00d699e1b91ace2
Simon Asika <asika32764@gmail.com>
2023-09-12 01:13:30 +0800

    Apply suggestions from code review
    Co-authored-by: Issayah <github.me.mzu5a@simplelogin.com>

--- packages/bootstrap-vue-next/src/composables/useModalManager.ts
+++ packages/bootstrap-vue-next/src/composables/useModalManager.ts
@@ -10,10 +10,11 @@ export const useSharedModalStack = createSharedComposable(() => {
   const stack = ref<ModalInstance[]>([])
   const count = computed(() => stack.value.length)
   const last = computed(() => stack.value[stack.value.length - 1])
-  const push = (modal: ModalInstance) => stack.value.push(modal)
-  const pop = () => stack.value.pop()
-  const remove = (modal: ModalInstance) =>
-    (stack.value = stack.value.filter((item: ModalInstance) => item.uid !== modal.uid))
+  const push = (modal: ModalInstance) => {stack.value.push(modal)}
+  const pop = () => {stack.value.pop()}
+  const remove = (modal: ModalInstance) => {
+    stack.value = stack.value.filter((item: ModalInstance) => item.uid !== modal.uid)
+  }
   const find = (id: string): ModalInstance | null =>
     registry.value.find((modal: ModalInstance) => modal.proxy?.id === id) || null
 
