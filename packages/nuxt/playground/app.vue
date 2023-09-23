353794208b2467aba149e7f521e5db0c4661cc23
Issayah <github.me.mzu5a@simplelogin.com>
2023-09-23 12:58:19 -0500

    feat(nuxt): automatically transform asset urls for vite fixes #1470

--- packages/nuxt/playground/app.vue
+++ packages/nuxt/playground/app.vue
@@ -1,5 +1,6 @@
 <template>
   <div style="min-height: 1000vh">
+    <BImg src="@/assets/0-circle.svg" fluid alt="Responsive image" />
     <BModal v-model="show"> Nuxt module playground! </BModal>
     <BButton @click="show = !show"> Click me </BButton>
     {{ f }}
--- packages/nuxt/playground/nuxt.config.ts
+++ packages/nuxt/playground/nuxt.config.ts
@@ -1,4 +1,7 @@
 export default defineNuxtConfig({
   modules: ['../src/module'],
+  alias: {
+    '@': '/',
+  },
   css: ['bootstrap/dist/css/bootstrap.min.css'],
 })
