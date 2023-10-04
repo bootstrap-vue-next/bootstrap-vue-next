ea3a6a565accb6eaaf16a5bd97a0bc13d8ee56b0
Matthew Lieder <mlieder@lt.life>
2023-10-04 17:00:41 -0500

    Revert simplification of useSafeScrollLock

--- packages/bootstrap-vue-next/src/composables/useSafeScrollLock.ts
+++ packages/bootstrap-vue-next/src/composables/useSafeScrollLock.ts
@@ -1,18 +1,23 @@
-import {type MaybeRefOrGetter, onMounted, toValue, watch} from 'vue'
+import {type MaybeRefOrGetter, onMounted, readonly, toRef, watch} from 'vue'
 import {useScrollLock} from '@vueuse/core'
 
-export default (
-  isOpen: MaybeRefOrGetter<boolean>,
-  bodyScrollLockingDisabled: MaybeRefOrGetter<boolean>
-) => {
+export default (isOpen: MaybeRefOrGetter<boolean>, bodyScroll: MaybeRefOrGetter<boolean>) => {
+  const resolvedIsOpen = readonly(toRef(() => isOpen))
+  const resolvedBodyScrolling = readonly(toRef(() => bodyScroll))
+
+  /**
+   * We use the inverse because bodyScrolling === true means we allow scrolling, while bodyScrolling === false means we disallow
+   */
+  const inverseBodyScrollingValue = toRef(() => !resolvedBodyScrolling.value)
+
   onMounted(() => {
     const isLocked = useScrollLock(
       document.body,
-      toValue(isOpen) && !toValue(bodyScrollLockingDisabled)
+      resolvedIsOpen.value && inverseBodyScrollingValue.value
     )
 
-    watch([isOpen, bodyScrollLockingDisabled], ([modelVal, bodyVal]) => {
-      isLocked.value = toValue(modelVal) && !toValue(bodyVal)
+    watch([resolvedIsOpen, inverseBodyScrollingValue], ([modelVal, bodyVal]) => {
+      isLocked.value = modelVal && bodyVal
     })
   })
 }
