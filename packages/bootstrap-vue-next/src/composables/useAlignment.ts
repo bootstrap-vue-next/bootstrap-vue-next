fa7ba9019a574a35a2027c81eb2248af0bf50d1c
Issayah <github.me.mzu5a@simplelogin.com>
2023-11-03 21:59:53 -0500

    refactor: use computed instead of computed eager

--- packages/bootstrap-vue-next/src/composables/useAlignment.ts
+++ packages/bootstrap-vue-next/src/composables/useAlignment.ts
@@ -1,11 +1,10 @@
-import {computedEager} from '@vueuse/core'
-import {type MaybeRefOrGetter, type Ref, toValue} from 'vue'
+import {computed, type MaybeRefOrGetter, type Ref, toValue} from 'vue'
 import type {AlignmentJustifyContent} from '../types'
 
 export default (
   align: MaybeRefOrGetter<AlignmentJustifyContent | undefined>
 ): Readonly<Ref<string>> =>
-  computedEager(() => {
+  computed(() => {
     const value = toValue(align)
     return !value ? '' : `justify-content-${value}`
   })
