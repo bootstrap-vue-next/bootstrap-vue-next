fb40d2e12cad8e35b671c2ca4fae2446296b8e50
Lias <mikhailcolt@gmail.com>
2023-11-09 18:16:53 +0700

    docs(BBbuttonGroup): update component reference

--- apps/docs/src/data/components/buttonGroup.data.ts
+++ apps/docs/src/data/components/buttonGroup.data.ts
@@ -9,26 +9,30 @@ export default {
           prop: 'ariaLabel',
           type: 'string',
           default: 'Group',
+          description: `Sets the ARIA attribute 'role' to a specific value`,
         },
         {
           prop: 'size',
           type: 'Size',
           default: 'md',
+          description: `Set the size of the component's appearance`,
         },
         {
           prop: 'tag',
           type: 'string',
           default: 'div',
+          description: `Specify the HTML tag to render instead of the default tag`,
         },
         {
           prop: 'vertical',
           type: 'Booleanish',
           default: false,
+          description: `When set, rendered the button group in vertical mode`,
         },
       ],
       slots: [
         {
-          description: '',
+          description: 'Content (buttons) to place in the button group',
           name: 'default',
           scope: [],
         },
