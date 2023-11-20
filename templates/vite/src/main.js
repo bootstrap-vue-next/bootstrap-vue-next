e60bb14b93dc21f7184f49e8e36243ad61794942
Jukka Raimovaara <roska@mentalhouse.fi>
2023-11-21 01:45:55 +0200

    Update template/vite: remove toasterPlugin

--- templates/vite/src/main.js
+++ templates/vite/src/main.js
@@ -3,10 +3,9 @@ import { createApp } from 'vue';
 import App from './App.vue';
 const app = createApp(App);
 
-import { BootstrapVueNext, BToastPlugin } from 'bootstrap-vue-next';
+import { BootstrapVueNext } from 'bootstrap-vue-next';
 import 'bootstrap-styles/css/bootstrap.css';
 import 'bootstrap-vue-next/dist/bootstrap-vue-next.css';
 app.use(BootstrapVueNext);
-app.use(BToastPlugin);
 
 app.mount('#app');
