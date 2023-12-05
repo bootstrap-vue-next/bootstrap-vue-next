881e00087bc351e279577df91c04a6b50ce53d23
David W. Gray <dwgray67@hotmail.com>
2023-12-05 15:00:24 -0800

    Create a typescript template for bug reporting

---
+++ templates/typescript/src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <Comp />
+  </div>
+</template>
+<script setup lang="ts">
+import Comp from "./components/Comp.vue";
+</script>
---
+++ templates/typescript/src/components/Comp.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    {{ msg }}
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from "vue";
+const msg = ref("Comp");
+</script>
