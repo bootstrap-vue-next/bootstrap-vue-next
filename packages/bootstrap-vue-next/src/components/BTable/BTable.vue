8ccf584a327a212eff9b05e3fa8c1ace59d2bd76
Marco Curatitoli <makroxyz@gmail.com>
2023-12-18 17:40:34 +0100

    fix table filter without filterable (#1684)
    fix(BTable): table filter without filterable fixes #1683

--- packages/bootstrap-vue-next/src/components/BTable/BTable.vue
+++ packages/bootstrap-vue-next/src/components/BTable/BTable.vue
@@ -321,10 +321,10 @@ const computedFields = computed<TableField[]>(() =>
               isSortable.value === false
                 ? undefined
                 : sortByModel.value !== el.key
-                  ? 'none'
-                  : sortDescBoolean.value === true
-                    ? 'descending'
-                    : 'ascending',
+                ? 'none'
+                : sortDescBoolean.value === true
+                ? 'descending'
+                : 'ascending',
             ...el.thAttr,
           },
         }
@@ -403,7 +403,13 @@ const computedItems = computed<TableItem[]>(() => {
   const filterItems = (items: TableItem[]) =>
     items.filter((item) =>
       Object.entries(item).some(([key, val]) => {
-        if (!val || key[0] === '_' || !props.filterable?.includes(key)) return false
+        if (
+          val === null ||
+          val === undefined ||
+          key[0] === '_' ||
+          (!props.filterable?.includes(key) && !!props.filterable?.length)
+        )
+          return false
         const itemValue: string =
           typeof val === 'object' ? JSON.stringify(Object.values(val)) : val.toString()
         return itemValue.toLowerCase().includes(props.filter?.toLowerCase() ?? '')
