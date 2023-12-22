1102a5ec801c7e720af12251a8217d5fde287745
Ángel Roldán Martínez <Angel.RoldanMartinez@Clarivate.com>
2023-12-22 20:30:02 +0100

    fix(modal) modal only emits show once when shown and hide once when dismissed

--- apps/docs/src/data/components/modal.data.ts
+++ apps/docs/src/data/components/modal.data.ts
@@ -399,7 +399,7 @@ export default {
               type: 'BvTriggerableEvent',
             },
           ],
-          description: 'Always emits just after modal is shown. Cancelable',
+          description: 'Always emits just before modal is shown. Cancelable',
         },
         {
           event: 'hide',
