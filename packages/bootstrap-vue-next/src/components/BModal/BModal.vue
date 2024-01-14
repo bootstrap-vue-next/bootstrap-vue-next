937293f427047afbf56f17884be48a580d26d681
Ángel Roldán Martínez <anrolmar@gmail.com>
2024-01-14 22:59:39 +0100

    fix(BModal): modal emits show and hide more than once fixes #1655
    Co-authored-by: Ángel Roldán Martínez <Angel.RoldanMartinez@Clarivate.com>

--- packages/bootstrap-vue-next/src/components/BModal/BModal.vue
+++ packages/bootstrap-vue-next/src/components/BModal/BModal.vue
@@ -104,6 +104,7 @@
 </template>
 
 <script setup lang="ts">
+import {onKeyStroke, useEventListener, useFocus, useVModel} from '@vueuse/core'
 import {computed, type CSSProperties, reactive, ref, toRef, watch} from 'vue'
 import {
   useBooleanish,
@@ -112,13 +113,12 @@ import {
   useModalManager,
   useSafeScrollLock,
 } from '../../composables'
-import {onKeyStroke, useEventListener, useFocus, useVModel} from '@vueuse/core'
 import type {BModalProps} from '../../types'
 import {BvTriggerableEvent, isEmptySlot} from '../../utils'
 import BButton from '../BButton/BButton.vue'
 import BCloseButton from '../BButton/BCloseButton.vue'
-import BTransition from '../BTransition/BTransition.vue'
 import BOverlay from '../BOverlay/BOverlay.vue'
+import BTransition from '../BTransition/BTransition.vue'
 
 defineOptions({
   inheritAttrs: false,
@@ -424,6 +424,8 @@ const hide = (trigger = '') => {
 // TODO: If a show is prevented, it will briefly show the animation. This is a bug
 // I'm not sure how to wait for the event to be determined. Before showing
 const showFn = () => {
+  if (isActive.value) return
+
   const event = buildTriggerableEvent('show', {cancelable: true})
   emit('show', event)
   if (event.defaultPrevented) {
