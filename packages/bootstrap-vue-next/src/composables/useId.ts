f38624af1eece0b05c171af4e03614c20dd30000
Al Rozhkov <rozhkov-aa@yandex.ru>
2024-01-26 16:12:31 +0300

    fix(utils): make useId SSR friendly

--- packages/bootstrap-vue-next/src/composables/useId.ts
+++ packages/bootstrap-vue-next/src/composables/useId.ts
@@ -1,5 +1,28 @@
 import {getId} from '../utils'
-import {computed, type ComputedRef, type MaybeRefOrGetter, toValue} from 'vue'
+import {
+  computed,
+  type ComputedRef,
+  type MaybeRefOrGetter,
+  nextTick,
+  onMounted,
+  ref,
+  toValue,
+} from 'vue'
 
-export default (id?: MaybeRefOrGetter<string | undefined>, suffix?: string): ComputedRef<string> =>
-  computed(() => toValue(id) || getId(suffix))
+export default (
+  id?: MaybeRefOrGetter<string | undefined>,
+  suffix?: string
+): ComputedRef<string | null> => {
+  const localId = ref(toValue(id) || null)
+
+  onMounted(() =>
+    nextTick(() => {
+      debugger
+      if (!localId.value) {
+        localId.value = getId(suffix)
+      }
+    })
+  )
+
+  return computed(() => toValue(id) || localId.value)
+}
