78bd7d3b78a77c03028d7cceae3a3bd2cbf1e9dc
sceee <11340487+sceee@users.noreply.github.com>
2024-02-23 13:52:56 +0100

    chore: remove unnecessary computed props in BFormSelectOption.vue
    Closes: #1774

--- packages/bootstrap-vue-next/src/components/BFormSelect/BFormSelectOption.vue
+++ packages/bootstrap-vue-next/src/components/BFormSelect/BFormSelectOption.vue
@@ -1,12 +1,10 @@
 <template>
-  <option :value="value" :disabled="disabledBoolean">
+  <option :value="value" :disabled="props.disabled">
     <slot />
   </option>
 </template>
 
 <script setup lang="ts" generic="T">
-import {computed} from 'vue'
-
 const props = withDefaults(
   defineProps<{
     disabled?: boolean
@@ -22,6 +20,4 @@ defineSlots<{
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   default?: (props: Record<string, never>) => any
 }>()
-
-const disabledBoolean = computed(() => props.disabled)
 </script>
