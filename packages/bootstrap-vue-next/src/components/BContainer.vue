b7421a7bb9dedffab8cad25d9303338be787f152
sceee <11340487+sceee@users.noreply.github.com>
2024-02-23 12:02:36 +0100

    chore: remove unnecessary computed props in BContainer.vue
    Closes: #1774

--- packages/bootstrap-vue-next/src/components/BContainer.vue
+++ packages/bootstrap-vue-next/src/components/BContainer.vue
@@ -26,12 +26,11 @@ const props = withDefaults(
 
 const gutterXToNumber = useToNumber(() => props.gutterX ?? NaN, {method: 'parseInt'})
 const gutterYToNumber = useToNumber(() => props.gutterY ?? NaN, {method: 'parseInt'})
-const resolvedFluid = computed(() => props.fluid)
 
 const computedClasses = computed(() => ({
-  container: resolvedFluid.value === false,
-  [`container-fluid`]: resolvedFluid.value === true,
-  [`container-${resolvedFluid.value}`]: typeof resolvedFluid.value === 'string',
+  container: props.fluid === false,
+  [`container-fluid`]: props.fluid === true,
+  [`container-${props.fluid}`]: typeof props.fluid === 'string',
   [`gx-${gutterXToNumber.value}`]: !Number.isNaN(gutterXToNumber.value),
   [`gy-${gutterYToNumber.value}`]: !Number.isNaN(gutterYToNumber.value),
 }))
