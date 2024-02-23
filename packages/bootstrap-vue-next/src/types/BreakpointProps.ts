1c2d91220d99dcedf7bf6871d1a8bc5c5159ee35
sceee <11340487+sceee@users.noreply.github.com>
2024-02-23 11:42:52 +0100

    chore: remove LiteralUnion from ColBreakpointProps
    Closes: #1774

--- packages/bootstrap-vue-next/src/types/BreakpointProps.ts
+++ packages/bootstrap-vue-next/src/types/BreakpointProps.ts
@@ -1,4 +1,4 @@
-import type {LiteralUnion, Numberish} from '.'
+import type {Numberish} from '.'
 
 export type Breakpoint = 'sm' | 'md' | 'lg' | 'xl' | 'xxl'
 
@@ -19,11 +19,11 @@ export interface OrderBreakpointProps {
 }
 
 export interface ColBreakpointProps {
-  sm?: LiteralUnion<boolean, Numberish>
-  md?: LiteralUnion<boolean, Numberish>
-  lg?: LiteralUnion<boolean, Numberish>
-  xl?: LiteralUnion<boolean, Numberish>
-  xxl?: LiteralUnion<boolean, Numberish>
+  sm?: boolean | Numberish
+  md?: boolean | Numberish
+  lg?: boolean | Numberish
+  xl?: boolean | Numberish
+  xxl?: boolean | Numberish
 }
 
 export interface RowColsBreakpointProps {
