806aed3c6e0cf7f5b125daa9f5fc15139b93d73f
Jukka Raimovaara <roska@mentalhouse.fi>
2024-03-20 04:12:12 +0200

    fix(BCollapse): reset modelValue if event is prevented

--- packages/bootstrap-vue-next/src/components/BCollapse/BCollapse.vue
+++ packages/bootstrap-vue-next/src/components/BCollapse/BCollapse.vue
@@ -99,7 +99,7 @@ const computedId = useId(() => props.id, 'collapse')
 
 const element = useTemplateRef<HTMLElement | null>('element')
 const isCollapsing = ref(false)
-const showRef = ref(modelValue.value)
+const showRef = ref(modelValue.value && !props.toggle)
 
 const computedClasses = computed(() => ({
   'show': showRef.value,
@@ -124,18 +124,23 @@ const sharedSlots = computed<SharedSlotsData>(() => ({
   show,
   hide,
   id: computedId.value,
-  visible: modelValue.value,
+  visible: showRef.value,
 }))
 
 let revealTimeout: ReturnType<typeof setTimeout> | undefined
 let hideTimeout: ReturnType<typeof setTimeout> | undefined
 let _skipAnimation = props.skipAnimation
 
+let noAction = false
 const reveal = () => {
   const event = buildTriggerableEvent('show', {cancelable: true})
   emit('show', event)
   if (event.defaultPrevented) {
     emit('show-prevented')
+    noAction = true
+    nextTick(() => {
+      modelValue.value = false
+    })
     return
   }
   clearTimeout(hideTimeout)
@@ -165,6 +170,10 @@ const hideFn = () => {
   emit('hide', event)
   if (event.defaultPrevented) {
     emit('hide-prevented')
+    noAction = true
+    nextTick(() => {
+      modelValue.value = true
+    })
     return
   }
   clearTimeout(revealTimeout)
@@ -201,15 +210,23 @@ const hideFn = () => {
 }
 
 watch(modelValue, () => {
+  if (noAction) {
+    noAction = false
+    return
+  }
   modelValue.value ? reveal() : hideFn()
 })
 
 onMounted(() => {
   if (element.value === null) return
-  if (!modelValue.value && props.toggle) {
-    nextTick(() => {
-      modelValue.value = true
-    })
+  if (props.toggle) {
+    if (!modelValue.value) {
+      nextTick(() => {
+        modelValue.value = true
+      })
+    } else {
+      reveal()
+    }
   }
 })
 
