35243af3beed93ce77379bf5632eaba324f12a3d
Jukka Raimovaara <roska@mentalhouse.fi>
2024-03-20 05:49:55 +0200

    fix(BModal): fix events

--- packages/bootstrap-vue-next/src/components/BModal/BModal.vue
+++ packages/bootstrap-vue-next/src/components/BModal/BModal.vue
@@ -480,13 +480,14 @@ const pickFocusItem = () => {
 const onBeforeEnter = () => {} //showFn()
 const onAfterEnter = (el: Element) => {
   fadeIn(el)
+  isActive.value = true
   pickFocusItem()
   emit('shown', buildTriggerableEvent('shown'))
   if (props.lazy === true) lazyLoadCompleted.value = true
 }
 const isLeaving = ref(false)
 const onLeave = () => {
-  // isActive.value = false
+  isActive.value = false
   isLeaving.value = true
 }
 const onAfterLeave = (el: Element) => {
@@ -504,12 +505,12 @@ const onAfterLeave = (el: Element) => {
 }
 
 const {activePosition, activeModalCount, stackWithoutSelf} = useModalManager(
-  isActive,
+  showRef,
   modelValue.value
 )
 
 watch(stackWithoutSelf, (newValue, oldValue) => {
-  if (newValue.length > oldValue.length && isActive.value === true && props.noStacking === true)
+  if (newValue.length > oldValue.length && showRef.value === true && props.noStacking === true)
     hideFn()
 })
 
@@ -542,7 +543,7 @@ const sharedSlots = computed<SharedSlotsData>(() => ({
     hideFn('ok')
   },
   active: isActive.value,
-  visible: modelValue.value,
+  visible: showRef.value,
 }))
 
 defineExpose({
