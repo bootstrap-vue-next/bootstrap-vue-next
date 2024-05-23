c77c1f3052aa56eac9680ee341dad1cb21793744
Issayah <github.me.mzu5a@simplelogin.com>
2024-05-23 10:43:50 -0500

    chore: revert app.vue

--- packages/bootstrap-vue-next/src/App.vue
+++ packages/bootstrap-vue-next/src/App.vue
@@ -1,39 +1,7 @@
 <template>
   <BContainer>
     <BRow>
-      <BCol>
-        <BDropdown split text="Dropdown" variant="info">
-          <BDropdownItem>Action A</BDropdownItem>
-          <BDropdownItem>Action B</BDropdownItem>
-        </BDropdown>
-      </BCol>
-    </BRow>
-    <BRow>
-      <BCol class="mt-5">
-        <BDropdown text="Dropdown" variant="info">
-          <BDropdownItem>Action A</BDropdownItem>
-          <BDropdownItem>Action B</BDropdownItem>
-        </BDropdown>
-      </BCol></BRow
-    >
-    <BRow>
-      <BCol class="mt-5">
-        <BInputGroup>
-          <template #prepend>
-            <BDropdown text="Dropdown" variant="info">
-              <BDropdownItem>Action A</BDropdownItem>
-              <BDropdownItem>Action B</BDropdownItem>
-            </BDropdown>
-          </template>
-          <BFormInput />
-          <template #append>
-            <BDropdown v-for="i in 2" :key="i" text="Dropdown" variant="outline-secondary">
-              <BDropdownItem>Action C</BDropdownItem>
-              <BDropdownItem>Action D</BDropdownItem>
-            </BDropdown>
-          </template>
-        </BInputGroup>
-      </BCol>
+      <BCol> Hello World! </BCol>
     </BRow>
   </BContainer>
 </template>
