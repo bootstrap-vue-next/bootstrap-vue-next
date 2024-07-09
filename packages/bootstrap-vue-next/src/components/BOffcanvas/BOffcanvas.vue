bootstrap-vue-next/bootstrap-vue-next

commit 1e88b09d8992e13948e6f94278c7c51cf4960ec8

feat(BOffcanvas)!: rename prop backdrop to hideBackdrop to be more in line with standard of bmodal
feat(BModal)!: remove prop backdropVariant

feat(BModal)!: revert back to using the native `div.modal-backdrop`

feat(BOffcanvas)!: revert back to native `div.offcanvas-backdrop`

feat(BOffcanvas)!: remove prop backdropBlur and backdropVariant

feat(createBootstrap)!: remove automatic import of components and directives fixing an issue with createBootstrap bloating the project size. When components were unused, it bloated the project size -- ie not tree shaking when you only used the plugins

feat(createBootstrap)!: flatten the params object. The previous "plugins" key is now the top most level object => `{ plugins: { id: {} }` => `{ id: {} }` and so on

feat(createBootstrap)!: remove alias in createBootstrap, use alias in unplugin-vue-components resolver instead

feat!: remove the default export. Use named export `createBootstrap` instead

feat(modalController): make {} default for show/confirm -- param not required

feat(toast): make {} default for show -- param not required

packages/bootstrap-vue-next/src/components/BOffcanvas/BOffcanvas.vue
@@ -50,16 +50,8 @@
         </template>
       </div>
     </Transition>
-    <slot v-if="props.responsive === undefined || !isOpenByBreakpoint" name="backdrop">
-      <BOverlay
-        :blur="props.backdropBlur"
-        :variant="props.backdropVariant"
-        :show="showBackdrop"
-        fixed
-        no-wrap
-        no-spinner
-        @click="hide('backdrop')"
-      />
+    <slot v-if="showBackdrop" name="backdrop">
+      <div class="offcanvas-backdrop fade show" @click="hide('backdrop')" />
     </slot>
   </Teleport>
 </template>
@@ -78,7 +70,6 @@ import type {BOffcanvasProps} from '../../types'
 import {BvTriggerableEvent, isEmptySlot} from '../../utils'
 import BButton from '../BButton/BButton.vue'
 import BCloseButton from '../BButton/BCloseButton.vue'
-import BOverlay from '../BOverlay/BOverlay.vue'
 
 // TODO once the responsive stuff may be implemented correctly,
 // What needs to occur is a fixing of the "body scrolling".
@@ -93,9 +84,7 @@ defineOptions({
 })
 
 const _props = withDefaults(defineProps<BOffcanvasProps>(), {
-  backdrop: true,
-  backdropBlur: undefined,
-  backdropVariant: 'dark',
+  hideBackdrop: false,
   bodyAttrs: undefined,
   bodyClass: undefined,
   bodyScrolling: false,
@@ -185,9 +174,14 @@ const isActive = ref(modelValue.value)
 const lazyLoadCompleted = ref(false)
 const wasClosedByBreakpointChange = ref(false)
 
-const showBackdrop = toRef(() => props.backdrop === true && modelValue.value === true)
+const showBackdrop = computed(
+  () =>
+    (props.responsive === undefined || !isOpenByBreakpoint.value) &&
+    props.hideBackdrop === false &&
+    modelValue.value === true
+)
 
-const lazyShowing = toRef(
+const lazyShowing = computed(
   () =>
     props.lazy === false ||
     (props.lazy === true && lazyLoadCompleted.value === true) ||

.gitignore
@@ -95,3 +95,5 @@ TODO.md
 packages/bootstrap-vue-next/src/App.vue
 
 vite.config.mts.timestamp*
+
+components.d.ts

apps/docs/src/components/TableOfContentsNav.vue
@@ -213,10 +213,6 @@ const groupComputedList = computed(() => [
         name: 'Global Options',
         route: withBase('/docs/configurations/global-options'),
       },
-      {
-        name: 'Component Aliasing',
-        route: withBase('/docs/configurations/component-aliasing'),
-      },
     ],
   },
 ])

packages/bootstrap-vue-next/src/BootstrapVue.ts
@@ -1,12 +1,13 @@
 import type {Plugin} from 'vue'
-import type {BootstrapVueOptions, ComponentType, DirectiveType} from './types'
+import type {BootstrapVueOptions, ComponentType, ConfigurationOption, DirectiveType} from './types'
 import type {ComponentResolver} from 'unplugin-vue-components'
+import type * as Components from './components'
+
 import * as Plugins from './plugins'
+import {ComponentNames, DirectiveNames} from './types/ComponentProps'
 
 import './styles/styles.scss'
 
-import * as Components from './components'
-import * as Directives from './directives'
 import parseActiveImports from './utils/parseActiveImports'
 
 declare module '@vue/runtime-core' {
@@ -116,51 +117,24 @@ declare module '@vue/runtime-core' {
 const sliceName = (str: string) => (str.toLowerCase().startsWith('v') ? str.slice(1) : str)
 
 // Main app plugin
-export const createBootstrap = ({
-  components = false,
-  directives = false,
-  plugins: pluginData = {},
-  aliases = {},
-}: Readonly<BootstrapVueOptions> = {}): Plugin => ({
+export const createBootstrap = (pluginData: Readonly<BootstrapVueOptions> = {}): Plugin => ({
   install(app) {
-    const selectedComponents = typeof components === 'boolean' ? {all: components} : components
-
-    const componentKeys = Object.keys(Components) as unknown as ComponentType[]
-    parseActiveImports(selectedComponents, componentKeys).forEach((name) => {
-      const component = Components[name]
-      app.component(name, component)
-    })
-
-    const selectedDirectives = typeof directives === 'boolean' ? {all: directives} : directives
-
-    const directiveKeys = Object.keys(Directives) as unknown as DirectiveType[]
-    parseActiveImports(selectedDirectives, directiveKeys).forEach((name) => {
-      const parsedName = sliceName(name)
-      const directive = Directives[name]
-      app.directive(parsedName, directive)
-    })
-
-    Object.entries(aliases).forEach(([alias, name]) => {
-      const component = typeof name === 'string' ? Components[name] : name
-      app.component(alias, component)
-    })
-
-    if (pluginData?.breadcrumb ?? true === true) {
+    if (pluginData.breadcrumb ?? true === true) {
       app.use(Plugins.breadcrumb)
     }
-    if ((pluginData?.id ?? true === true) || typeof pluginData.id === 'object') {
+    if ((pluginData.id ?? true === true) || typeof pluginData.id === 'object') {
       app.use(Plugins.id, pluginData)
     }
-    if (pluginData?.modalController ?? true === true) {
+    if (pluginData.modalController ?? true === true) {
       app.use(Plugins.modalController)
     }
-    if (pluginData?.modalManager ?? true === true) {
+    if (pluginData.modalManager ?? true === true) {
       app.use(Plugins.modalManager)
     }
-    if ((pluginData?.rtl ?? true === true) || typeof pluginData.rtl === 'object') {
+    if ((pluginData.rtl ?? true === true) || typeof pluginData.rtl === 'object') {
       app.use(Plugins.rtl, pluginData)
     }
-    if (pluginData?.toast ?? true === true) {
+    if (pluginData.toast ?? true === true) {
       app.use(Plugins.toast)
     }
     app.use(Plugins.defaults, pluginData)
@@ -174,22 +148,23 @@ export const BootstrapVueNextResolver = ({
   aliases = {},
   directives = true,
   components = true,
-}: Readonly<
-  Pick<BootstrapVueOptions, 'directives' | 'components'> & {aliases?: Record<string, ComponentType>}
-> = {}): ComponentResolver[] => {
+}: Readonly<{
+  components?: boolean | ConfigurationOption<ComponentType>
+  directives?: boolean | ConfigurationOption<DirectiveType>
+  aliases?: Record<string, ComponentType>
+}> = {}): ComponentResolver[] => {
   const selectedComponents = typeof components === 'boolean' ? {all: components} : components
-  const componentKeys = Object.keys(Components) as unknown as ComponentType[]
-  const compImports = new Set(parseActiveImports(selectedComponents, componentKeys) as string[])
+  const compImports = new Set<string>(parseActiveImports(selectedComponents, ComponentNames))
 
   const selectedDirectives = typeof directives === 'boolean' ? {all: directives} : directives
-  const directiveKeys = Object.keys(Directives) as unknown as DirectiveType[]
-  const dirImports = new Set(parseActiveImports(selectedDirectives, directiveKeys).map(sliceName))
+  const dirImports = new Set<string>(
+    parseActiveImports(selectedDirectives, DirectiveNames).map(sliceName)
+  )
   const resolvers: ComponentResolver[] = [
     {
       type: 'component',
       resolve: (name) => {
         if (compImports.has(name) || aliases[name]) {
-          // TODO using this list of components we may be able to tree shake off CSS imports for the user :eyes:
           return {
             name: aliases[name] || name,
             from: 'bootstrap-vue-next',
@@ -224,5 +199,3 @@ export * from './types/exports'
 export * as Types from './types/exports'
 export * from './plugins'
 export * as Plugins from './plugins'
-
-export default createBootstrap

packages/bootstrap-vue-next/src/components/BModal/BModal.vue
@@ -91,16 +91,8 @@
             </div>
           </div>
         </div>
-        <slot name="backdrop">
-          <BOverlay
-            :variant="computedBackdropVariant"
-            :show="modelValue"
-            no-spinner
-            fixed
-            no-wrap
-            :blur="null"
-            @click="hideFn('backdrop')"
-          />
+        <slot v-if="!props.hideBackdrop" name="backdrop">
+          <div class="modal-backdrop fade show" @click="hideFn('backdrop')" />
         </slot>
       </div>
     </Transition>
@@ -123,7 +115,6 @@ import type {BModalProps} from '../../types'
 import {BvTriggerableEvent, isEmptySlot} from '../../utils'
 import BButton from '../BButton/BButton.vue'
 import BCloseButton from '../BButton/BCloseButton.vue'
-import BOverlay from '../BOverlay/BOverlay.vue'
 
 defineOptions({
   inheritAttrs: false,
@@ -139,7 +130,6 @@ defineOptions({
 const _props = withDefaults(defineProps<BModalProps>(), {
   autoFocus: true,
   autoFocusButton: undefined,
-  backdropVariant: undefined,
   body: undefined,
   bodyBgVariant: null,
   bodyAttrs: undefined,
@@ -290,14 +280,6 @@ const lazyShowing = toRef(
     props.lazy === false ||
     (props.lazy === true && lazyLoadCompleted.value === true) ||
     (props.lazy === true && modelValue.value === true)
-)
-
-const computedBackdropVariant = toRef(() =>
-  props.backdropVariant !== undefined
-    ? props.backdropVariant
-    : props.hideBackdrop
-      ? 'transparent'
-      : 'dark'
 )
 
 const hasHeaderCloseSlot = toRef(() => !isEmptySlot(slots['header-close']))

packages/bootstrap-vue-next/src/main.ts
@@ -46,12 +46,4 @@ const router = createRouter({
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './styles/styles.scss'
 
-createApp(App)
-  .use(
-    createBootstrap({
-      components: true,
-      directives: true,
-    })
-  )
-  .use(router)
-  .mount('#app')
+createApp(App).use(createBootstrap()).use(router).mount('#app')

packages/bootstrap-vue-next/src/plugins/defaultsPlugin.ts
@@ -3,7 +3,7 @@ import type {BootstrapVueOptions, BvnComponentProps} from '../types'
 import {defaultsKey} from '../utils'
 
 export default {
-  install(app, options: BootstrapVueOptions['plugins']) {
+  install(app, options: BootstrapVueOptions) {
     const val = options?.components ?? {}
     app.provide(defaultsKey, ref(val) as Ref<Partial<BvnComponentProps>>)
   },

packages/bootstrap-vue-next/src/plugins/idPlugin.ts
@@ -3,7 +3,7 @@ import type {BootstrapVueOptions} from '../types'
 import {idPluginKey} from '../utils'
 
 export default {
-  install(app, options: BootstrapVueOptions['plugins']) {
+  install(app, options: BootstrapVueOptions) {
     if (!(options?.id instanceof Object && typeof options.id.getId === 'function')) return
 
     app.provide(idPluginKey, options.id.getId)

commit 4ec74b61e5bea60eb5b93b84745b2c98d79eedcb

fix(BOffcanvas): Fix regression in docs caused  by #1992

packages/bootstrap-vue-next/src/components/BOffcanvas/BOffcanvas.vue
@@ -51,7 +51,7 @@
       </div>
     </Transition>
     <slot v-if="showBackdrop" name="backdrop">
-      <div class="offcanvas-backdrop fade show" @click="hide('backdrop')" />
+      <BOverlay fixed no-wrap no-spinner @click="hide('backdrop')" />
     </slot>
   </Teleport>
 </template>
@@ -70,6 +70,7 @@ import type {BOffcanvasProps} from '../../types'
 import {BvTriggerableEvent, isEmptySlot} from '../../utils'
 import BButton from '../BButton/BButton.vue'
 import BCloseButton from '../BButton/BCloseButton.vue'
+import BOverlay from '../BOverlay/BOverlay.vue'
 
 // TODO once the responsive stuff may be implemented correctly,
 // What needs to occur is a fixing of the "body scrolling".