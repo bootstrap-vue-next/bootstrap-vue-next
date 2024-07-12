af88e861435f5655f34b550459f9f92bc431170f
matsaal <91875598+matsaal@users.noreply.github.com>
2024-07-12 21:54:22 +0100

    docs(BBadge): update component documentation

--- apps/docs/src/data/components/badge.data.ts
+++ apps/docs/src/data/components/badge.data.ts
@@ -26,6 +26,98 @@ export default {
             default: false,
             description: 'Indication position and dot styling applied',
           },
+          placement: {
+            type: 'CombinedPlacement',
+            default: undefined,
+          },
+          active: {
+            type: 'boolean',
+            default: undefined,
+          },
+          activeClass: {
+            type: 'string',
+            default: undefined,
+          },
+          disabled: {
+            type: 'boolean',
+            default: undefined,
+          },
+          exactActiveClass: {
+            type: 'string',
+            default: undefined,
+          },
+          href: {
+            type: 'string',
+            default: undefined,
+          },
+          icon: {
+            type: 'boolean',
+            default: undefined,
+          },
+          opacity: {
+            type: '10 | 25 | 50 | 75 | 100 | "10" | "25" | "50" | "75" | "100"',
+            default: undefined,
+          },
+          opacityHover: {
+            type: '10 | 25 | 50 | 75 | 100 | "10" | "25" | "50" | "75" | "100"',
+            default: undefined,
+          },
+          rel: {
+            type: 'string',
+            default: undefined,
+          },
+          replace: {
+            type: 'boolean',
+            default: undefined,
+          },
+          routerComponentName: {
+            type: 'string',
+            default: undefined,
+          },
+          stretched: {
+            type: 'boolean',
+            default: false,
+          },
+          target: {
+            type: 'LinkTarget',
+            default: undefined,
+          },
+          to: {
+            type: 'RouteLocationRaw',
+            default: undefined,
+          },
+          underlineOffset: {
+            type: '1 | 2 | 3 | "1" | "2" | "3"',
+            default: undefined,
+          },
+          underlineOffsetHover: {
+            type: '1 | 2 | 3 | "1" | "2" | "3"',
+            default: undefined,
+          },
+          underlineOpacity: {
+            type: '0 | 10 | 25 | 50 | 75 | 100 | "0" | "10" | "25" | "50" | "75" | "100"',
+            default: undefined,
+          },
+          underlineOpacityHover: {
+            type: '0 | 10 | 25 | 50 | 75 | 100 | "0" | "10" | "25" | "50" | "75" | "100"',
+            default: undefined,
+          },
+          underlineVariant: {
+            type: 'ColorVariant | null',
+            default: undefined,
+          },
+          variant: {
+            type: 'ColorVariant | null',
+            default: 'secondary',
+          },
+          bgVariant: {
+            type: 'ColorVariant | null',
+            default: null,
+          },
+          textVariant: {
+            type: 'TextColorVariant | null',
+            default: null,
+          },
         },
       },
       emits: [],
