9f051ea087140462fb1a108132d4d338a611381f
Issayah <github.me.mzu5a@simplelogin.com>
2024-07-21 11:47:15 -0500

    fix(focus-trap): focus trap issues with modal and offcanvas fixes #2064

--- packages/bootstrap-vue-next/src/composables/useActivatedFocusTrap.ts
+++ packages/bootstrap-vue-next/src/composables/useActivatedFocusTrap.ts
@@ -1,35 +1,36 @@
-import {type MaybeRefOrGetter, onMounted, readonly, type Ref, toRef, watch} from 'vue'
-import {useFocusTrap} from '@vueuse/integrations/useFocusTrap'
+import {type MaybeRefOrGetter, nextTick, readonly, type Ref, toRef, watch} from 'vue'
+import {useFocusTrap, type UseFocusTrapOptions} from '@vueuse/integrations/useFocusTrap'
 
-export const useActivatedFocusTrap = ({
-  element,
-  isActive,
-  noTrap,
-}: {
-  element: Ref<HTMLElement | null>
-  isActive: MaybeRefOrGetter<boolean>
-  noTrap: MaybeRefOrGetter<boolean>
-}) => {
+export const useActivatedFocusTrap = (
+  {
+    element,
+    isActive,
+    noTrap,
+  }: {
+    element: Ref<HTMLElement | null>
+    isActive: MaybeRefOrGetter<boolean>
+    noTrap: MaybeRefOrGetter<boolean>
+  },
+  focusTrapOpts: UseFocusTrapOptions = {
+    allowOutsideClick: true,
+  }
+) => {
   const resolvedIsActive = readonly(toRef(isActive))
   const resolvedNoTrap = readonly(toRef(noTrap))
 
-  onMounted(() => {
-    const trap = useFocusTrap(element, {
-      allowOutsideClick: true,
-      fallbackFocus: window?.document?.body,
-    })
-    watch(resolvedIsActive, (newValue) => {
-      if (newValue && resolvedNoTrap.value === false) {
-        trap.activate()
-      } else {
-        trap.deactivate()
-      }
-    })
+  const trap = useFocusTrap(element, focusTrapOpts)
+  watch(resolvedIsActive, async (newValue) => {
+    await nextTick()
+    if (newValue && resolvedNoTrap.value === false) {
+      trap.activate()
+    } else {
+      trap.deactivate()
+    }
+  })
 
-    watch(resolvedNoTrap, (newValue) => {
-      if (newValue === true) {
-        trap.deactivate()
-      }
-    })
+  watch(resolvedNoTrap, (newValue) => {
+    if (newValue === true) {
+      trap.deactivate()
+    }
   })
 }
