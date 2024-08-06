7e49c7d7db16a1b72096921dfb9416016f7127a0
Issayah <github.me.mzu5a@simplelogin.com>
2024-08-06 09:26:18 -0500

    chore: revert app.vue

--- packages/bootstrap-vue-next/src/App.vue
+++ packages/bootstrap-vue-next/src/App.vue
@@ -1,40 +1,12 @@
 <template>
-  <div class="comp">
-    <BTable striped :fields="fields" :items="items" />
-  </div>
+  <BContainer>
+    <BRow>
+      <BCol> Hello World! </BCol>
+    </BRow>
+  </BContainer>
 </template>
+
 <script setup lang="ts">
-import {ref} from 'vue'
-
-const fields = ref([
-  {
-    key: 'id',
-    tdClass: 'lightblue',
-  },
-  {
-    key: 'name',
-    tdClass: () => 'light---blue',
-  },
-])
-
-const items = ref([
-  {
-    id: '1',
-    name: 'One',
-  },
-  {
-    id: '2',
-    name: 'Two',
-  },
-])
+// You can use this file as a development spot to test your changes
+// Please do not commit this file
 </script>
-
-<style lang="scss" scoped>
-.comp {
-  margin: 20px;
-  :deep(.lightblue) {
-    color: lightblue;
-    font-weight: 700;
-  }
-}
-</style>
