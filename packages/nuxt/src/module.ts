77bd9988bc593fd6007672397185cd213af9a73b
Ezequiel Dias <ezequidias@outlook.com>
2024-08-13 15:32:15 -0300

    chore: indicate compatibility with nuxt v4

--- packages/nuxt/src/module.ts
+++ packages/nuxt/src/module.ts
@@ -10,7 +10,7 @@ export default defineNuxtModule<ModuleOptions>({
     name: 'bootstrap-vue-next',
     configKey: 'bootstrapVueNext',
     compatibility: {
-      nuxt: '^3.0.0',
+      nuxt: '>=3.0.0',
       bridge: false,
     },
   },
