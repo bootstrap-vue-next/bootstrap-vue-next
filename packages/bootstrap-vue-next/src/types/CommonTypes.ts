5c30050599f55ca883fc2bb2329f3e33b269f320
Issayah <github.me.mzu5a@simplelogin.com>
2024-08-20 17:23:41 -0500

    Update packages/bootstrap-vue-next/src/types/CommonTypes.ts

--- packages/bootstrap-vue-next/src/types/CommonTypes.ts
+++ packages/bootstrap-vue-next/src/types/CommonTypes.ts
@@ -7,24 +7,3 @@ export type TeleporterProps = {
   teleportDisabled?: TeleportProps['disabled']
   teleportTo?: TeleportProps['to']
 }
-
-export interface LinkUnderlineProps {
-  underlineOffset?: 1 | 2 | 3 | '1' | '2' | '3'
-  underlineOffsetHover?: 1 | 2 | 3 | '1' | '2' | '3'
-  underlineOpacity?: 0 | 10 | 25 | 50 | 75 | 100 | '0' | '10' | '25' | '50' | '75' | '100'
-  underlineOpacityHover?: 0 | 10 | 25 | 50 | 75 | 100 | '0' | '10' | '25' | '50' | '75' | '100'
-  underlineVariant?: ColorVariant | null
-}
-
-export interface LinkOpacityProps {
-  opacity?: 10 | 25 | 50 | 75 | 100 | '10' | '25' | '50' | '75' | '100'
-  opacityHover?: 10 | 25 | 50 | 75 | 100 | '10' | '25' | '50' | '75' | '100'
-}
-
-export interface LinkIconProps {
-  icon?: boolean
-}
-
-export interface LinkVariantProps {
-  variant?: ColorVariant | null
-}
