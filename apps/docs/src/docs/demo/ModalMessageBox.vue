f7aee137d02ac973110abe01ee33de0d7647eb6e
David W. Gray <dwgray67@hotmail.com>
2024-09-12 15:20:08 -0700

    docs: Migration Guide refactor (#2186)

---
+++ apps/docs/src/docs/demo/ModalMessageBox.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <BButton @click="okBox">Show Message</BButton>
+    <div>Result: {{ okResult }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {useModalController} from 'bootstrap-vue-next'
+import {ref} from 'vue'
+
+const {show} = useModalController()
+
+const okResult = ref<boolean | null | undefined>(undefined)
+
+const okBox = async () => {
+  okResult.value = await show?.({
+    props: {
+      body: 'This is an informational message',
+      title: 'Message',
+      okOnly: true,
+    },
+  })
+}
+</script>
