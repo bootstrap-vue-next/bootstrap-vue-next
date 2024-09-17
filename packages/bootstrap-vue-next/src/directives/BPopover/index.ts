99698a42170be7962d14f9c9bb5166e6dc89735c
Jukka Raimovaara <roska@mentalhouse.fi>
2024-09-17 19:50:35 +0300

    fix(vBPopover): fix vue error with floating directives.

--- packages/bootstrap-vue-next/src/directives/BPopover/index.ts
+++ packages/bootstrap-vue-next/src/directives/BPopover/index.ts
@@ -10,25 +10,23 @@ import {
 import {defaultsKey} from '../../utils/keys'
 
 export const vBPopover: Directive<ElementWithPopper> = {
-  mounted(el, binding, vnode) {
-    // @ts-expect-error type doesn't have runtime ctx
-    const defaults = vnode.ctx?.appContext?.provides?.[defaultsKey as symbol]?.value
+  mounted(el, binding) {
+    const defaults = binding.instance?.$.appContext?.provides?.[defaultsKey as symbol]?.value
     const isActive = resolveActiveStatus(binding.value)
     if (!isActive) return
 
     const text = resolveContent(binding.value, el)
 
     if (!text.content && !text.title) return
-    el.$__binding = JSON.stringify(binding)
+    el.$__binding = JSON.stringify([binding.modifiers, binding.value])
     bind(el, binding, {
       ...(defaults['BPopover'] || {}),
       ...resolveDirectiveProps(binding, el),
       ...text,
     })
   },
-  updated(el, binding, vnode) {
-    // @ts-expect-error type doesn't have runtime ctx
-    const defaults = vnode.ctx?.appContext?.provides?.[defaultsKey as symbol]?.value
+  updated(el, binding) {
+    const defaults = binding.instance?.$.appContext?.provides?.[defaultsKey as symbol]?.value
 
     const isActive = resolveActiveStatus(binding.value)
     if (!isActive) return
@@ -37,14 +35,14 @@ export const vBPopover: Directive<ElementWithPopper> = {
 
     if (!text.content && !text.title) return
     delete binding.oldValue
-    if (el.$__binding === JSON.stringify(binding)) return
+    if (el.$__binding === JSON.stringify([binding.modifiers, binding.value])) return
     unbind(el)
     bind(el, binding, {
       ...(defaults['BPopover'] || {}),
       ...resolveDirectiveProps(binding, el),
       ...text,
     })
-    el.$__binding = JSON.stringify(binding)
+    el.$__binding = JSON.stringify([binding.modifiers, binding.value])
   },
   beforeUnmount(el) {
     unbind(el)
