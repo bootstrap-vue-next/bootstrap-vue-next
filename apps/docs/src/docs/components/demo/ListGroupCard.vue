26dc877b03a889962616d8eaf4b42dea76dc40bd
David W. Gray <dwgray67@hotmail.com>
2024-10-16 13:32:34 -0700

    docs(BFormListGroup): Refactor and fill out Component reference

---
+++ apps/docs/src/docs/components/demo/ListGroupCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <!-- #region template -->
+  <BCardGroup deck>
+    <BCard header="Card with list group">
+      <BListGroup>
+        <BListGroupItem href="#">Cras justo odio</BListGroupItem>
+        <BListGroupItem href="#">Dapibus ac facilisis in</BListGroupItem>
+        <BListGroupItem href="#">Vestibulum at eros</BListGroupItem>
+      </BListGroup>
+      <p class="card-text mt-2">
+        Quis magna Lorem anim amet ipsum do mollit sit cillum voluptate ex nulla tempor. Laborum
+        consequat non elit enim exercitation cillum aliqua consequat id aliqua. Esse ex consectetur
+        mollit voluptate est in duis laboris ad sit ipsum anim Lorem.
+      </p>
+    </BCard>
+    <BCard no-body header="Card with flush list group">
+      <BListGroup flush>
+        <BListGroupItem href="#">Cras justo odio</BListGroupItem>
+        <BListGroupItem href="#">Dapibus ac facilisis in</BListGroupItem>
+        <BListGroupItem href="#">Vestibulum at eros</BListGroupItem>
+      </BListGroup>
+      <BCardBody>
+        Quis magna Lorem anim amet ipsum do mollit sit cillum voluptate ex nulla tempor. Laborum
+        consequat non elit enim exercitation cillum aliqua consequat id aliqua. Esse ex consectetur
+        mollit voluptate est in duis laboris ad sit ipsum anim Lorem.
+      </BCardBody>
+    </BCard>
+  </BCardGroup>
+  <!-- #endregion template -->
+</template>
---
+++ apps/docs/src/docs/components/demo/ListGroupHorizontalMd.vue
@@ -0,0 +1,9 @@
+<template>
+  <!-- #region template -->
+  <BListGroup horizontal>
+    <BListGroupItem>Cras justo odio</BListGroupItem>
+    <BListGroupItem>Dapibus ac facilisis in</BListGroupItem>
+    <BListGroupItem>Morbi leo risus</BListGroupItem>
+  </BListGroup>
+  <!-- #endregion template -->
+</template>
