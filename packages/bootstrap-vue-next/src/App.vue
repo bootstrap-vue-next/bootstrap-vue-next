300374b3164e84932c0c9283f0bcf57265342ba1
Issayah <github.me.mzu5a@simplelogin.com>
2024-10-18 11:01:36 -0500

    chore: revert app.vue

--- packages/bootstrap-vue-next/src/App.vue
+++ packages/bootstrap-vue-next/src/App.vue
@@ -1,29 +1,7 @@
 <template>
   <BContainer>
     <BRow>
-      <BCol>
-        <b-form-textarea
-          id="textarea-no-auto-shrink"
-          placeholder="Auto height (no-shrink) textarea"
-          rows="1"
-          max-rows="8"
-        />
-      </BCol>
-    </BRow>
-    <BRow>
-      <BCol>
-        <BFormCheckbox v-model="intermChecked" v-model:indeterminate="indeterminate"
-          >Click me to see what happens</BFormCheckbox
-        >
-        <BButton class="mt-2" :disabled="indeterminate" @click="indeterminate = true"
-          >Reset Indeterminate</BButton
-        >
-        <div class="mt-2">
-          Checked: <strong>{{ intermChecked }}</strong
-          ><br />
-          Indeterminate: <strong>{{ indeterminate }}</strong>
-        </div>
-      </BCol>
+      <BCol> Hello World! </BCol>
     </BRow>
   </BContainer>
 </template>
@@ -31,8 +9,4 @@
 <script setup lang="ts">
 // You can use this file as a development spot to test your changes
 // Please do not commit this file
-import {ref} from 'vue'
-
-const intermChecked = ref(true)
-const indeterminate = ref(true)
 </script>
