1b2468cea2342478c5216c2bb1bff4fa37a5cc71
David W. Gray <dwgray67@hotmail.com>
2024-10-18 17:25:02 -0700

    docs(grid): Parity pass (#2291)
    * doc(grid): Fill out documentation
    
    * docs(grid)): Fill out component reference
    
    * docs(grid): fix typo

---
+++ apps/docs/src/docs/components/demo/ColOrder.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- #region template -->
+  <BContainer fluid class="bv-example-row">
+    <BRow class="mb-3">
+      <BCol>First in DOM, no order applied</BCol>
+      <BCol order="5">Second in DOM, with a larger order</BCol>
+      <BCol order="1">Third in DOM, with an order of 1</BCol>
+    </BRow>
+
+    <BRow class="mb-3">
+      <BCol order="5">First in DOM, with order of 65</BCol>
+      <BCol order="1">Second in DOM, with an order of 1</BCol>
+      <BCol>Third in DOM, no order applied</BCol>
+    </BRow>
+  </BContainer>
+  <!-- #endregion template -->
+</template>
---
+++ apps/docs/src/docs/components/demo/ColVariableWidth.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- #region template -->
+  <BContainer class="bv-example-row">
+    <BRow class="justify-content-md-center">
+      <BCol col lg="2">1 of 3</BCol>
+      <BCol cols="12" md="auto">Variable width content</BCol>
+      <BCol col lg="2">3 of 3</BCol>
+    </BRow>
+
+    <BRow>
+      <BCol>1 of 3</BCol>
+      <BCol cols="12" md="auto">Variable width content</BCol>
+      <BCol col lg="2">3 of 3</BCol>
+    </BRow>
+  </BContainer>
+  <!-- #endregion template -->
+</template>
