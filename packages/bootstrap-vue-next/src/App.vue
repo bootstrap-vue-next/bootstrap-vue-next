f307560a58fddc07ab31d32840c5dce3060784ca
Issayah <github.me.mzu5a@simplelogin.com>
2024-11-16 17:35:58 -0600

    chore: revert app.vue

--- packages/bootstrap-vue-next/src/App.vue
+++ packages/bootstrap-vue-next/src/App.vue
@@ -1,30 +1,7 @@
 <template>
   <BContainer>
     <BRow>
-      <BCol>
-        <b-navbar toggleable>
-          <b-navbar-toggle :target="['navbar-toggle-collapse', 'foobar']">
-            <template #default="{expanded}">
-              {{ expanded }}
-            </template>
-          </b-navbar-toggle>
-
-          <b-collapse id="navbar-toggle-collapse" is-nav>
-            <b-navbar-nav class="ml-auto">
-              <b-nav-item href="#">Link 1</b-nav-item>
-              <b-nav-item href="#">Link 2</b-nav-item>
-              <b-nav-item href="#" disabled>Disabled</b-nav-item>
-            </b-navbar-nav>
-          </b-collapse>
-          <b-collapse id="foobar" is-nav>
-            <b-navbar-nav class="ml-auto">
-              <b-nav-item href="#">Link 1</b-nav-item>
-              <b-nav-item href="#">Link 2</b-nav-item>
-              <b-nav-item href="#" disabled>Disabled</b-nav-item>
-            </b-navbar-nav>
-          </b-collapse>
-        </b-navbar>
-      </BCol>
+      <BCol> Hello World! </BCol>
     </BRow>
   </BContainer>
 </template>
@@ -32,5 +9,5 @@
 <script setup lang="ts">
 // You can use this file as a development spot to test your changes
 // Please do not commit this file
-import {BCol, BCollapse, BContainer, BNavbarNav, BNavbarToggle, BNavItem, BRow} from './components'
+import {BCol, BContainer, BRow} from './components'
 </script>
