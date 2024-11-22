22baf841fcaf583976a999cd4836a6d9c99dacd5
David W. Gray <dwgray67@hotmail.com>
2024-11-22 10:11:30 -0800

    doc(BTabs): Fix laxy and customlink examples

--- apps/docs/src/docs/components/demo/TabsCustomLinks.vue
+++ apps/docs/src/docs/components/demo/TabsCustomLinks.vue
@@ -1,18 +1,21 @@
 <template>
   <BCard no-body>
     <BTabs v-model="tabIndex" card>
-      <BTab title="Tab 1" :title-link-class="linkClass(0)">Tab contents 1</BTab>
-      <BTab title="Tab 2" :title-link-class="linkClass(1)">Tab contents 2</BTab>
-      <BTab title="Tab 3" :title-link-class="linkClass(2)">Tab contents 3</BTab>
+      <BTab title="Tab 1" :title-link-class="linkClass[0]">Tab contents 1</BTab>
+      <BTab title="Tab 2" :title-link-class="linkClass[1]">Tab contents 2</BTab>
+      <BTab title="Tab 3" :title-link-class="linkClass[2]">Tab contents 3</BTab>
     </BTabs>
   </BCard>
 </template>
 
 <script setup lang="ts">
-import {ref} from 'vue'
+import {computed, ref} from 'vue'
 
 const tabIndex = ref(0)
 
-const linkClass = (idx: number) =>
-  tabIndex.value === idx ? ['bg-primary', 'text-light'] : ['bg-light', 'text-info']
+const linkClass = computed(() =>
+  Array.from(Array(3).keys()).map((idx) =>
+    tabIndex.value === idx ? ['bg-primary', 'text-light'] : ['bg-light', 'text-info']
+  )
+)
 </script>
--- apps/docs/src/docs/components/demo/TabsLazy.vue
+++ apps/docs/src/docs/components/demo/TabsLazy.vue
@@ -2,11 +2,11 @@
   <!-- #region template -->
   <BTabs content-class="mt-3">
     <!-- This tabs content will always be mounted -->
-    <BTab title="Regular tab"><BAlert show>I'm always mounted</BAlert></BTab>
+    <BTab title="Regular tab"><BAlert :model-value="true">I'm always mounted</BAlert></BTab>
 
     <!-- This tabs content will not be mounted until the tab is shown -->
     <!-- and will be un-mounted when hidden -->
-    <BTab title="Lazy tab" lazy><BAlert show>I'm lazy mounted!</BAlert></BTab>
+    <BTab title="Lazy tab" lazy><BAlert :model-value="true">I'm lazy mounted!</BAlert></BTab>
   </BTabs>
   <!-- #endregion template -->
 </template>
