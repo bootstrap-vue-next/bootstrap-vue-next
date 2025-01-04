6d3e33dc51139e203b4f2b7cd4d8a72722b04252
David W. Gray <dwgray67@hotmail.com>
2025-01-03 16:59:26 -0800

    docs(BFormGroup): Refactor examples into separate files

---
+++ apps/docs/src/docs/components/demo/FormGroupHorizontalLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- #region template -->
+  <BFormGroup
+    id="fieldset-horizontal"
+    label-cols-sm="4"
+    label-cols-lg="3"
+    content-cols-sm
+    content-cols-lg="7"
+    description="Let us know your name."
+    label="Enter your name"
+    label-for="input-horizontal"
+  >
+    <BFormInput id="input-horizontal" />
+  </BFormGroup>
+  <!-- #endregion template -->
+</template>
