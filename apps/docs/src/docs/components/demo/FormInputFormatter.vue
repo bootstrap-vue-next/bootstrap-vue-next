eedb26f416ede5769a42a153d66a60944b4a6408
David W. Gray <dwgray67@hotmail.com>
2025-01-07 15:00:35 -0800

    docs(BFormInput): refactor examples

---
+++ apps/docs/src/docs/components/demo/FormInputFormatter.vue
@@ -0,0 +1,33 @@
+<template>
+  <div role="group">
+    <label for="input-formatter">Text input with formatter (on input)</label>
+    <BFormInput
+      id="input-formatter"
+      v-model="text"
+      placeholder="Enter your name"
+      :formatter="toLowerCaseFormatter"
+    />
+    <p><b>Value:</b> {{ text }}</p>
+  </div>
+
+  <div role="group">
+    <label for="input-formatter">Text input with lazy formatter (on blur)</label>
+    <BFormInput
+      id="input-lazy"
+      v-model="lazyText"
+      placeholder="Enter your name"
+      lazy-formatter
+      :formatter="toLowerCaseFormatter"
+    />
+    <p><b>Value:</b> {{ lazyText }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {ref} from 'vue'
+
+const text = ref('')
+const lazyText = ref('')
+
+const toLowerCaseFormatter = (value: string) => value.toLowerCase()
+</script>
---
+++ apps/docs/src/docs/components/demo/FormInputState.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- #region template -->
+  <BRow class="my-1">
+    <BCol sm="3">
+      <label for="input-none">No State:</label>
+    </BCol>
+    <BCol sm="9">
+      <BFormInput id="input-none" :state="null" placeholder="No validation" />
+    </BCol>
+  </BRow>
+  <BRow class="my-1">
+    <BCol sm="3">
+      <label for="input-valid">Valid State:</label>
+    </BCol>
+    <BCol sm="9">
+      <BFormInput id="input-valid" :state="true" placeholder="Valid input" />
+    </BCol>
+  </BRow>
+  <BRow class="my-1">
+    <BCol sm="3">
+      <label for="input-invalid">Invalid State:</label>
+    </BCol>
+    <BCol sm="9">
+      <BFormInput id="input-invalid" :state="false" placeholder="Invalid input" />
+    </BCol>
+  </BRow>
+  <!-- #endregion template -->
+</template>
