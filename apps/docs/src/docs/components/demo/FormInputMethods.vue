6b6f9d6f0a54691cf95b134bfa100203bab7d54e
David W. Gray <dwgray67@hotmail.com>
2025-01-12 17:47:18 -0800

    docs(BFormInput): refactor examples (#2498)

---
+++ apps/docs/src/docs/components/demo/FormInputMethods.vue
@@ -0,0 +1,25 @@
+<template>
+  <div role="group">
+    <BFormInput ref="inputRef" v-model="value" placeholder="Enter your name" />
+  </div>
+  <div class="mt-2">
+    <BButton primary @click="selectAllText">Select all text</BButton>
+  </div>
+  <div class="mt-2">
+    <BButton primary @click="inputRef?.focus">Set Focus</BButton>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {ref} from 'vue'
+import {type BFormInput} from 'bootstrap-vue-next'
+
+// refs are unified in vue3. We need a ref variable with the same name as used in the template.
+// The variable will be filled up during mount with the reference to custom component.
+// inputRef will become the reference to the b-form-input component.
+const inputRef = ref<InstanceType<typeof BFormInput> | null>(null)
+const value = ref('sample text')
+
+// The inner native input is exposed as ref with name "element"
+const selectAllText = () => inputRef?.value?.element?.select()
+</script>
