6baf6149780e883fa193f781f0edb11eb0eafd19
David W. Gray <dwgray67@hotmail.com>
2025-01-12 17:02:51 -0800

    doc: Fix more typescript errors (#2495)
    docs(BTable): Fix type check errors in examples
    docs(BToast): Fix type errors in data

--- packages/bootstrap-vue-next/src/types/index.ts
+++ packages/bootstrap-vue-next/src/types/index.ts
@@ -45,6 +45,7 @@ export type {
 } from './SelectTypes'
 export type {InputType} from './InputType'
 export type {LinkTarget} from './LinkTarget'
+export type {LiteralUnion} from './LiteralUnion'
 export type {PaginationPage} from './PaginationPage'
 export type {PlaceholderAnimation, PlaceholderSize} from './PlaceholderTypes'
 export type {PopoverPlacement} from './PopoverPlacement'
