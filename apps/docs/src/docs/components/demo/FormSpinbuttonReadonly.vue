767fd6d01baa272c0976709eb8921350c93ab91a
David W. Gray <dwgray67@hotmail.com>
2025-01-12 17:52:43 -0800

    docs(BFormSpinbutton): Refactor examples (#2502)

---
+++ apps/docs/src/docs/components/demo/FormSpinbuttonReadonly.vue
@@ -0,0 +1,14 @@
+<template>
+  <!-- #region template -->
+  <BRow>
+    <BCol md="6" class="mb-2">
+      <label for="sb-disabled">Disabled spin button</label>
+      <BFormSpinbutton id="sb-disabled" disabled placeholder="--" />
+    </BCol>
+    <BCol md="6" class="mb-2">
+      <label for="sb-readonly" class="">Readonly spin button</label>
+      <BFormSpinbutton id="sb-readonly" readonly placeholder="--" />
+    </BCol>
+  </BRow>
+  <!-- #endregion template -->
+</template>
