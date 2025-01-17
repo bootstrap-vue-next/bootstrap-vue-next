ebcdd22097022d4fc478b750a7ebc5efe15bcc26
David W. Gray <dwgray67@hotmail.com>
2025-01-17 10:08:30 -0800

    docs(BForm): Refactor examples (#2514)

---
+++ apps/docs/src/docs/components/demo/FormDataHelper.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <label for="input-with-list">Input with datalist</label>
+    <BFormInput id="input-with-list" list="input-list" />
+    <BFormDatalist id="input-list" :options="datalistOptions" />
+  </div>
+</template>
+
+<script setup lang="ts">
+const datalistOptions = ['Apple', 'Banana', 'Grape', 'Kiwi', 'Orange']
+</script>
