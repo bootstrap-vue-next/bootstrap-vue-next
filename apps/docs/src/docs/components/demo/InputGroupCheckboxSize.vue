4444f9937cdf6e1716e4973541739a0c65c9320a
David W. Gray <dwgray67@hotmail.com>
2025-01-25 17:04:29 -0800

    docs(BInputGroup): Refactor examples

---
+++ apps/docs/src/docs/components/demo/InputGroupCheckboxSize.vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- #region template -->
+  <BInputGroup size="sm" prepend="Small" class="mb-2">
+    <BFormInput aria-label="Small text input with custom switch" />
+    <BInputGroupText>
+      <BFormCheckbox switch class="me-n2 mb-n1">
+        <span class="visually-hidden">Checkbox for previous text input</span>
+      </BFormCheckbox>
+    </BInputGroupText>
+  </BInputGroup>
+  <BInputGroup size="lg" prepend="Large" class="mb-2">
+    <BFormInput aria-label="Large text input with switch" />
+    <BInputGroupText>
+      <BFormCheckbox switch class="me-n2">
+        <span class="visually-hidden">Switch for previous text input</span>
+      </BFormCheckbox>
+    </BInputGroupText>
+  </BInputGroup>
+  <!-- #endregion template -->
+</template>
---
+++ apps/docs/src/docs/components/demo/InputGroupXPend.vue
@@ -0,0 +1,10 @@
+<template>
+  <!-- #region template -->
+  <BInputGroup prepend="$" append=".00">
+    <BFormInput />
+  </BInputGroup>
+  <BInputGroup prepend="0" append="100" class="mt-3">
+    <BFormInput type="range" min="0" max="100" />
+  </BInputGroup>
+  <!-- #endregion template -->
+</template>
