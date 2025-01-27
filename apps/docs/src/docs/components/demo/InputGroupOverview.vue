51c767d97936acfb4465c504676a58138e7ff573
David W. Gray <dwgray67@hotmail.com>
2025-01-27 11:46:21 -0800

    docs(BInputGroup): Refactor examples (#2529)

---
+++ apps/docs/src/docs/components/demo/InputGroupOverview.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- #region template -->
+  <!-- Using props -->
+  <BInputGroup size="lg" prepend="$" append=".00">
+    <BFormInput />
+  </BInputGroup>
+  <!-- Using slots -->
+  <BInputGroup class="mt-3">
+    <template #append>
+      <BInputGroupText><strong class="text-danger">!</strong></BInputGroupText>
+    </template>
+    <BFormInput />
+  </BInputGroup>
+  <!-- Using components -->
+  <BInputGroup prepend="Username" class="mt-3">
+    <BFormInput />
+    <BButton variant="outline-success">Button</BButton>
+    <BButton variant="info">Button</BButton>
+  </BInputGroup>
+  <!-- #endregion template -->
+</template>
