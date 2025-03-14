ffc408a1b94f9b93b291876c0eea3628bd2808c4
David W. Gray <dwgray67@hotmail.com>
2025-03-14 07:15:27 -0700

    docs: point users to migration guide from getting started (#2594)

--- apps/docs/src/components/NotYetDocumented.vue
+++ apps/docs/src/components/NotYetDocumented.vue
@@ -1,8 +1,10 @@
 <template>
-  This {{ type }} is either not documented or has not been through full parity review. Please use
-  with caution and refer to the code to find differences in behavior. We are working to bring the
-  migration guide and documenation up-to-date with the code. If you would like to help, please refer
-  to the
+  <span v-if="!helpOnly">
+    This {{ type }} is either not documented or has not been through full parity review. Please use
+    with caution and refer to the code to find differences in behavior. We are working to bring the
+    migration guide and documenation up-to-date with the code.
+  </span>
+  If you would like to help, please refer to the
   <a
     href="https://github.com/bootstrap-vue-next/bootstrap-vue-next/blob/main/CONTRIBUTING.md#improving-the-documentation"
   >
@@ -23,5 +25,6 @@
 <script setup lang="ts">
 defineProps<{
   type?: string
+  helpOnly?: boolean
 }>()
 </script>
