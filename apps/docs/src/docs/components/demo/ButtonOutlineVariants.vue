f5d4be16de3667079e245fd94da49de2fa2890d6
David W. Gray <dwgray67@hotmail.com>
2025-04-08 16:23:24 -0700

    docs(BButton): Outline variant example (#2639)

---
+++ apps/docs/src/docs/components/demo/ButtonOutlineVariants.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="d-flex gap-2">
+    <!-- #region template -->
+    <BButton variant="outline-primary">Primary</BButton>
+    <BButton variant="outline-secondary">Secondary</BButton>
+    <BButton variant="outline-success">Success</BButton>
+    <BButton variant="outline-danger">Danger</BButton>
+    <BButton variant="outline-warning">Warning</BButton>
+    <BButton variant="outline-info">Info</BButton>
+    <BButton variant="outline-light">Light</BButton>
+    <BButton variant="outline-dark">Dark</BButton>
+    <!-- #endregion template -->
+  </div>
+</template>
