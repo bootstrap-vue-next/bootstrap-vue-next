070cb8c6795ea4b8c2e286f936819f8fb585fff1
David W. Gray <dwgray67@hotmail.com>
2025-05-11 07:52:30 -0700

    docs(BProgress): Parity pass (#2689)

---
+++ apps/docs/src/docs/components/demo/ProgressAnimated.vue
@@ -0,0 +1,8 @@
+<template>
+  <!-- #region template -->
+  <BProgress variant="success" :value="25" striped animated />
+  <BProgress class="mt-3" variant="info" :value="50" striped animated />
+  <BProgress class="mt-3" variant="warning" :value="75" striped animated />
+  <BProgress class="mt-3" variant="danger" :value="100" striped animated />
+  <!-- #endregion template -->
+</template>
