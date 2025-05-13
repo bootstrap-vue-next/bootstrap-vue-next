54961f46ad1a97231cac8841fabdfc7c8921a4b3
William Castillo <william.castillo.jr@outlook.com>
2025-05-12 17:01:03 -0700

    feat: implement BAvatar badge-offset
    Badge-offset property prop to component BBadge component.
    
    Revise BBadge style prop to allow style changes for badge offset.
    
    Create badgeOffsetStyle function
    
    GitHub Issue: #2347: BAvatar badge-offset isn't implemented

--- packages/bootstrap-vue-next/src/types/ComponentProps.ts
+++ packages/bootstrap-vue-next/src/types/ComponentProps.ts
@@ -814,6 +814,7 @@ export interface BAvatarProps
   alt?: string
   badge?: boolean | string
   badgeBgVariant?: BgColorVariant | null
+  badgeOffset?: string | null
   badgePlacement?: CombinedPlacement
   badgeTextVariant?: TextColorVariant | null
   badgeVariant?: ColorVariant | null
