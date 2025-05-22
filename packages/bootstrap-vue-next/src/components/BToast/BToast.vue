86426bb7f54e85cd42ff4997f3ec19713ece52a0
Jukka Raimovaara <roska@mentalhouse.fi>
2025-05-22 16:29:01 +0300

    little fixes to the previous merge

--- packages/bootstrap-vue-next/src/components/BToast/BToast.vue
+++ packages/bootstrap-vue-next/src/components/BToast/BToast.vue
@@ -282,16 +282,3 @@ defineExpose({
   stop,
 })
 </script>
-
-<style lang="scss" scoped>
-.toast :deep(.progress .progress-bar) {
-  --bs-progress-bar-transition: none;
-}
-.toast:not(.show) {
-  display: block;
-  opacity: unset;
-}
-.toast.fade:not(.show) {
-  opacity: 0;
-}
-</style>
