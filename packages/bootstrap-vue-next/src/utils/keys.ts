7d20744f88e753b0536436c87579ac34f6a792d0
Jukka Raimovaara <roska@mentalhouse.fi>
2025-06-03 15:32:44 +0300

    fix(BTabs): correct id/index on ssr

--- packages/bootstrap-vue-next/src/utils/keys.ts
+++ packages/bootstrap-vue-next/src/utils/keys.ts
@@ -55,7 +55,7 @@ export const tabsInjectionKey: InjectionKey<{
   lazy: Readonly<Ref<boolean>>
   card: Readonly<Ref<boolean>>
   noFade: Readonly<Ref<boolean>>
-  registerTab: (tab: ComputedRef<TabType>) => void
+  registerTab: (tab: ComputedRef<TabType>) => string
   unregisterTab: (id: string) => void
   activateTab: (id: string | undefined) => void
   activeTabClass: Readonly<Ref<ClassValue>>
