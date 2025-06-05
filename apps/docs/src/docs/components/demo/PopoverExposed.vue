377491b5f5ef9f5bafd3186c53f771556abdfc4f
David W. Gray <dwgray67@hotmail.com>
2025-06-05 15:59:17 -0700

    docs(BPopover): Parity pass (#2701)
    Co-authored-by: Jukka Raimovaara <roska@mentalhouse.fi>

---
+++ apps/docs/src/docs/components/demo/PopoverExposed.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="d-flex flex-column text-md-center">
+    <div class="p-2">
+      <BButton id="popover-expose" variant="primary">I have a popover</BButton>
+    </div>
+
+    <div class="p-2">
+      <BButton class="mx-2" @click="show">Show</BButton>
+      <BButton class="mx-2" @click="hide">Hide</BButton>
+      <BButton class="mx-2" @click="toggle">Toggle</BButton>
+
+      <BPopover ref="myPopover" v-model="model" target="popover-expose" title="Popover">
+        Hello <strong>World!</strong>
+      </BPopover>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {ref} from 'vue'
+import type {BPopover} from 'bootstrap-vue-next'
+const myPopover = ref<InstanceType<typeof BPopover> | null>(null)
+const show = () => myPopover.value?.show()
+const hide = () => myPopover.value?.hide()
+const toggle = () => myPopover.value?.toggle()
+const model = ref(false)
+</script>
---
+++ apps/docs/src/docs/components/demo/PopoverTriggers.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- #region template -->
+  <div class="d-flex gap-2">
+    <BPopover placement="top" body="Default Trigger">
+      <template #target>
+        <BButton> Default Trigger </BButton>
+      </template>
+    </BPopover>
+    <BPopover click placement="top" body="Click Trigger">
+      <template #target>
+        <BButton> Click Trigger </BButton>
+      </template>
+    </BPopover>
+  </div>
+  <!-- #endregion template -->
+</template>
