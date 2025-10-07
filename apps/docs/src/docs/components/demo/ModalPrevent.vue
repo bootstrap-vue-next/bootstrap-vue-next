720daf80ffeca30319f5015a230149e61b54b5e3
David W. Gray <dwgray67@hotmail.com>
2025-10-07 09:40:15 -0700

    docs(BModal): Parity pass (#2866)

---
+++ apps/docs/src/docs/components/demo/ModalPrevent.vue
@@ -0,0 +1,23 @@
+<template>
+  <BButton @click="preventableModal = !preventableModal"> Toggle modal </BButton>
+
+  <BModal
+    v-model="preventableModal"
+    title="Hello, World!"
+    @hide="preventFn"
+  >
+    Foobar?
+    <BFormCheckbox v-model="preventModal">Prevent close</BFormCheckbox>
+  </BModal>
+</template>
+
+<script setup lang="ts">
+import type {BvTriggerableEvent} from 'bootstrap-vue-next'
+import {ref} from 'vue'
+
+const preventableModal = ref(false)
+const preventModal = ref(true)
+const preventFn = (e: BvTriggerableEvent) => {
+  if (preventModal.value) e.preventDefault()
+}
+</script>
--- apps/docs/src/docs/demo/ModalMessageBox.vue
+++ apps/docs/src/docs/demo/ModalMessageBox.vue
@@ -6,19 +6,19 @@
 </template>
 
 <script setup lang="ts">
-import type {BvTriggerableEvent} from 'bootstrap-vue-next'
 import {useModal} from 'bootstrap-vue-next/composables/useModal'
 import {ref} from 'vue'
 
 const {create} = useModal()
 
-const okResult = ref<boolean | null | BvTriggerableEvent>(null)
+const okResult = ref<boolean | null | undefined>(null)
 
 const okBox = async () => {
-  okResult.value = await create({
+  const value = await create({
     body: 'This is an informational message',
     title: 'Message',
     okOnly: true,
-  })
+  }).show()
+  okResult.value = typeof value === 'object' && value !== null && 'ok' in value ? value.ok : null
 }
 </script>
