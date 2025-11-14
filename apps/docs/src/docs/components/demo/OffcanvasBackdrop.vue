ca26be4fe3e73fdb6872b1297f8dc51a59fe1370
David W. Gray <dwgray67@hotmail.com>
2025-11-14 15:13:55 -0800

    docs(Offcanvas): Parity pass (#2900)

---
+++ apps/docs/src/docs/components/demo/OffcanvasBackdrop.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <BButton v-b-toggle.offcanvas-backdrop>Toggle Offcanvas</BButton>
+
+    <BOffcanvas
+      id="offcanvas-backdrop"
+      title="Offcanvas with backdrop"
+      no-backdrop
+      shadow
+    >
+      <div class="px-3 py-2">
+        <p>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
+          in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+        </p>
+        <BImg
+          src="https://picsum.photos/500/500/?image=54"
+          fluid
+          thumbnail
+        />
+      </div>
+    </BOffcanvas>
+  </div>
+</template>
