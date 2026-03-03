dfafd7561bcb1b9dd4b6ab2836f80fec0b4d0f4b
Issayah <github.me.mzu5a@simplelogin.com>
2026-03-03 15:19:25 -0600

    revert blink (#3097)

--- packages/nuxt/src/module.ts
+++ packages/nuxt/src/module.ts
@@ -61,7 +61,7 @@ export default defineNuxtModule<ModuleOptions>({
     }
 
     // Add components
-    useComponents(resolve)
+    useComponents()
 
     // Add directives
     if (Object.values(normalizedDirectiveOptions).includes(true)) {
