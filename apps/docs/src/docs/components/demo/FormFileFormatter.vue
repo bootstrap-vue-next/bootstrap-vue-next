66479034094d9bfc04c8d31be2012dda61e6f468
Issayah <github.me.mzu5a@simplelogin.com>
2026-06-22 08:45:30 -0500

    feat(BFormFile): add ariaLiveFormatter prop fixes #3223 (#3225)

--- apps/docs/src/docs/components/demo/FormFileFormatter.vue
+++ apps/docs/src/docs/components/demo/FormFileFormatter.vue
@@ -4,6 +4,7 @@
       v-model="files"
       multiple
       :file-name-formatter="formatNames"
+      :aria-live-formatter="formatAriaLive"
       placeholder="Choose multiple files"
     />
     <div class="mt-3">Display: {{ displayText }}</div>
@@ -16,10 +17,18 @@ import { computed, ref } from 'vue'
 const files = ref<File[] | null>(null)
 
 const formatNames = (files: readonly File[]): string => {
+  // This is where one would add internationalization code
   if (files.length === 1 && files[0] !== undefined) {
     return files[0].name
   }
   return `${files.length} files selected (${files.map((f) => f.name).join(', ')})`
+}
+const formatAriaLive = (files: readonly File[]): string => {
+  // This is where one would add internationalization code
+  if (files.length === 1 && files[0] !== undefined) {
+    return `Selected file: ${files[0].name}`
+  }
+  return `${files.length} files selected`
 }
 
 const displayText = computed(() => {
