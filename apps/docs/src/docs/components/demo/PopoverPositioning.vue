4beb37d546a97596d32f70bfe97cba21c530d65e
Issayah <github.me.mzu5a@simplelogin.com>
2026-08-21 10:56:42 -0500

    docs: consolidate positioning demos with v-for loops (#152) (#3310)

--- apps/docs/src/docs/components/demo/PopoverPositioning.vue
+++ apps/docs/src/docs/components/demo/PopoverPositioning.vue
@@ -2,71 +2,20 @@
   <!-- #region template -->
   <BContainer>
     <BRow class="my-2">
-      <BCol class="d-grid gap-2">
-        <BPopover placement="top">
-          <template #target><BButton>Top</BButton></template>
-          <template #title>Popover Top</template>
-          I am popover <b>component</b> content!
-        </BPopover>
-        <BPopover placement="top-start">
-          <template #target><BButton>Top Start</BButton></template>
-          <template #title>Popover Top Start</template>
-          I am popover <b>component</b> content!
-        </BPopover>
-        <BPopover placement="top-end">
-          <template #target><BButton>Top End</BButton></template>
-          <template #title>Popover Top End</template>
-          I am popover <b>component</b> content!
-        </BPopover>
-      </BCol>
-      <BCol class="d-grid gap-2">
-        <BPopover placement="bottom">
-          <template #target><BButton>Bottom</BButton></template>
-          <template #title>Popover Bottom</template>
-          I am popover <b>component</b> content!
-        </BPopover>
-        <BPopover placement="bottom-start">
-          <template #target><BButton>Bottom Start</BButton></template>
-          <template #title>Popover Bottom Start</template>
-          I am popover <b>component</b> content!
-        </BPopover>
-        <BPopover placement="bottom-end">
-          <template #target><BButton>Bottom End</BButton></template>
-          <template #title>Popover Bottom End</template>
-          I am popover <b>component</b> content!
-        </BPopover>
-      </BCol>
-      <BCol class="d-grid gap-2">
-        <BPopover placement="left">
-          <template #target><BButton>Left</BButton></template>
-          <template #title>Popover Left</template>
-          I am popover <b>component</b> content!
-        </BPopover>
-        <BPopover placement="left-start">
-          <template #target><BButton>Left Start</BButton></template>
-          <template #title>Popover Left Start</template>
-          I am popover <b>component</b> content!
-        </BPopover>
-        <BPopover placement="left-end">
-          <template #target><BButton>Left End</BButton></template>
-          <template #title>Popover Left End</template>
-          I am popover <b>component</b> content!
-        </BPopover>
-      </BCol>
-      <BCol class="d-grid gap-2">
-        <BPopover placement="right">
-          <template #target><BButton>Right</BButton></template>
-          <template #title>Popover Right</template>
-          I am popover <b>component</b> content!
-        </BPopover>
-        <BPopover placement="right-start">
-          <template #target><BButton>Right Start</BButton></template>
-          <template #title>Popover Right Start</template>
-          I am popover <b>component</b> content!
-        </BPopover>
-        <BPopover placement="right-end">
-          <template #target><BButton>Right End</BButton></template>
-          <template #title>Popover Right End</template>
+      <BCol
+        v-for="group in [['top', 'top-start', 'top-end'], ['bottom', 'bottom-start', 'bottom-end'], ['left', 'left-start', 'left-end'], ['right', 'right-start', 'right-end']]"
+        :key="group[0]"
+        class="d-grid gap-2"
+      >
+        <BPopover
+          v-for="placement in group"
+          :key="placement"
+          :placement="placement"
+        >
+          <template #target>
+            <BButton>{{ placement }}</BButton>
+          </template>
+          <template #title>Popover {{ placement }}</template>
           I am popover <b>component</b> content!
         </BPopover>
       </BCol>
@@ -74,28 +23,20 @@
   </BContainer>
   <BContainer>
     <BRow class="my-2">
-      <BCol class="d-grid gap-2">
-        <BPopover placement="auto">
-          <template #target><BButton>Auto</BButton></template>
-          <template #title>Popover Auto</template>
+      <BCol
+        v-for="placement in ['auto', 'auto-start', 'auto-end']"
+        :key="placement"
+        class="d-grid gap-2"
+      >
+        <BPopover :placement="placement">
+          <template #target>
+            <BButton>{{ placement }}</BButton>
+          </template>
+          <template #title>Popover {{ placement }}</template>
           I am popover <b>component</b> content!
         </BPopover>
       </BCol>
-      <BCol class="d-grid gap-2">
-        <BPopover placement="auto-start">
-          <template #target><BButton>Auto Start</BButton></template>
-          <template #title>Popover Auto Start</template>
-          I am popover <b>component</b> content!
-        </BPopover>
-      </BCol>
-      <BCol class="d-grid gap-2">
-        <BPopover placement="auto-end">
-          <template #target><BButton>Auto End</BButton></template>
-          <template #title>Popover Auto End</template>
-          I am popover <b>component</b> content!
-        </BPopover>
-      </BCol></BRow
-    ></BContainer
-  >
+    </BRow>
+  </BContainer>
   <!-- #endregion template -->
 </template>
